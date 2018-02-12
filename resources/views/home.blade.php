a868cb5a5a37d6161128a00a801779e399e29dc7
zombieite <john_binns@yahoo.com>
2018-02-11 23:02:31 -0700

    Redirect after editing, better logged in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,9 @@
 
 @section('content')
 <h2>Find thems you're looking for and thems you've lost</h2>
-<p>
-Since The Big Bang happened, Wasteland has become known the world over as the place we come to find thems we're looking for and thems we've lost. Whether you're looking for love, a friend you haven't met yet, an enemy you haven't battled yet, or someone you knew once and lost, without telephones or a reliable post office we need a designated meeting place. That's Wasteland.
-</p>
-<p>
-On this site you'll create a profile that is as brief, mysterious, and information-free as you like. Individuals, couples, or groups can create profiles. Once you've created your profile, you'll either accept a random match or choose some other profiles as ones you'd be open to searching for and meeting at the next Wasteland event. Shortly before the next Wasteland event the matching algorithm will run and you'll return to see if you have a mutual match. Your mission is to find them at the event. They'll be looking for you, too.
-</p>
-<p>
-When you meet, you are to merge the backstories of your Wasteland personas. You'll come up with a story of how you met. The story can be realistic (we just met here today at this Wasteland event) or wild (we fought each other in a battle 200 years ago before we were both cryogenically frozen) or romantic (we were once in love but haven't seen each other since The Big Bang separated us ten years ago). If your backstories are utterly incompatible, you can each declare the other insane.
-</p>
-<p>
-If you come together to find me, Firebird, and tell me your story, I'll reward you with special caps. <a href="/profile/1/Firebird">Here's my profile</a>.
-</p>
-<p>
-<a href="/profile/create">Get started by creating your profile</a>.
-</p>
+<h3>1. COMPLETE: Profile created</h3>
+<h3>2. NEXT: <a href="/profile/compatible">Mark other profiles as favorites</a></h3>
+<h3>3. Find out who you've been matched with</h3>
+<h3>4. Seek out the person or group you've been matched with</h3>
+<h3>5. Get your caps</h3>
 @endsection
