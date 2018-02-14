5dc0dea3a8c62212f78ff6b58c88533670fe6526
zombieite <387685+zombieite@users.noreply.github.com>
2018-02-13 20:10:05 -0700

    add composer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <h2>Find thems you're looking for and thems you've lost</h2>
 <h3>1. COMPLETE: Profile created</h3>
 <h3>2. NEXT: <a href="/profile/compatible">Mark other profiles as favorites</a></h3>
-<h3>3. Find out who you've been matched with</h3>
+<h3>3. Find out who you've been matched with. Next matching run will be complete Feb 22.</h3>
 <h3>4. Seek out the person or group you've been matched with</h3>
 <h3>5. Get your caps</h3>
 @endsection
