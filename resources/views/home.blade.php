21dad2f8de95538b64d5889de1f2cbff04bb84ad
zombieite <john_binns@yahoo.com>
2018-02-15 21:53:09 -0700

    Rating profiles sends you to home when done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,11 @@
 @section('content')
 <h2>Find thems you're looking for and thems you've lost</h2>
 <h3>1. COMPLETE: Profile created</h3>
+@if ($all_seen)
+<h3> 2. COMPLETE: Other profiles rated</h3>
+@else
 <h3>2. NEXT: <a href="/profile/compatible">Choose potential matches</a></h3>
+@endif
 <h3>3. Find out who you've been matched with. Next matching run will be complete Feb 22.</h3>
 <h3>4. Seek out the person or group you've been matched with. Next event is The Wasteland Winter Games.</h3>
 <h3>5. Get your caps</h3>
