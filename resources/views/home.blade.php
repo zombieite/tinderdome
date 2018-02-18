df43658c8ac8c13735d8e62978643692c6191512
zombieite <387685+zombieite@users.noreply.github.com>
2018-02-18 11:24:13 -0700

    tweaks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 <h2>Find thems you're looking for and thems you've lost</h2>
-<h3>1. COMPLETE: Profile created</h3>
+<h3>1. COMPLETE: Profile created.</h3>
 @if ($all_seen)
-<h3> 2. COMPLETE: Other profiles rated</h3>
+<h3> 2. COMPLETE: Other profiles rated.</h3>
 @else
-<h3>2. NEXT: <a href="/profile/compatible">Choose potential matches</a></h3>
+<h3>2. NEXT: <a href="/profile/compatible">Choose potential matches</a>.</h3>
 @endif
-<h3>3. Find out who you've been matched with. Next matching run will be complete Feb 22.</h3>
-<h3>4. At the next event, seek out the person or group you've been matched with. Next event is The Wasteland Winter Games.</h3>
-<h3>5. Find <a href="/profile/Firebird">Firebird</a> and get your caps</h3>
+<h3>3. Find out who you've been matched with. Next matching run will be complete Feb 22, 2018, just before the Winter Games.</h3>
+<h3>4. At the event, seek out the person or group you've been matched with.</h3>
+<h3>5. Find <a href="/profile/Firebird">Firebird</a> and get your caps.</h3>
 @endsection
