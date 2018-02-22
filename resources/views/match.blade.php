a60291e2c4e9be7606102be0495064b91550d92c
zombieite <387685+zombieite@users.noreply.github.com>
2018-02-21 19:55:00 -0700

    Begin prioritizing the mutual matches

--- resources/views/match.blade.php
+++ resources/views/match.blade.php
@@ -2,10 +2,31 @@
 @section('content')
 
 <h2>Matches</h2>
-<ul>
+<table>
+	<tr>
+		<th><b>Id</b></th>
+		<th><b>Name</b></th>
+		<th><b>Gender</b></th>
+		<th><b>DGOM</b></th>
+		<th><b>Popularity</b></th>
+		<th><b>Random ok</b></th>
+		<th><b>Mutual matches</b></th>
+	</tr>
 @foreach ($users as $user)
-	<li>{{ $user->name }} {{ $user->popularity }} </li>
+	<tr>
+		<td>{{ $user->id }}</td>
+		<td>{{ $user->name }}</td>
+		<td>{{ $user->gender }} </td>
+		<td>{{ $user->gender_of_match }} </td>
+		<td>{{ $user->popularity }}</td>
+		<td>{{ $user->random_ok}} </td>
+		<td>
+			@foreach ($user->mutual_matches as $mutual_match)
+				{{ $mutual_match->name }},
+			@endforeach
+		</td>
+	</tr>
 @endforeach
-</ul>
+</table>
 
 @endsection
