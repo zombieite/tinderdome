35ea15a53abfe01b838de2ddfa7eb2927610713f
zombieite <387685+zombieite@users.noreply.github.com>
2018-02-21 20:25:55 -0700

    Sort mutual matches by chooser's desired gender of match

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -8,9 +8,24 @@ use Illuminate\Support\Facades\DB;
 
 class MatchController extends Controller
 {
-
+	// Prioritize this user's mutual matches by
+	// 1. Whether they are user's preferred match gender
+	// 2. If preferred match gender is unspecified, male first (just because there are disproportionate numbers of popular females, and unmatched straight males)
+	// 3. Popularity (this matches popular to popular)
+	// 4. Has photos (prioritize more complete profiles)
+	// 5. Length of description (prioritize more complete profiles)
+	// 6. Random ok (prioritize random-ok users just as a perk to them)
+	// 6. Id (prioritize early signups just as a perk to them)
 	private static function sortMatches($a, $b) {
-		return strcmp($a->name, $b->name);
+		$desired_gender_of_chooser = $a->desired_gender_of_chooser; // Should be same for both $a and $b
+		if ($desired_gender_of_chooser) {
+			if (($a->gender === $desired_gender_of_chooser) && ($b->gender !== $desired_gender_of_chooser)) {
+				return -1;
+			} else if (($b->gender === $desired_gender_of_chooser) && ($a->gender !== $desired_gender_of_chooser)) {
+				return 1;
+			}
+		}
+		return $a->id - $b->id; // intcmp
 	}
 
 	public function match()
@@ -79,19 +94,13 @@ class MatchController extends Controller
 				where
 					attending_$next_event_name
 					and name != 'Firebird'
-				order by
-					id
 			", [ $user->id, $user->id ]);
+			
+			// Can't figure out a better way to pass params to sort
+			foreach ($mutual_matches as $match) {
+				$match->desired_gender_of_chooser = $user->gender_of_match;
+			}
 
-			// TODO: SORT AND MATCH.
-			// Prioritize this user's mutual matches by
-			// 1. Whether they are user's preferred match gender
-			// 2. If preferred match gender is unspecified, male first (just because there are disproportionate numbers of popular females, and unmatched straight males)
-		    // 3. Popularity (this matches popular to popular)
-			// 4. Has photos (prioritize more complete profiles)
-			// 5. Length of description (prioritize more complete profiles)
-			// 6. Random ok (prioritize random-ok users just as a perk to them)
-			// 6. Id (prioritize early signups just as a perk to them)
 			usort($mutual_matches, array($this, 'sortMatches'));
 
 			$user->mutual_matches = $mutual_matches;
