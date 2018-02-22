18d275fc73778ab1af5d204da97b6b15c50ea213
zombieite <387685+zombieite@users.noreply.github.com>
2018-02-22 01:19:38 -0700

    messaging

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
 <h3>Mission steps</h3>
 <h4>1. COMPLETE: Profile created.</h4>
 @if ($all_seen)
-<h4> 2. COMPLETE: Other profiles rated.</h4>
+<h4> 2. COMPLETE: Other profiles rated. Next event is Wastelanders Ball. Rating of profiles for Ball attendees will begin soon.</h4>
 @else
 <h4>2. NEXT: <a href="/profile/compatible">Choose potential matches</a>.</h4>
 @endif
-<h4>3. Find out who you've been matched with. Next matching run will be complete 7pm Pacific time, Feb 21, 2018, just before the Winter Games.</h4>
+<h4>3. <a href="/profile/match">COMPLETE: You are awaited! Go to your match page to see who you're matched with.</a></h4>
 <h4>4. At the event, seek out the person or group you've been matched with. When you find them, the topic of conversation will be the merging of the backstories of your wasteland personas.</h4>
 <h4>5. (Optional) Together, find <a href="/profile/Firebird">Firebird</a>, tell him your stories, and get your caps.</h4>
 @endsection
