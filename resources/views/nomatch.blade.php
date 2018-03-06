1f66706239724c990d768173a0e44d3928e17b1d
zombieite <387685+zombieite@users.noreply.github.com>
2018-03-05 23:29:20 -0700

    tell people when they have no match

---
+++ resources/views/nomatch.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+
+<h2>You do not yet have a match for the next event</h2>
+<p>
+	There are three possible reasons for this.
+	<ol>
+		<li>It's possible matches have not been run yet for the next event (this page doesn't check that yet to find out for sure).</li>
+		<li>You were too picky. If you want to be more likely to get a match at the next event, edit your profile and mark that you are open to a random match.</li>
+		<li>Random chance did not favor you this time. Part of our matching algorithm is pure luck.</li>
+		<li>You signed up late and not enough people rated your profile. Users at the next event will see your profile much sooner and you'll be sure to receive more ratings then.</li>
+	</ol>
+</p>
+
+@endsection
