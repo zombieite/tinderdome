cf9eb3a799b99260182dbe3217c88944840276eb
zombieite <387685+zombieite@users.noreply.github.com>
2018-03-07 02:45:55 -0700

    finished for ball

--- resources/views/nomatch.blade.php
+++ resources/views/nomatch.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 @section('content')
 
-<h2>You do not yet have a match for the next event</h2>
+<h2>You do not have a match for the next event</h2>
 <p>
 	There are a few possible reasons for this.
 	<ol>
-		<li>It's possible matches have not been run yet for the next event (this page doesn't check that yet to find out for sure).</li>
+<!--		<li>It's possible matches have not been run yet for the next event (this page doesn't check that yet to find out for sure).</li> -->
 		<li>You were too picky. If you want to be more likely to get a match at the next event, rate more profiles with a yes, or edit your profile and mark that you are open to a random match.</li>
 		<li>Random chance did not favor you this time. Part of our matching algorithm is pure luck.</li>
 		<li>You signed up late and not enough people had time to rate your profile. Users at the next event will see your profile much sooner and you'll receive more ratings for the next event.</li>
