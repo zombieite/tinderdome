b3d8589c2b762f64fba98b8a3c609165e3945f05
zombieite <john_binns@yahoo.com>
2018-03-06 22:26:15 -0700

    Got new match algorithm mostly working

--- resources/views/match.blade.php
+++ resources/views/match.blade.php
@@ -4,31 +4,33 @@
 <h2>Matches</h2>
 <table>
 	<tr>
-		<th><b>Id</b></th>
 		<th><b>Name</b></th>
+		<th><b>Id</b></th>
+		<th><b>Matched to id</b></th>
+		<th><b>Matched to name</b></th>
+		<th><b>Random match</b></th>
+		<th><b>Random ok</b></th>
 		<th><b>Gender</b></th>
 		<th><b>DGOM</b></th>
 		<th><b>Popularity</b></th>
-		<th><b>Random ok</b></th>
 		<th><b>Mutual matches</b></th>
-		<th><b>Best match</b></th>
-		<th><b>Random match</b></th>
 	</tr>
 @foreach ($users as $user)
 	<tr @if ($user->cant_match) style="background-color:red;" @endif>
-		<td>{{ $user->id }}</td>
 		<td>{{ $user->name }}</td>
+		<td>{{ $user->id }}</td>
+		<td>{{ $matched_users_hash[$user->id] }}</td>
+		<td>{{ $matched_users_hash[$user->id] ? $id_to_name_hash[$matched_users_hash[$user->id]] : '' }}</td>
+		<td>{{ $user->random_match ? 'Random' : '' }}</td>
+		<td>{{ $user->random_ok }}</td>
 		<td>{{ $user->gender }}</td>
 		<td>{{ $user->gender_of_match }}</td>
 		<td>{{ $user->popularity }}</td>
-		<td>{{ $user->random_ok }}</td>
 		<td>
 			@foreach ($user->mutual_unmet_matches as $mutual_match)
 				{{ $mutual_match->name }},
 			@endforeach
 		</td>
-		<td>{{ $matched_users_hash[$user->id]->taken ? $matched_users_hash[$user->id]->taken : '' }}</td>
-		<td>{{ $user->random_match ? 'Yes' : '' }}</td>
 	</tr>
 @endforeach
 </table>
