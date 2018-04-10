dc8a5dca515996b18da12bf7270a263689fdb0dd
zombieite <john_binns@yahoo.com>
2018-04-09 22:41:23 -0700

    re-rating profiles works

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 @section('content')
+@php ($last_profile = 0)
 @foreach ($profiles as $profile)
 	<p>
-		{{ $profile['wasteland_name'] }}
+		<a name="profile{{ $profile['profile_id'] }}">{{ $profile['wasteland_name'] }}</a>
 		@if ($profile['gender'])
 			&middot; {{ $profile['gender'] === 'M' ? 'Male' : ($profile['gender'] === 'F' ? 'Female' : 'Other') }}
 		@endif
@@ -30,6 +31,15 @@
 			<a target="_blank" href="/uploads/image-{{ $profile['profile_id'] }}-{{ preg_replace('/\s/', '-', $profile['wasteland_name']) }}-{{ $i }}.jpg"><img src="/uploads/image-{{ $profile['profile_id'] }}-{{ preg_replace('/\s/', '-', $profile['wasteland_name']) }}-{{ $i }}.jpg" style="height:100px;"></a>
 		@endfor
 	</p>
+	<form action="#profile{{ $last_profile }}" method="POST">
+		{{ csrf_field() }}
+		<input type="hidden" name="chosen" value="{{ $profile['profile_id'] }}">
+		<input type="submit" name="YesYesYes" value="YES YES YES"@if ($profile['choice'] == 3) class="yes"@endif>
+		<input type="submit" name="YesYes" value="Yes YES"@if ($profile['choice'] == 2) class="yes"@endif>
+		<input type="submit" name="Yes" value="Yes"@if ($profile['choice'] == 1) class="yes"@endif>
+		<input type="submit" name="No" value="No"@if ($profile['choice'] == 0) class="no"@endif>
+	</form>
+	@php ($last_profile = $profile['profile_id'])
 	<hr>
 @endforeach
 @endsection
