73d3e3ea8601472f16d2df1e4369ca418a8b6716
zombieite <john_binns@yahoo.com>
2018-04-10 22:17:02 -0700

    only allow limited nos

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,7 +12,7 @@ use Log;
 
 class ProfileController extends Controller
 {
-	public function show($profile_id, $wasteland_name_from_url, $unchosen_user = null, $count_left = null, $is_my_match = null)
+	public function show($profile_id, $wasteland_name_from_url, $unchosen_user = null, $count_left = null, $is_my_match = null, $nos_left = null)
 	{
 		$profile                 = $unchosen_user ? $unchosen_user : \App\User::find( $profile_id );
 		$wasteland_name_from_url = preg_replace('/-/', ' ', $wasteland_name_from_url);
@@ -61,6 +61,7 @@ class ProfileController extends Controller
 			'count_left'             => $count_left,
 			'success_message'        => $success_message,
 			'is_my_match'            => $is_my_match,
+			'nos_left'               => $nos_left,
 			'auth_user'              => $auth_user,
 		]);
 	}
@@ -266,14 +267,14 @@ class ProfileController extends Controller
 	public function match()
 	{
 		$user        = Auth::user();
-		$user_id     = $user->id;
+		$user_id     = Auth::id();
 
 		if ($user_id === 1 && isset($_GET['masquerade'])) {
 			$user_id = $_GET['masquerade']+0;
 			Log::debug("Masquerading as $user_id");
 		}
 
-		$next_event  = 'ball';
+		$next_event  = 'detonation';
 		$year        = 2018;
 		$match_array = DB::select("
 			select
@@ -315,16 +316,7 @@ class ProfileController extends Controller
 	public function compatible()
 	{
 		$chooser_user                     = Auth::user();
-		$chooser_user_id                  = $chooser_user->id;
-		$gender_of_match                  = $chooser_user->gender_of_match;
-
-		if ($gender_of_match) {
-			if (in_array($gender_of_match, ['M', 'F', 'O'])) {
-				// All good
-			} else {
-				abort(500, "Invalid value found for gender of match: '$gender_of_match'");
-			}
-		}
+		$chooser_user_id                  = Auth::id();
 
 		if (isset($_POST['chosen'])) {
 			$chosen_id    = $_POST['chosen'];
@@ -344,8 +336,6 @@ class ProfileController extends Controller
 			DB::update( $update, [ $choose_value, $chooser_user_id, $chosen_id ] );
 		}
 
-		$are_they_my_wanted_gender_clause = $gender_of_match ? "and gender='" . $gender_of_match ."'" : '';
-
 		$unchosen_users = DB::select("
 			select
 				*
@@ -360,7 +350,6 @@ class ProfileController extends Controller
 				and id<>1
 				and choice is null
 				and seen is null
-				$are_they_my_wanted_gender_clause
 			order by
 				id
 		",
@@ -370,6 +359,14 @@ class ProfileController extends Controller
 		foreach ($unchosen_users as $user_to_count) {
 			$count_left++;
 		}
+
+		$nos_left = 5;
+		$nos_used = 0;
+		$nos_used_results = DB::select('select count(*) nos_used from choose where choice=0 and chooser_id=?', [$chooser_user_id]);
+		foreach ($nos_used_results as $nos_used_result) {
+			$nos_used = $nos_used_result->nos_used;
+		}
+		$nos_left -= $nos_used;
 
 		if ($unchosen_user) {
 			$unchosen_user_id = $unchosen_user->id;
@@ -384,7 +381,7 @@ class ProfileController extends Controller
 				', [ $chooser_user_id, $unchosen_user_id ]);
 			}
 
-			return $this->show($unchosen_user_id, $unchosen_user->name, $unchosen_user, $count_left);
+			return $this->show($unchosen_user_id, $unchosen_user->name, $unchosen_user, $count_left, null, $nos_left);
 		}
 
 		return redirect('/');
