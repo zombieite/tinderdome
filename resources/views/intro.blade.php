6f300b9769c0e610b8ec0da218262dfd19141485
zombieite <387685+zombieite@users.noreply.github.com>
2018-04-29 17:44:52 -0700

    text

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -4,9 +4,9 @@
 Since The Apocalypse, Wasteland has become known as the place we come to find thems we're looking for and thems we've lost. Whether you're looking for love, a friend you haven't met yet, an enemy you haven't battled yet, or someone you knew once and lost, we need a designated meeting place. That's Wasteland.
 </p>
 <p>
-You Are Awaited is a simple mission that is conducted during Wasteland Weekend and other post-apocalyptic events. Signups are year-round, there's no need to wait for the next event. There are no in-person signups, all signups must be done online. Money is useless nowadays so participation is free.
+You Are Awaited is a simple mission that is conducted during Wasteland Weekend and other post-apocalyptic events. Signups can be done year-round. There are no in-person signups. All signups are done online. Money is useless nowadays so participation is free. There's no punishment for failure, but there are rewards for victory.
 </p>
-<h2>Meet our top {{ $leader_count }} heroes... and {{ $nonleader_count }} others</h2>
+<h2>Meet our top {{ $leader_count }} heroes... and {{ $nonleader_count }} others. Here's how.</h2>
 @foreach ($leaderboard as $leader)
 <div class="profile_search_block">
 	@if ($leader['number_photos'])
@@ -25,7 +25,7 @@ You'll create a profile that tells everyone a little bit about you. <a href="/pr
 </p>
 <h3>2. Let us know who you'd enjoy meeting at the next event</h3>
 <p>
-Once you've created your profile, you can browse other profiles and choose the people you'd enjoy meeting at the next event.
+Once you've created your profile, you can browse other profiles and choose who you'd enjoy meeting.
 </p>
 <h3>3. Find out who you're matched with</h3>
 <p>
@@ -33,10 +33,10 @@ Shortly before each event, the matching algorithm will run. You will be matched 
 </p>
 <h3>4. Seek out your match</h3>
 <p>
-Your mission is to find your match at the event. They'll be looking for you, too. You can use this opportunity to merge the backstories of your Wasteland personas. How did you two meet?
+Your mission is to find your match at the event. They'll be looking for you, too. Optionally, when you meet, you can use this opportunity to merge the backstories of your Wasteland personas. Come up with a real or fictionalized account of how you met.
 </p>
 <h3>5. Get your caps</h3>
 <p>
-If you find <a href="/profile/Firebird">Firebird</a> and tell him your stories, you'll be rewarded with caps. Every mission you complete earns you a different cap. The sooner you sign up and the more missions you complete, the higher priority your preferences will be given by the matching algorithm. So get started by <a href="{{ route('register') }}">creating your profile</a>.
+If you find <a href="/profile/Firebird">Firebird</a> and tell him your story, you'll be rewarded with caps. Every mission you complete earns you a different cap. Get started now by <a href="{{ route('register') }}">creating your profile</a>.
 </p>
 @endsection
