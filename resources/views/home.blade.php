a05c75f873ebc1f10d67fa373a0281f260991642
zombieite <387685+zombieite@users.noreply.github.com>
2018-05-04 16:36:13 -0700

    css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<p>1. COMPLETE: Profile created.</p>
+<ol>
+<li>COMPLETE: Profile created.</li>
 @if (!$unrated_users)
-<p> 2. COMPLETE: You have rated every profile. Check back later to rate new arrivals. Or you can <a href="/search">revisit profiles</a> you've already viewed.</p>
+<li>COMPLETE: You have rated every profile. Check back later to rate new arrivals. Or you can <a href="/search">revisit profiles</a> you've already viewed.</li>
 @else
-<p>2. <a href="/profile/compatible?">Choose potential matches</a>.</p>
+<li><a href="/profile/compatible?">Choose potential matches</a>.</li>
 @endif
 @if ($matched)
-<p>3. <a href="/profile/match">COMPLETE: You are awaited! Click here to see who you're matched with.</a></p>
+<li><a href="/profile/match">COMPLETE: You are awaited! Click here to see who you're matched with.</a></li>
 @else
-<p>3. Next matching run will happen before Detonation Uranium Springs 2018. If you'll be attending <a href="/profile/edit">be sure your profile has the appropriate box checked</a>. Check back before then to see who you're matched with.</p>
+<li>Next matching run will happen before Detonation Uranium Springs 2018. If you'll be attending <a href="/profile/edit">be sure your profile has the appropriate box checked</a>. Check back before then to see who you're matched with.</li>
 @endif
-<p>4. At the event, seek out your match.</p>
-<p>5. Find <a href="/profile/Firebird">Firebird</a> to receive your reward.</p>
+<li>At the event, seek out your match.</li>
+<li>Find <a href="/profile/Firebird">Firebird</a> to receive your reward.</li>
+</ol>
 @endsection
