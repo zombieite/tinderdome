1affb110d5523b584182ad2f29df66c5a22a080c
zombieite <john_binns@yahoo.com>
2018-05-04 09:14:49 -0700

    css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<h4>1. COMPLETE: Profile created.</h4>
+<p>1. COMPLETE: Profile created.</p>
 @if (!$unrated_users)
-<h4> 2. COMPLETE: You have rated every profile. Check back later to rate new arrivals. Or you can <a href="/search">revisit profiles</a> you've already viewed.</h4>
+<p> 2. COMPLETE: You have rated every profile. Check back later to rate new arrivals. Or you can <a href="/search">revisit profiles</a> you've already viewed.</p>
 @else
-<h4>2. <a href="/profile/compatible?">Choose potential matches</a>.</h4>
+<p>2. <a href="/profile/compatible?">Choose potential matches</a>.</p>
 @endif
 @if ($matched)
-<h4>3. <a href="/profile/match">COMPLETE: You are awaited! Click here to see who you're matched with.</a></h4>
+<p>3. <a href="/profile/match">COMPLETE: You are awaited! Click here to see who you're matched with.</a></p>
 @else
-<h4>3. Next matching run will happen before Detonation Uranium Springs 2018. If you'll be attending <a href="/profile/edit">be sure your profile has the appropriate box checked</a>. Check back before then to see who you're matched with.</h4>
+<p>3. Next matching run will happen before Detonation Uranium Springs 2018. If you'll be attending <a href="/profile/edit">be sure your profile has the appropriate box checked</a>. Check back before then to see who you're matched with.</p>
 @endif
-<h4>4. At the event, seek out your match.</h4>
-<h4>5. Find <a href="/profile/Firebird">Firebird</a> to receive your reward.</h4>
+<p>4. At the event, seek out your match.</p>
+<p>5. Find <a href="/profile/Firebird">Firebird</a> to receive your reward.</p>
 @endsection
