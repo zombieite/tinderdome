6536c4f857248ed26d9514ceaeb43ee33ca41423
zombieite <387685+zombieite@users.noreply.github.com>
2018-05-09 08:48:49 -0700

    interface

--- resources/views/rating_form.blade.php
+++ resources/views/rating_form.blade.php
@@ -2,7 +2,7 @@
 	{{ csrf_field() }}
 	<input type="hidden" name="chosen" value="{{ $user_id_to_rate }}">
 	@if ($logged_in_user_hoping_to_find_love)
-		<input type="submit" name="YesYesYes" value="Yes 🖤"@if (($current_choice == 3) || !isset($current_choice)) class="yesyesyes"@else class="unselected_emoji_button" @endif>
+		<input type="submit" name="YesYesYes" value="YES 🖤"@if (($current_choice == 3) || !isset($current_choice)) class="yesyesyes"@else class="unselected_emoji_button" @endif>
 	@endif
 	<input type="submit" name="YesYes" value="Yes"@if (($current_choice == 2) || !isset($current_choice)) class="yesyes"@endif>
 	<input type="submit" name="Yes" value="Neutral"@if (($current_choice == 1) || !isset($current_choice)) class="yes"@endif>
