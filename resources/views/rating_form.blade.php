ae18b700039eda24e5e9344eea7067ced8a42a21
zombieite <john_binns@yahoo.com>
2018-05-08 23:39:37 -0700

    move ratings into component

---
+++ resources/views/rating_form.blade.php
@@ -0,0 +1,15 @@
+<form action="{{ $action }}" method="POST">
+	{{ csrf_field() }}
+	<input type="hidden" name="chosen" value="{{ $user_id_to_rate }}">
+	@if ($logged_in_user_hoping_to_find_love)
+		<input type="submit" name="YesYesYes" value="Yes Yes Yes"@if (($current_choice == 3) || !isset($current_choice)) class="yes"@endif>
+	@endif
+	<input type="submit" name="YesYes" value="Yes Yes"@if (($current_choice == 2) || !isset($current_choice)) class="yes"@endif>
+	<input type="submit" name="Yes" value="Yes"@if (($current_choice == 1) || !isset($current_choice)) class="yes"@endif>
+	<input type="submit" name="Met" value="I have met them"@if (($current_choice == -1) || !isset($current_choice)) class="met"@endif>
+	@if ($nos_left > 0)
+		<input type="submit" name="No" value="No ({{ $nos_left }} left)"@if (($current_choice == 0) || !isset($current_choice)) class="no"@endif>
+	@else
+		<br><br><span class="no">To mark more users as no, you must <a href="/search">change {{ -$nos_left+1 }} of your previous no ratings to yes</a>.</span>
+	@endif
+</form>
