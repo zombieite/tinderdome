d528fee551215a5d0a7ba7f480ce011c045c655a
zombieite <john_binns@yahoo.com>
2018-05-19 08:23:15 -0700

    text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
 @if ($matched)
 <li><a href="/profile/match">COMPLETE: You are awaited! Click here to see who you're matched with.</a></li>
 @else
-<li>Next matching run will happen on the night of May 23 for Detonation Uranium Springs 2018. If you'll be attending <a href="/profile/edit">be sure your profile has the appropriate box checked</a>. Check back before then to see who you're matched with.</li>
+<li>Next matching run will happen on the night of May 23 for Detonation Uranium Springs 2018. If you'll be attending <a href="/profile/edit">be sure your profile has the appropriate box checked</a>. Check back here just before the event to see who you're matched with.</li>
 @endif
 <li>At the event, seek out your match.</li>
 <li>Find <a href="/profile/Firebird">Firebird</a> to receive your reward.</li>
