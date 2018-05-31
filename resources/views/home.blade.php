c5c4a85dc9abb59262df313836db0e14a0e8467d
zombieite <john_binns@yahoo.com>
2018-05-31 07:17:13 -0700

    formatting, text, cleanup, redirects

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <ol>
-<li>COMPLETE: Profile created.</li>
+<li>COMPLETE: <a href="/profile/me">Profile</a> created.</li>
 @if ($unrated_users)
 	@if ($random_ok) 
-		<li>COMPLETE: <a href="/profile/edit">Your preferences</a> indicate you are ok with a random match, so you don't have to rate other profiles. <a href="/profile/compatible?">But you can if you want to</a>.</li>
+		<li>COMPLETE: Your preferences indicate you are ok with a random match, so you don't have to rate other profiles. <a href="/profile/compatible?">But you can if you want to</a>.</li>
 	@else
 		<li><a href="/profile/compatible?">Choose who you'd like to meet</a>.</li>
 	@endif
@@ -14,12 +14,12 @@
 @endif
 @if ($attending_next_event)
 	@if ($matched)
-		<li><a href="/profile/match?event={{ $next_event }}&year={{ $year }}">COMPLETE: YOU ARE AWAITED AT {{ strtoupper($pretty_names[$next_event]) }} {{ $year }}! Find out who you're matched with</a>.</li>
+		<li><b><a class="bright" href="/profile/match?event={{ $next_event }}&year={{ $year }}">COMPLETE: YOU ARE AWAITED AT {{ strtoupper($pretty_names[$next_event]) }} {{ $year }}! Find out who you're matched with.</a></b></li>
 	@else
 		<li>Matches are complete for {{ $pretty_names[$next_event] }} {{ $year }}, but you were not matched. <a href="/profile/match?event={{ $next_event }}&year={{ $year }}">Find out why</a>.</li>
 	@endif
 @else
-	<li>Check back here a few days before the next event you'll be attending to find out who you've been matched with. Let us know what events you'll be attending by <a href="/profile/edit">updating your profile</a>.</li>
+	<li>Let us know what events you'll be attending by <a href="/profile/edit">updating your profile</a>. Check back here a few days before the next event to find out who you've been matched with.</li>
 @endif
 <li>
 	@if ($attending_next_event && $matched)
@@ -29,7 +29,7 @@
 			Did you find your match? <a href="/profile/match?event={{ $next_event }}&year={{ $year }}">Let us know</a>!
 		@endif
 	@else
-		At the event, seek out your match. If you found your match, <a href="/search">let us know that you've met them by updating their profile rating</a>.
+		At the event, seek out your match. When you find your match, <a href="/search">let us know that you've met them</a>.
 	@endif
 </li>
 <li>Find <a href="/profile/Firebird">Firebird</a> to receive your reward.</li>
