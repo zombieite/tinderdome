4b0eca544ab616212eca27cfd474fab45cbe2bdb
zombieite <387685+zombieite@users.noreply.github.com>
2018-08-04 22:18:39 -0700

    link to new photo pile

--- resources/views/photosearch.blade.php
+++ resources/views/photosearch.blade.php
@@ -3,14 +3,10 @@
 @if ($users_who_must_be_rated)
 	<a href="/profile/compatible?" class="bright">You must rate all users before you can view all users</a>.<br><br>
 @else
-	@foreach ($profiles as $profile)
-		@if ($profile['number_photos'])
-			@for ($i = 1; $i <= $profile['number_photos']; $i++)
-				<a href="/profile/{{ $profile['profile_id'] }}/{{ $profile['wasteland_name_hyphenated'] }}">
-					<img src="/uploads/image-{{ $profile['profile_id'] }}-{{ $i }}.jpg" style="height:150px;">
-				</a>
-			@endfor
-		@endif
+	@foreach ($photos as $photo)
+		<a href="/profile/{{ $photo['profile_id'] }}/{{ $photo['wasteland_name_hyphenated'] }}">
+			<img src="/uploads/image-{{ $photo['profile_id'] }}-{{ $photo['number'] }}.jpg" style="height:150px;">
+		</a>
 	@endforeach
 @endif
 @endsection
