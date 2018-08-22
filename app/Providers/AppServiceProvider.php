7102aad978c61f4ae7ca82fdb8d11409cc02b32f
zombieite <387685+zombieite@users.noreply.github.com>
2018-08-22 07:43:15 -0700

    add active user count

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	public function boot()
+	{
+		View::composer('*', function($view) {
+			$active_count_result = DB::select('select count(*) active_count from users where last_active>now()-interval 1 day');
+			$active_count = 0;
+			if ($active_count_result) {
+				$active_count = $active_count_result[0]->active_count;
+			}
+			$view->with('active_count', $active_count);
+		});
+	}
 }
