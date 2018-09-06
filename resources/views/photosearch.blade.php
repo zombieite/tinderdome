92dc54a92f563a32b2184222127c8f01d678cbf7
zombieite <387685+zombieite@users.noreply.github.com>
2018-09-06 08:00:40 -0700

    txt

--- resources/views/photosearch.blade.php
+++ resources/views/photosearch.blade.php
@@ -4,9 +4,7 @@
 	You must <a href="/profile/compatible?">rate all users</a> before you can view all users.<br><br>
 @else
 	@foreach ($photos as $photo)
-		<a href="/profile/{{ $photo['profile_id'] }}/{{ $photo['wasteland_name_hyphenated'] }}">
-			<img src="/uploads/image-{{ $photo['profile_id'] }}-{{ $photo['number'] }}.jpg" style="height:150px;">
-		</a>
+		<a href="/profile/{{ $photo['profile_id'] }}/{{ $photo['wasteland_name_hyphenated'] }}"><img src="/uploads/image-{{ $photo['profile_id'] }}-{{ $photo['number'] }}.jpg" style="height:150px;"></a>
 	@endforeach
 @endif
 @endsection
