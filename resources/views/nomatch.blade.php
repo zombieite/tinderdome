f77e7a02e94758df8f97d53bf095809360b66216
zombieite <387685+zombieite@users.noreply.github.com>
2018-09-19 22:05:18 -0700

    txt

--- resources/views/nomatch.blade.php
+++ resources/views/nomatch.blade.php
@@ -14,6 +14,7 @@
 					<li>You were too picky. If you want to be more likely to get a match at the next event, rate fewer profiles as No, and <a href="/profile/edit">edit your profile</a> to indicate that you are open to a random match.</li>
 					<li>Random chance did not favor you this time. Part of our matching algorithm is pure luck.</li>
 					<li>You signed up late and not enough people had time to rate your profile. If that is the case, you are much more likely to get a match at the next event.</li>
+					<li>You were matched to someone, but one of you dropped out by changing their rating to "I have met them" or "No" before the event began.</li>
 				</ul>
 			</p>
 		@endif
