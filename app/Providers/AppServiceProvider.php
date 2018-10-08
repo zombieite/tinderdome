e5a2f9e267710642d95ac9a751b90ba26b7bb525
zombieite <john_binns@yahoo.com>
2018-10-08 11:58:12 -0700

    show number signups for next event

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,14 +21,26 @@ class AppServiceProvider extends ServiceProvider
 			if ($total_count_result) {
 				$total_count = $total_count_result[0]->total_count;
 			}
-			$wasteland_count_result = DB::select('select count(*) wasteland_count from users where id>10 and attending_wasteland');
-			$wasteland_count = 0;
-			if ($wasteland_count_result) {
-				$wasteland_count = $wasteland_count_result[0]->wasteland_count;
+			$next_event                = null;
+			$year                      = null;
+			$pretty_event_names        = \App\Util::pretty_event_names();
+			$upcoming_events_with_year = \App\Util::upcoming_events_with_year();
+			foreach ($upcoming_events_with_year as $event => $event_year) {
+				$next_event = $event;
+				$year       = $event_year;
+				break;
 			}
-			$view->with('active_count',    $active_count);
-			$view->with('total_count',     $total_count);
-			$view->with('wasteland_count', $wasteland_count);
+			$next_event_count_result = DB::select("select count(*) next_event_count from users where id>10 and attending_$next_event");
+			$next_event_count = 0;
+			if ($next_event_count_result) {
+				$next_event_count = $next_event_count_result[0]->next_event_count;
+			}
+			$view->with('active_count',       $active_count);
+			$view->with('total_count',        $total_count);
+			$view->with('next_event_count',   $next_event_count);
+			$view->with('next_event',         $next_event);
+			$view->with('year',               $year);
+			$view->with('pretty_event_names', $pretty_event_names);
 		});
 	}
 }
