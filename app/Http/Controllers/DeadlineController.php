388b111633137a23110a4fc77a76caadd4ad9673
zombieite <john_binns@yahoo.com>
2018-10-17 23:14:18 -0700

    add save deadline

---
+++ app/Http/Controllers/DeadlineController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Util;
+use Log;
+
+class DeadlineController extends Controller
+{
+	public function save_deadline()
+	{
+		$auth_user             = Auth::user();
+		$auth_user_id          = Auth::id();
+
+		return view('deadline', [
+		]);
+	}
+}
---
+++ resources/views/deadline.blade.php
@@ -0,0 +1,195 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>I swore I wasn't going to take on any missions. But this one was different.</h1>
+
+<img src="/images/fun/save_deadline/article.jpg">
+<br>
+
+<h1>We got more red herrings than a fish market.</h1>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/thomas_kerns_last_known_photo.jpg"><img style="width:200px;" src="/images/fun/save_deadline/thomas_kerns_last_known_photo.jpg"></a>
+<br>
+Last known photo of Deadline, by Thomas Kerns. Presumed taken Wednesday, September 26 based on newspaper in his hand.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_001.jpg"><img src="/images/fun/save_deadline/evidence_001_s.jpg"></a>
+<br>
+10pm Friday, September 28. Suspect known only as "Dweller 6969", Deadline's fraternal twin, was seen cavorting shortly after his brother's disappearance.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_002.jpg"><img src="/images/fun/save_deadline/evidence_002_s.jpg"></a>
+<br>
+This man knows how to cavort.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_003.jpg"><img src="/images/fun/save_deadline/evidence_003_s.jpg"></a>
+<br>
+Found at the crime scene 11pm Friday, September 28. This article was published on Friday, September 28.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_004.jpg"><img src="/images/fun/save_deadline/evidence_004_s.jpg"></a>
+<br>
+Found at the crime scene 11:02pm Friday, September 28. This article was not published. It may have been intended for publication on Saturday, September 29.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_005.jpg"><img src="/images/fun/save_deadline/evidence_005_s.jpg"></a>
+<br>
+Found at the crime scene 11:04pm Friday, September 28. This article was not published. It may have been intended for publication on Saturday, September 29. Did someone kidnap Deadline to keep this article from ever seeing the light of day?
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_006.jpg"><img src="/images/fun/save_deadline/evidence_006_s.jpg"></a>
+<br>
+Found at the crime scene 11:07pm Friday, September 28. This is probably unrelated as it was recently discovered that a Wasteland Communication Corporation employee is actually known by this moniker.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_007.jpg"><img src="/images/fun/save_deadline/evidence_007_s.jpg"></a>
+<br>
+Crime scene overview 11:11pm Friday, September 28.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_008.jpg"><img src="/images/fun/save_deadline/evidence_008_s.jpg"></a>
+<br>
+Crime scene overview 11:18pm Friday, September 28. I do not always agree with the opinions published in the Wastelander, but I promise to remain impartial. Because of this promise I have determined there is no conflict of interest and due to that determination I have ruled myself out as a suspect.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_009.jpg"><img src="/images/fun/save_deadline/evidence_009_s.jpg"></a>
+<br>
+Crime scene 11:30pm Friday, September 28. Signs of a struggle or something, who knows. It's late and I'm getting sleepy. There's a potato with writing on it, maybe that's a clue, I dunno.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_010.jpg"><img src="/images/fun/save_deadline/evidence_010_s.jpg"></a>
+<br>
+Oh Jesus Cocksucking Christ I am not trying to decipher this. Fuck it's like almost midnight or something
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_011.jpg"><img src="/images/fun/save_deadline/evidence_011_s.jpg"></a>
+<br>
+Twinkie on a chair ha ha ha good one someone is clearly fucking with me
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_012.jpg"><img src="/images/fun/save_deadline/evidence_012_s.jpg"></a>
+<br>
+Some kind of old timey machine with blood or ink beside it fuck this I am going to bed
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/ryan_marlett.jpg"><img src="/images/fun/save_deadline/ryan_marlett_s.jpg"></a>
+<br>
+Evidence submission by Ryan Marlett, time unknown.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/crime_scene_notes.jpg"><img src="/images/fun/save_deadline/crime_scene_notes_s.jpg"></a>
+<br>
+Saturday morning, September 29. With a good night's sleep and a clear head, I am back on the case. I'm carrying around a radio and it's a good thing because The Swede just said Deadline was last seen at Civil Defense. Civil Defense publishes a rival newspaper called the Daily Bugle.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_013.jpg"><img src="/images/fun/save_deadline/evidence_013_s.jpg"></a>
+<br>
+Forager Pace of Civil Defense, trying to act innocent.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_014.jpg"><img src="/images/fun/save_deadline/evidence_014_s.jpg"></a>
+<br>
+A barrel with Wes Anderson fonts all over it!
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_015.jpg"><img src="/images/fun/save_deadline/evidence_015_s.jpg"></a>
+<br>
+Warden PeCo of Civil Defense, acting like he doesn't know where Deadline is. This guy can talk. He did mention Eaters and the Killtower Faction. Suspect Trav'lyn Ruze (not pictured) also mentioned "kill tower" so maybe there's something to this.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_016.jpg"><img src="/images/fun/save_deadline/evidence_016_s.jpg"></a>
+<br>
+Cool patch dude but where's Deadline?
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_017.jpg"><img src="/images/fun/save_deadline/evidence_017_s.jpg"></a>
+<br>
+Maybe this is related. It's time for my afternoon nap.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_018.jpg"><img src="/images/fun/save_deadline/evidence_018_s.jpg"></a>
+<br>
+I envy this guy, he gets to sleep forever!
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/evidence_019.jpg"><img src="/images/fun/save_deadline/evidence_019_s.jpg"></a>
+<br>
+Civil Defense told me to talk to suspects Digits and Doc, pictured. I was told to come back after the swimsuit competition. I took a nap and came back and these two told me to go talk to Civil Defense. Ah good, a few hours wasted.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+Someone at the WCC said the Rust Devils threw a grenade at them. I brought along Mad Skelli for security and I interviewed the Rust Devils under the pseudonym Thunderbird since they're no friends of mine for unrelated reasons. That interview also yielded nothing.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+Mad Skelli then asked some random person, "Where's Deadline?" They said he was in Vault 77. But no one would tell us where that was. After threatening to shoot a few people in the kneecaps, Mad Skelli found out that the secret Vault 77 was either inside or around the Mushroom Cloud Lounge. But at this point it was Sunday. The Mushroom Cloud lounge had vanished from the site, along with the entire Gathering of the Tribes compound.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+On the evening of Sunday Oct 7, someone claiming to be Deadline signed up for a You Are Awaited mission at the Wastelanders Ball on January 5. It's quite possible Dweller 6969 is impersonating his brother again. If Deadline himself does show up, the next question will be: Will he have copies of the never-printed Wastelander vol. II no. 4? I'm dying to find out if the First Feral Family had sex with mutants. 
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+Around noon on Oct 10, a member of the WCC who wished not to be named casually mentioned "a slogan for the newspaper next year." This implies that he knows the newspaper is coming back. How he knows, and who will be a the helm, is still anyone's guess.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/deadline_2point0.jpg"><img src="/images/fun/save_deadline/deadline_2point0_s.jpg"></a>
+<br>
+On Oct 13, for the purposes of maintaining order and spelling accuracy at The Wastelander, I whipped up a semi-interactive algorithm called Deadline 2.0. I don't know how helpful it's going to be but we have to do <i>something</i>.
+</div>
+
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/submission_1.jpg"><img src="/images/fun/save_deadline/submission_1_s.jpg"></a>
+<br>
+On Oct 17, I received three anonymous submissions from someone named Kate. This one clearly shows some suspects contaminating my crime scene.
+</div>
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/submission_2.jpg"><img src="/images/fun/save_deadline/submission_2_s.jpg"></a>
+Another anonymous submission from Kate.
+</div>
+<div class="profile_search_block" style="width:200px;vertical-align:top;">
+<a target="_blank" href="/images/fun/save_deadline/submission_3.jpg"><img src="/images/fun/save_deadline/submission_3_s.jpg"></a>
+Yet another anonymous submission from the anonymous Kate.
+</div>
+
+<h1>Theories</h1>
+<div class="profile_search_block">
+One theory is he got sick of doing the Wastelander and wanted a night off to do karaoke, so he kidnapped himself and faked his own death and came back as his own evil twin.
+<br><br>
+"Is he alive? Dead? Chained to a desk at the Bunker Bugle?"--Deadline 2.0
+<br><br>
+"Dweller 6969 has nothing to do with this."--Dweller 6969
+<br><br>
+Another theory is that someone spilled a drink on Deadline's typewriter, rendering it useless, and the WCC decided to quickly come up with a more elaborate explanation that did not make them seem like they're a bunch of bumbling dolts.
+<br><br>
+Trav'lyn Ruze: "I keep saying that deadline is time traveling rn, but no one will listen."<br>
+Firebird: "How do you know this?"<br>
+Trav'lyn Ruze: "...kill tower."
+</div>
+
+@endsection
