d10a02e73e9dfec5fe71080e18844bbad8dcce61
zombieite <387685+zombieite@users.noreply.github.com>
2018-11-16 11:51:30 -0700

    txt

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -70,7 +70,7 @@ I am...
 <label for="hoping_to_find_love">Open to finding a new romantic partner.</label>
 <p style="margin-left: 2em; margin-top: 0em; margin-bottom: 0em;">
 	<input type="checkbox" name="share_info_with_favorites" id="share_info_with_favorites" @guest @else @if ($share_info_with_favorites) checked @endif @endguest>
-	<label for="share_info_with_favorites">Share my email address with mutual "fuck yeah" ratings. You must also check "Open to finding a new romantic partner" above.</label>
+	<label for="share_info_with_favorites">Share my email address with mutual "fuck yeah" ratings.</label> You must also check "Open to finding a new romantic partner" above.
 </p>
 
 <hr>
