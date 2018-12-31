73157951b5839d49d97b76ac76298ff4a843b402
zombieite <387685+zombieite@users.noreply.github.com>
2018-12-30 23:08:15 -0700

    txt

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -18,7 +18,7 @@
 	<br>
 @else
 	@if (!$is_me && $unchosen_user_id != 1 )
-		<h3>Would you enjoy meeting this wastelander? @if ($count_left)({{$count_left}} profiles left to view) @endif</h3>
+		<h3>Would you enjoy meeting this user? @if ($count_left)({{$count_left}} profiles left to view) @endif</h3>
 		@include('rating_form', ['action' => '/profile/compatible?', 'user_id_to_rate' => $unchosen_user_id, 'current_choice' => $choice])
 	@endif
 	@if ($missions_completed['points'])
