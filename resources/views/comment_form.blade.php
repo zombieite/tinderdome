e41bb6e1760b7fc25b467ebb72a686a0702eabc0
zombieite <387685+zombieite@users.noreply.github.com>
2018-12-31 20:01:04 -0700

    txt

--- resources/views/comment_form.blade.php
+++ resources/views/comment_form.blade.php
@@ -1,7 +1,7 @@
 <form action="{{ $action }}" method="POST">
     {{ csrf_field() }}
     <input type="hidden" name="commented_upon_user_id" value="{{ $user_id_to_rate }}">
-    <label for="comment">You know each other. You can comment on their profile. Comments must be approved. All comments are deleted after one year.</label>
+    <label for="comment">You can comment on this profile. Comments must be approved. All comments are deleted after one year.</label>
     <br>
     <input type="text" name="comment" size="140" maxlength="140">
     <input type="submit" value="Submit comment" class="yesyes">
