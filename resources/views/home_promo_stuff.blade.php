95a6eadaadaca13142bd3016a867a96bcc8ba5e9
John Binns <john_binns@yahoo.com>
2019-06-01 23:25:23 -0700

    txt

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -24,7 +24,7 @@
                         @endforeach
                     @else
 --}}
-                        <iframe style="width:100%;max-width:720px;height:480px" src="https://www.youtube.com/embed/kdXWJ4crKkE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                        <iframe style="width:100%;max-width:720px;height:480px" src="https://www.youtube.com/embed/C1KfykeThdI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 {{--
                     @endif
                 @endif
