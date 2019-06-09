f834ff342ece112cf5c1838836c6ffc20e659d51
zombieite <387685+zombieite@users.noreply.github.com>
2019-06-09 12:25:56 -0700

    txt

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 // Visible to everyone
 Route::get( '/',                                     'HomeController@index');
 Route::get( '/save-deadline',                        'DeadlineController@save_deadline');
-Route::get( '/404',                                  'DeadlineController@fourohfour');
+Route::get( '/awaited-nonfictional-delusion',        'AwaitedNonfictionalDelusionController@awaited_nonfictional_delusion');
 Route::get( 'profile/Firebird',                      'ProfileController@showFirebird');
 
 // Visible to logged in only ( add ->middleware('auth'); )
