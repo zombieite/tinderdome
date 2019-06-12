a09970164fde529a958338c8e057c2118394fa59
zombieite <john_binns@yahoo.com>
2019-06-12 09:29:56 -0700

    txt

--- resources/views/awaited.blade.php
+++ resources/views/awaited.blade.php
@@ -3,15 +3,25 @@
 @section('content')
 
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-000008.txt">http://youareawaited.com/txt/awaited_nonfictional_delusion-000008.txt</a><br>
+<br><br>
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-002502.txt">http://youareawaited.com/txt/awaited_nonfictional_delusion-002502.txt</a><br>
+<br><br>
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003012.txt">http://youareawaited.com/txt/awaited_nonfictional_delusion-003012.txt</a><br>
+<br><br>
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003509.txt">http://youareawaited.com/txt/awaited_nonfictional_delusion-003509.txt</a><br>
+<br><br>
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003605.txt">http://youareawaited.com/txt/awaited_nonfictional_delusion-003605.txt</a><br>
+<br><br>
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003609.txt">http://youareawaited.com/txt/awaited_nonfictional_delusion-003609.txt</a><br>
+<br><br>
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003702.txt">http://youareawaited.com/txt/awaited_nonfictional_delusion-003702.txt</a><br>
+<br><br>
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003705.txt">http://youareawaited.com/txt/awaited_nonfictional_delusion-003705.txt</a><br>
+<br><br>
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003709.txt">http://youareawaited.com/txt/awaited_nonfictional_delusion-003709.txt</a><br>
+<br><br>
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003712.txt">http://youareawaited.com/txt/awaited_nonfictional_delusion-003712.txt</a><br>
+<br><br>
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003809.txt">http://youareawaited.com/txt/awaited_nonfictional_delusion-003809.txt</a><br>
 
 @endsection
