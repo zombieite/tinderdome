9121c3dfaa4dd4cb86c8bed72adbaa2958ca520e
zombieite <387685+zombieite@users.noreply.github.com>
2019-07-06 16:42:31 -0700

    txt

--- resources/views/awaited.blade.php
+++ resources/views/awaited.blade.php
@@ -6,22 +6,24 @@
 <br>
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-002502.txt">Chapter 2: 002502</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003012.txt">Chapter 3: 003012</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-002503.txt">Chapter 3: 002503</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003509.txt">Chapter 4: 003509</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003012.txt">Chapter 4: 003012</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003605.txt">Chapter 5: 003605</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003509.txt">Chapter 5: 003509</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003609.txt">Chapter 6: 003609</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003605.txt">Chapter 6: 003605</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003702.txt">Chapter 7: 003702</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003609.txt">Chapter 7: 003609</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003705.txt">Chapter 8: 003705</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003702.txt">Chapter 8: 003702</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003709.txt">Chapter 9: 003709</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003705.txt">Chapter 9: 003705</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003712.txt">Chapter 10: 003712</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003709.txt">Chapter 10: 003709</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003809.txt">Chapter 11: 003809</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003712.txt">Chapter 11: 003712</a><br>
+<br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003809.txt">Chapter 12: 003809</a><br>
 
 @endsection
