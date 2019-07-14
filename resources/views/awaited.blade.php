8ea2db14fc6e68d98447b70e62b08a9ad976cdcc
zombieite <387685+zombieite@users.noreply.github.com>
2019-07-14 08:54:22 -0700

    txt

--- resources/views/awaited.blade.php
+++ resources/views/awaited.blade.php
@@ -4,26 +4,26 @@
 
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-000000.txt">Chapter 00: 000000</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-002501.txt">Chapter 01: 002501</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-012501.txt">Chapter 01: 012501</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-002502.txt">Chapter 02: 002502</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-022502.txt">Chapter 02: 022502</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003011.txt">Chapter 03: 003011</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-033011.txt">Chapter 03: 033011</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003508.txt">Chapter 04: 003508</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-043508.txt">Chapter 04: 043508</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003604.txt">Chapter 05: 003604</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-053604.txt">Chapter 05: 053604</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003608.txt">Chapter 06: 003608</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-063608.txt">Chapter 06: 063608</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003701.txt">Chapter 07: 003701</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-073701.txt">Chapter 07: 073701</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003704.txt">Chapter 08: 003704</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-083704.txt">Chapter 08: 083704</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003708.txt">Chapter 09: 003708</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-093708.txt">Chapter 09: 093708</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003711.txt">Chapter 10: 003711</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-103711.txt">Chapter 10: 103711</a><br>
 <br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-003808.txt">Chapter 11: 003808</a><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-113808.txt">Chapter 11: 113808</a><br>
 
 @endsection
