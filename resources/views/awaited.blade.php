889c2cb304f674cb2639d028e0477d93d4dd5e7a
zombieite <387685+zombieite@users.noreply.github.com>
2019-08-31 11:49:16 -0700

    txt

--- resources/views/awaited.blade.php
+++ resources/views/awaited.blade.php
@@ -2,32 +2,63 @@
 
 @section('content')
 
-<h1><a href="http://www.lulu.com/shop/john-binns/awaited-nonfictional-delusion/paperback/product-24218988.html">Awaited: Nonfictional Delusion</a></h1>
+<img style="width:100%" src="http://youareawaited.com/images/awaited-nonfictional-delusion/awaited-nonfictional-delusion.jpg">
 
-<iframe style="width:100%;max-width:720px;height:480px" src="https://www.youtube.com/embed/zQqjvO-useM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/zQqjvO-useM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
 <p>
 What if, by going to Wasteland Weekend, you really could enter another world? Too implausible to be an autobiography. Too real to be fiction. It's much more than just a story about Wasteland Weekend and Uranium Springs. It's a missing instruction manual for life. This book will help you make friends. This book will help you make money. This book might even save your life. Two years in the making, this tiny, 143-page book has been meticulously engineered to evoke a sense of hope during the apocalypse. It will inspire you to change your life and change the world. Anarchy, attachment, cars, economics, entrepreneurship, entropy, ethics, grief, guilt, guns, kindness, self-reliance, Seventies nostalgia, and stoicism. This is the You Are Awaited story. Awaited: Nonfictional Delusion, by John Binns. 
 </p>
 
-<h1><a href="http://www.lulu.com/shop/john-binns/awaited-nonfictional-delusion/paperback/product-24218988.html">Buy a paperback copy</a></h1>
-
-<img style="width:100%" src="http://youareawaited.com/images/awaited-nonfictional-delusion/awaited-nonfictional-delusion.jpg">
+<h1><a href="http://www.lulu.com/shop/john-binns/awaited-nonfictional-delusion/paperback/product-24218988.html">Buy paperback</a></h1>
 
 <h1><a href="/images/fun/awaited-nonfictional-delusion/awaited-nonfictional-delusion.pdf">pdf</a></h1>
 
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-000000.txt">Chapter 00: 000000</a><br><br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-012501.txt">Chapter 01: 012501</a> (Be sure to watch the <a href="https://www.youtube.com/watch?v=zQqjvO-useM">film version of scene II</a>)<br><br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-022502.txt">Chapter 02: 022502</a><br><br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-033011.txt">Chapter 03: 033011</a><br><br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-043508.txt">Chapter 04: 043508</a><br><br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-053604.txt">Chapter 05: 053604</a><br><br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-063608.txt">Chapter 06: 063608</a><br><br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-073701.txt">Chapter 07: 073701</a><br><br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-083704.txt">Chapter 08: 083704</a><br><br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-093708.txt">Chapter 09: 093708</a><br><br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-103711.txt">Chapter 10: 103711</a><br><br>
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-113808.txt">Chapter 11: 113808</a><br><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-000000.txt">Chapter 00</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/00.mp3">Chapter 00 (Audiobook)</a>
+<br><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-012501.txt">Chapter 01</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/01.mp3">Chapter 01 (Audiobook)</a><br>
+<a href="https://www.youtube.com/watch?v=zQqjvO-useM">Chapter 01, Scene II (Film)</a>
+<br><br>
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-022502.txt">Chapter 02</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/02.mp3">Chapter 02 (Audiobook)</a>
+<br><br>
 
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-033011.txt">Chapter 03</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/03.mp3">Chapter 03 (Audiobook)</a>
+<br><br>
+
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-043508.txt">Chapter 04</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/04.mp3">Chapter 04 (Audiobook)</a>
+<br><br>
+
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-053604.txt">Chapter 05</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/05.mp3">Chapter 05 (Audiobook)</a>
+<br><br>
+
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-063608.txt">Chapter 06</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/06.mp3">Chapter 06 (Audiobook)</a>
+<br><br>
+
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-073701.txt">Chapter 07</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/07.mp3">Chapter 07 (Audiobook)</a>
+<br><br>
+
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-083704.txt">Chapter 08</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/08.mp3">Chapter 08 (Audiobook)</a>
+<br><br>
+
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-093708.txt">Chapter 09</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/09.mp3">Chapter 09 (Audiobook)</a>
+<br><br>
+
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-103711.txt">Chapter 10</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/10.mp3">Chapter 10 (Audiobook)</a>
+<br><br>
+
+<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-113808.txt">Chapter 11</a><br>
+<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/11.mp3">Chapter 11 (Audiobook)</a>
+<br><br>
 
 @endsection
