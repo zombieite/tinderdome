615a8d7412a44de1eff82d87f7ab884a5697f8b5
zombieite <387685+zombieite@users.noreply.github.com>
2019-08-31 19:48:21 -0700

    txt

---
+++ resources/views/home_promo_stuff.blade.php
@@ -0,0 +1,4 @@
+  <iframe style="width:100%;max-width:720px;height:480px" src="https://www.youtube.com/embed/zQqjvO-useM" frameborder="0" allow="autoplay    ; encrypted-media" allowfullscreen></iframe>
+  <h1><a href="https://www.indiegogo.com/projects/you-are-awaited-film-2020#/">CROWDFUND THE FILM</a></h1>
+  <h1><a href="/awaited-nonfictional-delusion">READ THE BOOK OR LISTEN TO THE AUDIOBOOK FOR FREE</a></h1>
+  <h1>Read on for mission details...</h1>
