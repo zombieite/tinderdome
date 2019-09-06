99bbee3f08dbb7a9d13d11a9be339d35e0424cb3
zombieite <387685+zombieite@users.noreply.github.com>
2019-09-06 07:27:29 -0700

    txt

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,2 +1,3 @@
-  <iframe style="width:100%;max-width:720px;height:480px" src="https://www.youtube.com/embed/zQqjvO-useM" frameborder="0" allow="autoplay    ; encrypted-media" allowfullscreen></iframe>
-  <h1><a href="https://www.indiegogo.com/projects/you-are-awaited-film-2020#/">CROWDFUND THE 2020 FILM</a>. <a href="/awaited-nonfictional-delusion">READ THE BOOK</a>.</h1>
+<iframe style="width:100%;max-width:720px;height:480px" src="https://www.youtube.com/embed/zQqjvO-useM" frameborder="0" allow="autoplay    ; encrypted-media" allowfullscreen></iframe>
+<h1><a href="https://www.indiegogo.com/projects/you-are-awaited-film-2020#/">Everyone donate $3 to the 2020 film and we'll hit our goal</a></h1>
+<h1><a href="/awaited-nonfictional-delusion">Read the You Are Awaited/Save Deadline story</a></h1>
