957c9814b39c30137c0a75072b8c2c64b93c261b
zombieite <john_binns@yahoo.com>
2019-11-04 09:30:13 -0700

    kindle

--- resources/views/awaited.blade.php
+++ resources/views/awaited.blade.php
@@ -12,9 +12,10 @@ What if, by going to Wasteland Weekend, you really could enter another world? To
 
 <h1><a href="http://www.lulu.com/shop/john-binns/awaited-nonfictional-delusion/paperback/product-24218988.html">Buy the paperback</a></h1>
 
-<h1><a href="/images/fun/awaited-nonfictional-delusion/awaited-nonfictional-delusion.pdf">Download the pdf</a></h1>
+<!--<h1><a href="/images/fun/awaited-nonfictional-delusion/awaited-nonfictional-delusion.pdf">Download the pdf</a></h1>-->
+<h1><a href="https://www.amazon.com/Awaited-Nonfictional-Delusion-John-Binns-ebook/dp/B07ZYBBCGF/">Kindle eBook</a>
 
-<h1>Download the text files or audiobook files (200 minutes in total):</h1>
+<h1>Download sample text files or audiobook files (200 minutes in total):</h1>
 
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
 <a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-000000.txt">Chapter 00 (txt)</a><br>
@@ -26,7 +27,7 @@ Carpenters - Close To You
 </div>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-012501.txt">Chapter 01 (txt)</a><br>
+<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-012501.txt">Chapter 01 (txt)</a><br>-->
 <a href="http://youareawaited.com/images/awaited-nonfictional-delusion/01.mp3">Chapter 01 (mp3)</a><br>
 <div class="small">
 Sex Pistols - Anarchy In The UK Demo<br>
@@ -36,11 +37,11 @@ Sheldon Allman - Crawl Out Through The Fallout<br>
 <a href="https://www.youtube.com/watch?v=zQqjvO-useM">Chapter 01, Scene II (Film)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-022502.txt">Chapter 02 (txt)</a><br>
+<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-022502.txt">Chapter 02 (txt)</a><br>-->
 <a href="http://youareawaited.com/images/awaited-nonfictional-delusion/02.mp3">Chapter 02 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-033011.txt">Chapter 03 (txt)</a><br>
+<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-033011.txt">Chapter 03 (txt)</a><br>-->
 <a href="http://youareawaited.com/images/awaited-nonfictional-delusion/03.mp3">Chapter 03 (mp3)</a>
 <div class="small">
 Jonathan Richman - Road Runner<br>
@@ -50,27 +51,27 @@ Peter Gabriel and Kate Bush - Don't Give Up
 </div>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-043508.txt">Chapter 04 (txt)</a><br>
+<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-043508.txt">Chapter 04 (txt)</a><br>-->
 <a href="http://youareawaited.com/images/awaited-nonfictional-delusion/04.mp3">Chapter 04 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-053604.txt">Chapter 05 (txt)</a><br>
+<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-053604.txt">Chapter 05 (txt)</a><br>-->
 <a href="http://youareawaited.com/images/awaited-nonfictional-delusion/05.mp3">Chapter 05 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-063608.txt">Chapter 06 (txt)</a><br>
+<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-063608.txt">Chapter 06 (txt)</a><br>-->
 <a href="http://youareawaited.com/images/awaited-nonfictional-delusion/06.mp3">Chapter 06 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-073701.txt">Chapter 07 (txt)</a><br>
+<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-073701.txt">Chapter 07 (txt)</a><br>-->
 <a href="http://youareawaited.com/images/awaited-nonfictional-delusion/07.mp3">Chapter 07 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-083704.txt">Chapter 08 (txt)</a><br>
+<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-083704.txt">Chapter 08 (txt)</a><br>-->
 <a href="http://youareawaited.com/images/awaited-nonfictional-delusion/08.mp3">Chapter 08 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-093708.txt">Chapter 09 (txt)</a><br>
+<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-093708.txt">Chapter 09 (txt)</a><br>-->
 <a href="http://youareawaited.com/images/awaited-nonfictional-delusion/09.mp3">Chapter 09 (mp3)</a>
 <div class="small">
 Bob Crosby And The Bobcats - Way Back Home<br>
@@ -80,7 +81,7 @@ Ministry - Jesus Built My Hotrod<br>
 </div>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-103711.txt">Chapter 10 (txt)</a><br>
+<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-103711.txt">Chapter 10 (txt)</a><br>-->
 <a href="http://youareawaited.com/images/awaited-nonfictional-delusion/10.mp3">Chapter 10 (mp3)</a>
 <div class="small">
 AC/DC - Back In Black<br>
@@ -88,7 +89,7 @@ Carpenters - Close To You
 </div>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-113808.txt">Chapter 11 (txt)</a><br>
+<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-113808.txt">Chapter 11 (txt)</a><br>-->
 <a href="http://youareawaited.com/images/awaited-nonfictional-delusion/11.mp3">Chapter 11 (mp3)</a>
 <div class="small">
 John Gorka - I'm From New Jersey
