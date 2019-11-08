013fc0b27719ed767ec6c42ec07039c6c03272ca
John Binns <john_binns@yahoo.com>
2019-11-07 23:10:14 -0700

    add new tables

---
+++ sql/create_event.sql
@@ -0,0 +1,10 @@
+USE tinderdome;
+
+CREATE TABLE event (
+	event_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	event_short_name VARCHAR(255) NOT NULL,
+    event_long_name VARCHAR(255) NOT NULL,
+    event_date DATE NOT NULL,
+	created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+alter table event add unique `unique_event_date`(`event`, `event_date`);
