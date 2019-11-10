ed70aa117bbe0e87847ddbe19b62475f16b50663
John Binns <john_binns@yahoo.com>
2019-11-09 21:18:51 -0700

    fixing bugs

--- resources/views/nomatch.blade.php
+++ resources/views/nomatch.blade.php
@@ -1,28 +1,24 @@
 @extends('layouts.app')
 @section('content')
 
-@if ($logged_in_is_signed_up)
-	@if ($matches_done)
-		@if ($deleted_match_or_match_said_no)
-			<h2>You were matched for {{ $pretty_event_names[$event] }} {{ $year }}, but your match has deleted their profile.</h2>
-		@else
-			<h2>We did not find you a match for {{ $pretty_event_names[$event] }} {{ $year }}.</h2>
-			<p>
-				There are a few possible reasons for this.
-				<ul>
-					<li>You already know too many of the people who are attending. We only match you with people you don't know.</li>
-					<li>You were too picky. If you want to be more likely to get a match at the next event, rate fewer profiles as No, and <a href="/profile/edit">edit your profile</a> to indicate that you are open to a random match.</li>
-					<li>Random chance did not favor you this time. Part of our matching algorithm is pure luck.</li>
-					<li>You signed up late and not enough people had time to rate your profile. If that is the case, you are much more likely to get a match at the next event.</li>
-					<li>You signed up after matches had already been run.</li>
-					<li>You were matched to someone, but one of you dropped out before the event began.</li>
-				</ul>
-			</p>
-		@endif
+@if ($matches_done)
+	@if ($deleted_match_or_match_said_no)
+		<h2>You were matched for {{ $event }} {{ $year }}, but your match has deleted their profile.</h2>
 	@else
-		Matches have not been run yet for {{ $pretty_event_names[$event] }} {{ $year }}. Check back a few days before the event.
+		<h2>We did not find you a match for {{ $event }} {{ $year }}.</h2>
+		<p>
+			There are a few possible reasons for this.
+			<ul>
+				<li>You already know too many of the people who are attending. We only match you with people you don't know.</li>
+				<li>You were too picky. If you want to be more likely to get a match at the next event, rate fewer profiles as No, and <a href="/profile/edit">edit your profile</a> to indicate that you are open to a random match.</li>
+				<li>Random chance did not favor you this time. Part of our matching algorithm is pure luck.</li>
+				<li>You signed up late and not enough people had time to rate your profile. If that is the case, you are much more likely to get a match at the next event.</li>
+				<li>You signed up after matches had already been run.</li>
+				<li>You were matched to someone, but one of you dropped out before the event began.</li>
+			</ul>
+		</p>
 	@endif
 @else
-	You are not signed up for {{ $pretty_event_names[$event] }} {{ $year }}. <a href="/profile/edit">Edit your profile</a> to let us know what events you'll be attending.
+	Matches have not been run yet for {{ $event }} {{ $year }}. Check back a few days before the event.
 @endif
 @endsection
