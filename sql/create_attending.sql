2bc9e6a978a4fb742df4fca3c1ad37dfb35e22c3
zombieite <387685+zombieite@users.noreply.github.com>
2019-11-10 17:47:25 -0700

    refactoring matching design

--- sql/create_attending.sql
+++ sql/create_attending.sql
@@ -3,7 +3,9 @@ USE tinderdome;
 CREATE TABLE attending (
     attending_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
+    user_id_of_match INT,
 	event_id INT NOT NULL,
 	created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 alter table attending add unique `unique_user_event`(`user_id`, `event_id`);
+alter table attending add unique `unique_match`(`user_id`, `user_id_of_match`);
