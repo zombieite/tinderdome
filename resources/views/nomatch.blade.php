96c4dce12dbd424cc93c12ea24439ba8a082d58f
zombieite <387685+zombieite@users.noreply.github.com>
2019-11-12 00:08:56 -0700

    txt

--- resources/views/nomatch.blade.php
+++ resources/views/nomatch.blade.php
@@ -5,15 +5,18 @@
 	@if ($deleted_match_or_match_said_no)
 		<h2>You were matched for {{ $event }}, but your match has deleted their profile.</h2>
 	@else
-		<h2>We did not find you a match for {{ $event }}.</h2>
+        @if ($event)
+    		<h2>We did not find you a match for {{ $event }}.</h2>
+        @else
+            <h2>We did not find you a match.</h2>
+        @endif
 		<p>
 			There are a few possible reasons for this.
 			<ul>
 				<li>You already know too many of the people who are attending. We only match you with people you don't know.</li>
 				<li>You were too picky. If you want to be more likely to get a match at the next event, rate fewer profiles as No, and <a href="/profile/edit">edit your profile</a> to indicate that you are open to a random match.</li>
 				<li>Random chance did not favor you this time. Part of our matching algorithm is pure luck.</li>
-				<li>You signed up late and not enough people had time to rate your profile. If that is the case, you are much more likely to get a match at the next event.</li>
-				<li>You signed up after matches had already been run.</li>
+				<li>You signed up too late to be matched at this event.</li>
 				<li>You were matched to someone, but one of you dropped out before the event began.</li>
 			</ul>
 		</p>
