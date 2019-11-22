47f4ba3b65aafb9ef6f5c10beac39df172272c62
zombieite <387685+zombieite@users.noreply.github.com>
2019-11-22 13:32:58 -0800

    match

--- resources/views/match_me.blade.php
+++ resources/views/match_me.blade.php
@@ -12,32 +12,30 @@
     @php $counter = 0; @endphp
     <table>
     <tr>
-            <td>&nbsp;</td>
-            <td>id</td>
-            <td>name</td>
-            <td>email</td>
+            <td>user</td>
+            <td>chooser's random ok</td>
+            <td>chosen's random ok</td>
+            <td>user looking to be matcheds rating of this user</td>
+            <td>this users rating of user looking to be matched</td>
             <td>chooser's desired gender of match</td>
             <td>chosen's gender</td>
             <td>chosen's desired gender of match</td>
             <td>chooser's gender</td>
             <td>score</td>
-            <td>photos</td>
-            <td>greylist</td>
     </tr>
     @foreach ($potential_matches as $potential_match)
         @php $counter++; @endphp
         <tr>
-            <td>{{ $counter }}</td>
-            <td>{{ $potential_match->id }}</td>
-            <td>{{ $potential_match->name }}</td>
-            <td>{{ $potential_match->email }}</td>
+            <td>{{ $counter }}|{{ $potential_match->id }}|{{ $potential_match->name }}</td>
+            <td>{{ $logged_in_user->random_ok }}</td>
+            <td>{{ $potential_match->random_ok }}</td>
+            <td>{{ $potential_match->user_looking_to_be_matcheds_rating_of_this_user }}</td>
+            <td>{{ $potential_match->this_users_rating_of_user_looking_to_be_matched }}</td>
             <td>{{ $potential_match->choosers_desired_gender_of_match }}</td>
             <td>{{ $potential_match->gender }}</td>
             <td>{{ $potential_match->gender_of_match }}</td>
             <td>{{ $logged_in_user->gender }}</td>
             <td>{{ $potential_match->score }}</td>
-            <td>{{ $potential_match->number_photos }}</td>
-            <td>{{ $potential_match->greylist }}</td>
         </tr>
     @endforeach
     </table>
