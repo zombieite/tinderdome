946f0b14d5aa6cd6aad0d93a4b777713b3cd32e6
John Binns <john_binns@yahoo.com>
2019-11-22 14:03:52 -0800

    match

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -17,6 +17,7 @@ class MatchController extends Controller
         $upcoming_events_and_signup_status = \App\Util::upcoming_events_with_pretty_name_and_date_and_signup_status( $logged_in_user );
         $event                             = null;
         $mutual_unmet_matches              = null;
+		$my_match_user_id                       = null;
         foreach ($upcoming_events_and_signup_status as $maybe_event) {
             if ($maybe_event->event_id == $event_id) {
                 $event = $maybe_event;
@@ -46,42 +47,53 @@ class MatchController extends Controller
         if (isset($_POST['matchme'])) {
 			$matchme = $_POST['matchme'];
 
-			// TODO XXX FIXME check if logged in user was already matched to someone when that person got their match earlier
-            // TODO XXX FIXME filter out logged in user nos&knows and potential match nos&knows
-			// TODO XXX FIXME filter out users who have not rated chooser if those users said not ok with random match
-			$left_maybe = $logged_in_user->random_ok ? 'left' : '';
-            $mutual_unmet_matches = DB::select("
-                select
-                    users.id,
-                    name,
-                    email,
-                    gender,
-                    gender_of_match,
-                    score,
-					random_ok,
-                    number_photos,
-                    greylist,
-					c1.choice user_looking_to_be_matcheds_rating_of_this_user,
-					c2.choice this_users_rating_of_user_looking_to_be_matched
-                from
-                    users
-                    join attending on (users.id = attending.user_id and attending.user_id_of_match is null and attending.event_id = ?)
-					$left_maybe join choose c1 on (users.id = c1.chosen_id and c1.chooser_id = ?)
-					left join choose c2 on (users.id = c2.chooser_id and c1.chosen_id = ?)
-                where
-                    users.id > 10
-					and (users.random_ok = 1 or c2.choice is not null)
-					and (c1.choice is null or c1.choice > 0)
-					and (c2.choice is null or c2.choice > 0)
-                    and users.id != ?
-            ", [$event_id, $logged_in_user_id, $logged_in_user_id, $logged_in_user_id]);
-            foreach ($mutual_unmet_matches as $match) {
-                $match->choosers_desired_gender_of_match = $logged_in_user->gender_of_match;
-                $match->gender_of_chooser                = $logged_in_user->gender;
-            }
+			$someone_else_claimed_me_row = DB::select('select * from attending where event_id = ? and user_id_of_match = ?', [$event_id, $logged_in_user_id]);
+			if ($someone_else_claimed_me_row) {
+				$my_match_user_id = array_shift($someone_else_claimed_me_row)->user_id;
+			} else {
+				$i_already_requested_and_got_matched_row = DB::select('select * from attending where event_id = ? and user_id = ? and user_id_of_match is not null', [$event_id, $logged_in_user_id]);
+				if ($i_already_requested_and_got_matched_row) {
+					$my_match_user_id = array_shift($i_already_requested_and_got_matched_row)->user_id_of_match;
+				}
+			}
 
-            // Where the magic happens
-            usort($mutual_unmet_matches, array($this, 'sort_matches'));
+			if ($my_match_user_id) {
+				// All good
+			} else {
+				$left_maybe = $logged_in_user->random_ok ? 'left' : '';
+				$mutual_unmet_matches = DB::select("
+					select
+						users.id,
+						name,
+						email,
+						gender,
+						gender_of_match,
+						score,
+						random_ok,
+						number_photos,
+						greylist,
+						c1.choice user_looking_to_be_matcheds_rating_of_this_user,
+						c2.choice this_users_rating_of_user_looking_to_be_matched
+					from
+						users
+						join attending on (users.id = attending.user_id and attending.user_id_of_match is null and attending.event_id = ?)
+						$left_maybe join choose c1 on (users.id = c1.chosen_id and c1.chooser_id = ?)
+						left join choose c2 on (users.id = c2.chooser_id and c1.chosen_id = ?)
+					where
+						users.id > 10
+						and (users.random_ok = 1 or c2.choice is not null)
+						and (c1.choice is null or c1.choice > 0)
+						and (c2.choice is null or c2.choice > 0)
+						and users.id != ?
+				", [$event_id, $logged_in_user_id, $logged_in_user_id, $logged_in_user_id]);
+				foreach ($mutual_unmet_matches as $match) {
+					$match->choosers_desired_gender_of_match = $logged_in_user->gender_of_match;
+					$match->gender_of_chooser                = $logged_in_user->gender;
+				}
+
+				// Where the magic happens
+				usort($mutual_unmet_matches, array($this, 'sort_matches'));
+			}
         }
 
         $event_name = $event->event_long_name;
@@ -92,6 +104,7 @@ class MatchController extends Controller
             'event_name'        => $event_name,
             'potential_matches' => $mutual_unmet_matches,
 			'matchme'           => $matchme,
+			'my_match_user_id'  => $my_match_user_id,
         ]);
     }
 
