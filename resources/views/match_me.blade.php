e9246fda59ab19a22cf83ffb64a6289c2bc9c6ae
zombieite <387685+zombieite@users.noreply.github.com>
2019-11-22 19:05:46 -0800

    match

--- resources/views/match_me.blade.php
+++ resources/views/match_me.blade.php
@@ -13,11 +13,10 @@
     <table>
     <tr>
             <td>user</td>
-            <td>potential match is hoping to find love (liuser: {{ $logged_in_user->hoping_to_find_love }})</td>
             <td>chosen's random ok (liuser: {{ $logged_in_user->random_ok }})</td>
             <td>user looking to be matcheds rating of this user</td>
             <td>this users rating of user looking to be matched</td>
-            <td>chosen's gender (liuser's pref: $logged_in_user->gender_of_match)</td>
+            <td>chosen's gender (liuser's pref: {{ $logged_in_user->gender_of_match }})</td>
             <td>chosen's desired gender of match (liuser's gender: {{ $logged_in_user->gender }})</td>
             <td>score</td>
     </tr>
@@ -25,7 +24,6 @@
         @php $counter++; @endphp
         <tr>
             <td @if ($my_match_user_id == $potential_match->user_id) class="bright" @endif >{{ $counter }}|{{ $potential_match->user_id }}|{{ $potential_match->name }}</td>
-            <td>{{ $potential_match->hoping_to_find_love }}</td>
             <td>{{ $potential_match->random_ok }}</td>
             <td>{{ $potential_match->user_looking_to_be_matcheds_rating_of_this_user }}</td>
             <td>{{ $potential_match->this_users_rating_of_user_looking_to_be_matched }}</td>
