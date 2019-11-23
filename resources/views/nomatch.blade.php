c9dca47ad108921f1e6aefe7c1c6bce73980fb22
zombieite <387685+zombieite@users.noreply.github.com>
2019-11-23 15:51:26 -0800

    txt

--- resources/views/nomatch.blade.php
+++ resources/views/nomatch.blade.php
@@ -3,7 +3,7 @@
 
 @if ($matches_done)
 	@if ($deleted_match_or_match_said_no)
-		<h2>You were matched for {{ $event }}, but your match has deleted their profile.</h2>
+		<h2>You were matched for {{ $event }}, and your match marked you as found.</h2>
 	@else
         @if ($event)
     		<h2>We have not yet found you a match for {{ $event }}.</h2>
