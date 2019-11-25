638a29128c05ba0b2250e2a166b3a7acfba8b814
zombieite <387685+zombieite@users.noreply.github.com>
2019-11-25 01:53:25 -0700

    match

--- resources/views/match_me.blade.php
+++ resources/views/match_me.blade.php
@@ -4,7 +4,11 @@
     @if ($my_match_user_id)
         <a href="/profile/match?event_id={{ $event_id }}">You now have a match for {{ $event_name }}!</a>
     @else
-        You do not yet have a match. You may still get a match, because new people are signing up and they could be matched to you. Check back later!
+        We could not yet find you a match. You may still get a match, because new people are signing up and they could be matched to you. Try again later.
+        @if ($logged_in_user->random_ok)
+        @else
+            To increase your chances, go to <a href="/profile/edit">Edit Profile</a> and set your preference to "Open to a random match."
+        @endif
     @endif
 @else
     Click to be matched for {{ $event_name }}.
