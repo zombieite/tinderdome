90819e6def71848fcdd80252c8570be4d2b9246c
John Binns <john_binns@yahoo.com>
2019-11-26 21:34:48 -0700

    match

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -191,51 +191,45 @@
     @foreach ($matched_to_users as $matched_to_user)
         @if ($matched_to_user->logged_in_users_rating_of_this_user === 0)
             @include('user_block_found_not_liked', [
-                'event_long_name'                      => $matched_to_user->event_long_name,
+                'event_long_name'                  => $matched_to_user->event_long_name,
             ])
         @else
-            @if ($matched_to_user->this_users_rating_of_logged_in_user === 0)
-                @include('user_block_found_but_deleted', [
-                    'event_long_name'                  => $matched_to_user->event_long_name,
-                    'event_id'                         => $matched_to_user->event_id,
-                ])
+            @if ($matched_to_user->logged_in_users_rating_of_this_user === -1)
+                @if (($matched_to_user->this_users_rating_of_logged_in_user === 0) or (!$matched_to_user->name))
+                    @include('user_block_found_but_deleted', [
+                        'event_long_name'          => $matched_to_user->event_long_name,
+                        'event_id'                 => $matched_to_user->event_id,
+                    ])
+                @else
+                    @include('user_block_found', [
+                        'number_photos'            => $matched_to_user->number_photos,
+                        'url'                      => $matched_to_user->url,
+                        'user_id'                  => $matched_to_user->id,
+                        'name'                     => $matched_to_user->name,
+                        'event_long_name'          => $matched_to_user->event_long_name,
+                    ])
+                @endif
             @else
                 @if ($matched_to_user->name)
-                    @if ($matched_to_user->logged_in_users_rating_of_this_user === -1)
-                        @include('user_block_found', [
-                            'number_photos'            => $matched_to_user->number_photos,
-                            'url'                      => $matched_to_user->url,
-                            'user_id'                  => $matched_to_user->id,
-                            'name'                     => $matched_to_user->name,
-                            'event_long_name'          => $matched_to_user->event_long_name,
-                        ])
-                    @else
-                        @include('user_block_not_found_yet', [
-                            'number_photos'            => $matched_to_user->number_photos,
-                            'url'                      => $matched_to_user->url,
-                            'user_id'                  => $matched_to_user->id,
-                            'name'                     => $matched_to_user->name,
-                            'event_long_name'          => $matched_to_user->event_long_name,
-                            'ok_to_delete_old_mission' => $matched_to_user->ok_to_delete_old_mission,
-                            'event_id'                 => $matched_to_user->event_id,
-                            'ok_to_mark_user_found'    => $matched_to_user->ok_to_mark_user_found,
-                        ])
-                    @endif
+                    @include('user_block_not_found_yet', [
+                        'number_photos'            => $matched_to_user->number_photos,
+                        'url'                      => $matched_to_user->url,
+                        'user_id'                  => $matched_to_user->id,
+                        'name'                     => $matched_to_user->name,
+                        'event_long_name'          => $matched_to_user->event_long_name,
+                        'ok_to_delete_old_mission' => $matched_to_user->ok_to_delete_old_mission,
+                        'event_id'                 => $matched_to_user->event_id,
+                        'ok_to_mark_user_found'    => $matched_to_user->ok_to_mark_user_found,
+                    ])
                 @else
-                    @if ($matched_to_user->choice === -1)
-                        @include('user_block_found_but_deleted', [
-                            'event_long_name'          => $matched_to_user->event_long_name,
-                            'event_id'                 => $matched_to_user->event_id,
-                        ])
-                    @else
-                        @include('user_block_matched_to_deleted', [
-                            'event_long_name'          => $matched_to_user->event_long_name,
-                            'event_id'                 => $matched_to_user->event_id,
-                        ])
-                    @endif
+                    @include('user_block_matched_to_deleted', [
+                        'event_long_name'          => $matched_to_user->event_long_name,
+                        'event_id'                 => $matched_to_user->event_id,
+                    ])
                 @endif
             @endif
         @endif
     @endforeach
 @endif
+
 @endsection
