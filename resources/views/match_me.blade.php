5ae0079d7e0b4a5ea3faf0897972e82e8c3b34dd
zombieite <387685+zombieite@users.noreply.github.com>
2019-11-27 07:50:18 -0700

    throttle re-attempts at getting a match

--- resources/views/match_me.blade.php
+++ resources/views/match_me.blade.php
@@ -4,17 +4,23 @@
     @if ($my_match_user_id)
         <a href="/profile/match?event_id={{ $event_id }}">You now have a match for {{ $event_name }}!</a>
     @else
-        We could not yet find you a match. You may still get a match, because new people are signing up and they could be matched to you. Try again later.
+        We could not find you a match yet. You may still get a match, because new people are signing up and they could be matched to you. Try again later.
         @if ($logged_in_user->random_ok)
         @else
             To increase your chances, go to <a href="/profile/edit">Edit Profile</a> and set your preference to "Open to a random match."
         @endif
     @endif
 @else
-    Click to be matched for {{ $event_name }}.
-    <form action="" method="POST">
-        {{ csrf_field() }}
-        <input type="submit" name="matchme" value="TRUST THE ALGORITHM" class="yesyesyes">
-    </form>
+    @if ($minutes_until_can_resubmit == 1)
+        You can retry the matching algorithm in {{ $minutes_until_can_resubmit }} minute.
+    @elseif ($minutes_until_can_resubmit)
+        You can retry the matching algorithm in {{ $minutes_until_can_resubmit }} minutes.
+    @else
+        Click to be matched for {{ $event_name }}.
+        <form action="" method="POST">
+            {{ csrf_field() }}
+            <input type="submit" name="matchme" value="TRUST THE ALGORITHM" class="yesyesyes">
+        </form>
+    @endif
 @endif
 @endsection
