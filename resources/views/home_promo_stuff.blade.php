d62cc268ee691d8414d37bc96480b7ea86cf72c1
zombieite <387685+zombieite@users.noreply.github.com>
2019-11-28 12:02:39 -0700

    ads

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,5 +1,7 @@
 <a href="/awaited-nonfictional-delusion"><img src="/images/stuff/awaited-nonfictional-delusion.jpg"></a>
 <a href="https://www.youtube.com/watch?v=pMKM1d0IsNs"><img src="/images/stuff/awaited-youtube.jpg"></a>
 <a href="https://www.youtube.com/watch?v=zQqjvO-useM"><img src="/images/stuff/roadblock-youtube.jpg"></a>
+<a href="https://www.facebook.com/thewastelander/"><img src="/images/stuff/wastelander.jpg"></a>
+<a href="https://www.facebook.com/WCCorp/"><img src="/images/stuff/wcc.png"></a>
 <a href="https://cultofcatmeat.com"><img src="/images/stuff/catmeat.png"></a>
 <hr>
