5c360d6299ec4e32f2bb51e28bdefb58a6e68249
John Binns <john_binns@yahoo.com>
2019-12-15 10:32:22 -0700

    https

--- resources/views/awaited.blade.php
+++ resources/views/awaited.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 
-<img style="width:100%" src="http://youareawaited.com/images/awaited-nonfictional-delusion/awaited-nonfictional-delusion.jpg">
+<img style="width:100%" src="/images/awaited-nonfictional-delusion/awaited-nonfictional-delusion.jpg">
 
 <iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/zQqjvO-useM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
 <p>
-What if, by going to Wasteland Weekend, you really could enter another world? Too implausible to be an autobiography. Too real to be fiction. It's much more than just a story about Wasteland Weekend and Uranium Springs. It's a missing instruction manual for life. This book will help you make friends. This book will help you make money. This book might even save your life. Two years in the making, this tiny, 143-page book has been meticulously engineered to evoke a sense of hope during the apocalypse. It will inspire you to change your life and change the world. Anarchy, attachment, cars, economics, entrepreneurship, entropy, ethics, grief, guilt, guns, kindness, self-reliance, Seventies nostalgia, and stoicism. This is the You Are Awaited story. Awaited: Nonfictional Delusion, by John Binns. 
+What if, by going to Wasteland Weekend, you really could enter another world? Too implausible to be an autobiography. Too real to be fiction. It's much more than just a story about Wasteland Weekend and Uranium Springs. It's a missing instruction manual for life. This book will help you make friends. This book will help you make money. This book might even save your life. Two years in the making, this tiny, 143-page book has been meticulously engineered to evoke a sense of hope during the apocalypse. It will inspire you to change your life and change the world. Anarchy, attachment, cars, economics, entrepreneurship, entropy, ethics, grief, guilt, guns, kindness, self-reliance, Seventies nostalgia, and stoicism. This is the You Are Awaited story. Awaited: Nonfictional Delusion, by John Binns.
 </p>
 
 <h1><a href="http://www.lulu.com/shop/john-binns/awaited-nonfictional-delusion/paperback/product-24218988.html">Buy the paperback</a></h1>
@@ -18,8 +18,8 @@ What if, by going to Wasteland Weekend, you really could enter another world? To
 <h1>Download sample text files or audiobook files (200 minutes in total):</h1>
 
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-000000.txt">Chapter 00 (txt)</a><br>
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/00.mp3">Chapter 00 (mp3)</a><br>
+<a href="/txt/awaited_nonfictional_delusion-000000.txt">Chapter 00 (txt)</a><br>
+<a href="/images/awaited-nonfictional-delusion/00.mp3">Chapter 00 (mp3)</a><br>
 <div class="small">
 Hymie's Basement - 21st Century Pop Song<br>
 AC/DC - Back In Black<br>
@@ -27,8 +27,8 @@ Carpenters - Close To You
 </div>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-012501.txt">Chapter 01 (txt)</a><br>-->
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/01.mp3">Chapter 01 (mp3)</a><br>
+<!--<a href="/txt/awaited_nonfictional_delusion-012501.txt">Chapter 01 (txt)</a><br>-->
+<a href="/images/awaited-nonfictional-delusion/01.mp3">Chapter 01 (mp3)</a><br>
 <div class="small">
 Sex Pistols - Anarchy In The UK Demo<br>
 Sheldon Allman - Crawl Out Through The Fallout<br>
@@ -37,12 +37,12 @@ Sheldon Allman - Crawl Out Through The Fallout<br>
 <a href="https://www.youtube.com/watch?v=zQqjvO-useM">Chapter 01, Scene II (Film)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-022502.txt">Chapter 02 (txt)</a><br>-->
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/02.mp3">Chapter 02 (mp3)</a>
+<!--<a href="/txt/awaited_nonfictional_delusion-022502.txt">Chapter 02 (txt)</a><br>-->
+<a href="/images/awaited-nonfictional-delusion/02.mp3">Chapter 02 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-033011.txt">Chapter 03 (txt)</a><br>-->
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/03.mp3">Chapter 03 (mp3)</a>
+<!--<a href="/txt/awaited_nonfictional_delusion-033011.txt">Chapter 03 (txt)</a><br>-->
+<a href="/images/awaited-nonfictional-delusion/03.mp3">Chapter 03 (mp3)</a>
 <div class="small">
 Jonathan Richman - Road Runner<br>
 Blondie - Heart Of Glass<br>
@@ -51,28 +51,28 @@ Peter Gabriel and Kate Bush - Don't Give Up
 </div>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-043508.txt">Chapter 04 (txt)</a><br>-->
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/04.mp3">Chapter 04 (mp3)</a>
+<!--<a href="/txt/awaited_nonfictional_delusion-043508.txt">Chapter 04 (txt)</a><br>-->
+<a href="/images/awaited-nonfictional-delusion/04.mp3">Chapter 04 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-053604.txt">Chapter 05 (txt)</a><br>-->
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/05.mp3">Chapter 05 (mp3)</a>
+<!--<a href="/txt/awaited_nonfictional_delusion-053604.txt">Chapter 05 (txt)</a><br>-->
+<a href="/images/awaited-nonfictional-delusion/05.mp3">Chapter 05 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-063608.txt">Chapter 06 (txt)</a><br>-->
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/06.mp3">Chapter 06 (mp3)</a>
+<!--<a href="/txt/awaited_nonfictional_delusion-063608.txt">Chapter 06 (txt)</a><br>-->
+<a href="/images/awaited-nonfictional-delusion/06.mp3">Chapter 06 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-073701.txt">Chapter 07 (txt)</a><br>-->
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/07.mp3">Chapter 07 (mp3)</a>
+<!--<a href="/txt/awaited_nonfictional_delusion-073701.txt">Chapter 07 (txt)</a><br>-->
+<a href="/images/awaited-nonfictional-delusion/07.mp3">Chapter 07 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-083704.txt">Chapter 08 (txt)</a><br>-->
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/08.mp3">Chapter 08 (mp3)</a>
+<!--<a href="/txt/awaited_nonfictional_delusion-083704.txt">Chapter 08 (txt)</a><br>-->
+<a href="/images/awaited-nonfictional-delusion/08.mp3">Chapter 08 (mp3)</a>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-093708.txt">Chapter 09 (txt)</a><br>-->
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/09.mp3">Chapter 09 (mp3)</a>
+<!--<a href="/txt/awaited_nonfictional_delusion-093708.txt">Chapter 09 (txt)</a><br>-->
+<a href="/images/awaited-nonfictional-delusion/09.mp3">Chapter 09 (mp3)</a>
 <div class="small">
 Bob Crosby And The Bobcats - Way Back Home<br>
 Explosions In The Sky - Time Stops<br>
@@ -81,16 +81,16 @@ Ministry - Jesus Built My Hotrod<br>
 </div>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-103711.txt">Chapter 10 (txt)</a><br>-->
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/10.mp3">Chapter 10 (mp3)</a>
+<!--<a href="/txt/awaited_nonfictional_delusion-103711.txt">Chapter 10 (txt)</a><br>-->
+<a href="/images/awaited-nonfictional-delusion/10.mp3">Chapter 10 (mp3)</a>
 <div class="small">
 AC/DC - Back In Black<br>
 Carpenters - Close To You
 </div>
 </div>
 <div class="profile_search_block" style="width:13em;height:7em;vertical-align:top;">
-<!--<a href="http://youareawaited.com/txt/awaited_nonfictional_delusion-113808.txt">Chapter 11 (txt)</a><br>-->
-<a href="http://youareawaited.com/images/awaited-nonfictional-delusion/11.mp3">Chapter 11 (mp3)</a>
+<!--<a href="/txt/awaited_nonfictional_delusion-113808.txt">Chapter 11 (txt)</a><br>-->
+<a href="/images/awaited-nonfictional-delusion/11.mp3">Chapter 11 (mp3)</a>
 <div class="small">
 John Gorka - I'm From New Jersey
 </div>
