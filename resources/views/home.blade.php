3f59afc73e45affb51ce83d6f01c2befd9c95ad5
Firebird <wastelandfirebird@gmail.com>
2019-12-25 23:27:31 -0700

    txt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -153,7 +153,7 @@
         </form>
     </div></li>
 @else
-    <li>When new events are added, they will appear here. Sign up here to be matched.</li>
+    <li>When new events are added, they will appear here. Sign up to be matched at an upcoming event.</li>
 @endif
 
 @if ($unrated_users)
@@ -173,14 +173,14 @@
         @endforeach
         @if (isset($viewed_all))
         @else
-            Let us know who you'd like to meet at the events.
+            Let us know who you'd like to meet.
         @endif
     </li>
 @endif
 
-<li>Check here a few days before each event to find out who you're matched with.
+<li>Check here a few days before the event to find out who you've been matched with.
 
-<li>At each event, seek out your match and introduce yourself.</li>
+<li>At the event, seek out your match and introduce yourself.</li>
 
 </ol>
 
