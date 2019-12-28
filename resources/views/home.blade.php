e133e7f7e760a082a7cc36fc6dd81936288534a8
zombieite <387685+zombieite@users.noreply.github.com>
2019-12-28 11:59:36 -0700

    txt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 @endif
 
 @foreach ($matched_to_users as $matched_to_user)
-    @if ($matched_to_user->event_is_in_future and $matched_to_user->name and !$matched_to_user->this_users_rating_of_logged_in_user === 0)
+    @if ($matched_to_user->event_is_in_future and $matched_to_user->name and !$matched_to_user->this_users_rating_of_logged_in_user == 0)
         <h1>YOU ARE AWAITED AT {{ strtoupper($matched_to_user->event_long_name) }}! <a class="bright" href="/profile/match?event_id={{ $matched_to_user->event_id }}">Here's your match</a>.</h1>
     @endif
 @endforeach
