2af3c17186df594340d719fbb1c2574cae834bde
zombieite <387685+zombieite@users.noreply.github.com>
2019-12-28 23:18:13 -0700

    ads

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -3,12 +3,12 @@
 '<a href="https://www.youtube.com/watch?v=zQqjvO-useM"><img src="/images/stuff/roadblock-youtube.jpg"></a>',
 '<a href="https://www.facebook.com/thewastelander/"><img src="/images/stuff/wastelander.jpg"></a>',
 '<a href="https://www.facebook.com/WCCorp/"><img src="/images/stuff/wcc.png"></a>',
-'<a href="https://cultofcatmeat.com"><img src="/images/stuff/catmeat.png"></a>',
 ];
 shuffle($ads);
 array_pop($ads);
 array_pop($ads);
 array_push($ads, '<a href="/awaited-nonfictional-delusion"><img src="/images/stuff/awaited-nonfictional-delusion.jpg"></a>');
+array_push($ads, '<a href="https://cultofcatmeat.com"><img src="/images/stuff/catmeat.png"></a>');
 shuffle($ads);
 @endphp
 @foreach ($ads as $ad)
