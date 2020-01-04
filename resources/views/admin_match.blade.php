b4af5c314f59e52277d76c8d04117e30b775710d
zombieite <387685+zombieite@users.noreply.github.com>
2020-01-04 09:21:10 -0700

    tweak admin form

--- resources/views/admin_match.blade.php
+++ resources/views/admin_match.blade.php
@@ -30,7 +30,7 @@
             <td class="{{ $choice_map[$match->user_1_choice] }}">{{ $match->user_1_choice }}</td>
             <td class="{{ $choice_map[$match->user_2_choice] }}">{{ $match->user_2_choice }}</td>
             <td @if ($match->user_1_choice == $match->user_2_choice && ($match->user_1_choice == 3 || $match->user_1_choice == -1)) class="{{ $choice_map[$match->user_1_choice] }}" @endif><a href="/profile/{{ $match->user_id_of_match }}/{{ $match->matchs_name_hyphenated }}">{{ $match->name_of_match }}</a></td>
-            <td>@if ($match->name_of_match) <form action="" method="POST">{{ csrf_field() }}<input type="hidden" name="attending_id" value="{{ $match->attending_id }}"><input type="submit" value="Mark {{ $match->name }}/{{ $match->name_of_match }} found"></form> @else &nbsp; @endif </td>
+            <td>@if ($match->name_of_match && (($match->user_1_choice !== 0 && $match->user_2_choice !== 0)) && !($match->user_1_choice == -1 && $match->user_2_choice == -1)) <form action="" method="POST">{{ csrf_field() }}<input type="hidden" name="attending_id" value="{{ $match->attending_id }}"><input type="submit" value="Mark {{ $match->name }}/{{ $match->name_of_match }} found"></form> @else &nbsp; @endif </td>
         </tr>
     @endforeach
 </table>
