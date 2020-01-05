54fd4f7b088c16521b076a4a73a460d71e92a3f3
Firebird <wastelandfirebird@gmail.com>
2020-01-05 10:25:34 -0700

    noindex

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="robots" content="noindex">
 <meta property="og:type" content="website">
 <meta property="og:title" content="You Are Awaited">
 <meta property="og:url" content="https://youareawaited.com">
