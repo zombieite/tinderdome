9baf1539f30751afafbf53fe3f5cc2b8fba078e3
Firebird <wastelandfirebird@gmail.com>
2020-01-05 00:28:19 -0700

    txt

--- resources/views/potential_match.blade.php
+++ resources/views/potential_match.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 @section('content')
 @php $previous_profile_id = isset($profiles[0]) ? $profiles[0]['profile_id'] : ''; @endphp
-@foreach ($profiles as $profile)
-    @include('user_block_search_result')
-    @php $previous_profile_id = $profile['profile_id'] @endphp
-@endforeach
+@if ($profiles)
+    @foreach ($profiles as $profile)
+        @include('user_block_search_result')
+        @php $previous_profile_id = $profile['profile_id'] @endphp
+    @endforeach
+@else
+    We can't find any potential matches for you yet. Keep an eye on your <a href="/">home page</a> and be sure to rate all new users as they show up.
+@endif
 @endsection
