d507fe56c9e845befaf563e450d083976970b467
zombieite <387685+zombieite@users.noreply.github.com>
2020-01-05 10:39:20 -0700

    remove cruft, disallow robots

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,6 @@
 <head>
 <title>You Are Awaited</title>
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="robots" content="noindex">
 <meta property="og:type" content="website">
 <meta property="og:title" content="You Are Awaited">
