d614b15f0e7faaa379fae365fb89caeb3b8f794d
Firebird <wastelandfirebird@gmail.com>
2020-01-07 21:41:25 -0700

    Prompt to leave a comment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,9 +54,18 @@
         <h2 class="bright">Do you know these users?</h2>
         @for ($i = 0; (($i < 6) && ($i < count($users_who_say_they_know_you))); $i++)
             @if ($users_who_say_they_know_you[$i]->number_photos)
-                @include('user_block_maybe_known', ['user_id' => $users_who_say_they_know_you[$i]->user_id, 'wasteland_name_hyphenated' => $users_who_say_they_know_you[$i]->wasteland_name_hyphenated, 'name' => $users_who_say_they_know_you[$i]->name])
+                @include('user_block_maybe_comment', ['user_id' => $users_who_say_they_know_you[$i]->user_id, 'wasteland_name_hyphenated' => $users_who_say_they_know_you[$i]->wasteland_name_hyphenated, 'name' => $users_who_say_they_know_you[$i]->name])
             @endif
         @endfor
+    @else
+        @if (count($users_you_can_comment_on_but_havent))
+            <h2 class="bright">Leave a comment for these users</h2>
+            @for ($i = 0; (($i < 6) && ($i < count($users_you_can_comment_on_but_havent))); $i++)
+                @if ($users_you_can_comment_on_but_havent[$i]->number_photos)
+                    @include('user_block_maybe_comment', ['user_id' => $users_you_can_comment_on_but_havent[$i]->user_id, 'wasteland_name_hyphenated' => $users_you_can_comment_on_but_havent[$i]->wasteland_name_hyphenated, 'name' => $users_you_can_comment_on_but_havent[$i]->name])
+                @endif
+            @endfor
+        @endif
     @endif
 @endif
 
