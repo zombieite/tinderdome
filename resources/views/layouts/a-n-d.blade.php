3277adb9fa2422fe8c06081dd27340c68afdb2cd
Firebird <wastelandfirebird@gmail.com>
2020-01-15 06:05:00 -0600

    fix viewport

--- resources/views/layouts/a-n-d.blade.php
+++ resources/views/layouts/a-n-d.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta property="og:type" content="website">
 <meta property="og:title" content="Awaited: Nonfictional Delusion">
 <meta property="og:url" content="http://youareawaited.com/awaited-nonfictional-delusion">
