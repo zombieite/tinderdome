aa104fad74a6df503ef89c0a4f78a042852d83a0
Firebird <wastelandfirebird@gmail.com>
2020-02-25 11:23:14 -0700

    css

--- resources/views/admin_match.blade.php
+++ resources/views/admin_match.blade.php
@@ -24,10 +24,10 @@
         <tr>
             <td class="tight">{{ $counter }}</td>
             <td class="tight">{{ $match->cap }}</td>
-            <td class=" @if ($match->user_1_choice == $match->user_2_choice && ($match->user_1_choice == 3 || $match->user_1_choice == -1)) {{ $choice_map[$match->user_1_choice] }} @else @if (!$match->user_id_of_match) no @endif @endif tight"><a href="/profile/{{ $match->user_id }}/{{ $match->wasteland_name_hyphenated }}">{{ $match->name }}</a> ({{ $match->user_id }})</td>
+            <td class=" @if ($match->user_1_choice == $match->user_2_choice && ($match->user_1_choice == 3 || $match->user_1_choice == -1)) {{ $choice_map[$match->user_1_choice] }} @else @if (!$match->user_id_of_match) @if (isset($users_who_are_matched_but_dont_know[$match->user_id])) caution @else no @endif @endif @endif tight"><a href="/profile/{{ $match->user_id }}/{{ $match->wasteland_name_hyphenated }}">{{ $match->name }}</a></td>
             <td class="{{ $choice_map[$match->user_1_choice] }} tight">{{ $match->user_1_choice }}</td>
             <td class="{{ $choice_map[$match->user_2_choice] }} tight">{{ $match->user_2_choice }}</td>
-            <td class=" @if ($match->user_1_choice == $match->user_2_choice && ($match->user_1_choice == 3 || $match->user_1_choice == -1)) {{ $choice_map[$match->user_1_choice] }} @endif tight">@if ($match->user_id_of_match) <a href="/profile/{{ $match->user_id_of_match }}/{{ $match->matchs_name_hyphenated }}">{{ $match->name_of_match }}</a> ({{ $match->user_id_of_match }}) @else &nbsp; @endif </td>
+            <td class=" @if ($match->user_1_choice == $match->user_2_choice && ($match->user_1_choice == 3 || $match->user_1_choice == -1)) {{ $choice_map[$match->user_1_choice] }} @endif tight">@if ($match->user_id_of_match) <a href="/profile/{{ $match->user_id_of_match }}/{{ $match->matchs_name_hyphenated }}">{{ $match->name_of_match }}</a> @else &nbsp; @endif </td>
             <td class="tight">@if ($match->name_of_match && (($match->user_1_choice !== 0 && $match->user_2_choice !== 0)) && !($match->user_1_choice == -1 && $match->user_2_choice == -1)) <form action="" method="POST">{{ csrf_field() }}<input type="hidden" name="attending_id" value="{{ $match->attending_id }}"><input class="tight" type="submit" value="Mark {{ $match->name }}/{{ $match->name_of_match }} found"></form> @else &nbsp; @endif </td>
         </tr>
     @endforeach
