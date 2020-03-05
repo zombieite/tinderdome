603ced3fe85a99081f0ed02c902d92e7a18d4693
Firebird <wastelandfirebird@gmail.com>
2020-03-04 22:33:00 -0700

    txt

--- resources/views/create_event.blade.php
+++ resources/views/create_event.blade.php
@@ -2,9 +2,10 @@
 @section('content')
 
 <h1>Create an event</h1>
+<h2>This feature is coming coming soon for selected users!</h2>
 
 <form action="" method="POST">
-
+    {{ csrf_field() }}
 </form>
 
 @endsection
