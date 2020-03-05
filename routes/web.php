296e3ef8469f002c37c1e0218aa0b32dd61cef56
zombieite <387685+zombieite@users.noreply.github.com>
2020-03-04 22:20:27 -0700

    add link to create event

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get( '/image/upload',                          'ImageController@upload')-
 Route::get( '/photosearch',                           'PhotoSearchController@photosearch')->middleware(       'auth');
 Route::get( '/search',                                'SearchController@search')->middleware(                 'auth');
 Route::get( '/potential-match',                       'PotentialMatchController@potential_match')->middleware('auth');
+Route::get( '/create-event',                          'CreateEventController@create_event')->middleware(      'auth');
 Route::post('/',                                      'HomeController@index')->middleware(                    'auth');
 Route::post('/profile/compatible',                    'ProfileController@compatible')->middleware(            'auth');
 Route::post('/profile/comment',                       'ProfileController@comment')->middleware(               'auth');
@@ -24,6 +25,7 @@ Route::post('/image/upload',                          'ImageController@upload')-
 Route::post('/profile/edit',                          'ProfileController@update')->middleware(                'auth');
 Route::post('/profile/{profile_id}/{wasteland_name}', 'ProfileController@compatible')->middleware(            'auth');
 Route::post('/search',                                'SearchController@update_rating')->middleware(          'auth');
-Route::post('/potential-match',                       'PotentialMatchController@update_rating')->middleware('auth');
+Route::post('/potential-match',                       'PotentialMatchController@update_rating')->middleware(  'auth');
 Route::post('/match-me',                              'MatchController@match_me')->middleware(                'auth');
 Route::post('/admin-match',                           'AdminMatchController@admin_match')->middleware(        'auth');
+Route::post('/create-event',                          'CreateEventController@create_event')->middleware(      'auth');
