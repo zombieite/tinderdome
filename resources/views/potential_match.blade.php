c0c20fb17cbb5e1061d1a69bb32b5ba7011bfbde
Firebird <wastelandfirebird@gmail.com>
2020-03-09 22:41:01 -0700

    hide attendees if you're not signed up for an event

--- resources/views/potential_match.blade.php
+++ resources/views/potential_match.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.app')
 @section('content')
 @php $previous_profile_id = isset($profiles[0]) ? $profiles[0]['profile_id'] : ''; @endphp
-@if ($profiles)
-    @foreach ($profiles as $profile)
-        @include('user_block_search_result')
-        @php $previous_profile_id = $profile['profile_id'] @endphp
-    @endforeach
+@if ($not_signed_up)
+    You are not signed up for this event.
 @else
-    @if ($show_met)
-        No one has signed up yet.
+    @if ($profiles)
+        @foreach ($profiles as $profile)
+            @include('user_block_search_result')
+            @php $previous_profile_id = $profile['profile_id'] @endphp
+        @endforeach
     @else
-        We can't find any potential matches for you yet.
+        @if ($show_met)
+            No one has signed up yet.
+        @else
+            We can't find any potential matches for you yet.
+        @endif
+        Encourage people to sign up, keep an eye on the <a href="/">home page</a>, and be sure to rate all new users as they show up.
     @endif
-    Encourage people to sign up, keep an eye on the <a href="/">home page</a>, and be sure to rate all new users as they show up.
 @endif
 @endsection
