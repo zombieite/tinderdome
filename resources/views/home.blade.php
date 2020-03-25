b955efb44b5f1c61cca52bae510469d094e197e9
zombieite <387685+zombieite@users.noreply.github.com>
2020-03-25 03:19:27 -0700

    txt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,7 +59,7 @@
         @endfor
     @else
         @if (count($users_you_can_comment_on_but_havent))
-            <h2 class="bright">Leave a comment for these users</h2>
+            <h2>Leave a comment for these users</h2>
             @for ($i = 0; (($i < 6) && ($i < count($users_you_can_comment_on_but_havent))); $i++)
                 @if ($users_you_can_comment_on_but_havent[$i]->number_photos)
                     @include('user_block_maybe_comment', ['user_id' => $users_you_can_comment_on_but_havent[$i]->user_id, 'wasteland_name_hyphenated' => $users_you_can_comment_on_but_havent[$i]->wasteland_name_hyphenated, 'name' => $users_you_can_comment_on_but_havent[$i]->name])
