8fbdef0fa331d0dea7c9bdb9d4b6998d78611262
Firebird <wastelandfirebird@gmail.com>
2020-03-26 02:00:31 -0700

    sql

--- sql/create_event.sql
+++ sql/create_event.sql
@@ -5,6 +5,7 @@ CREATE TABLE event (
 	event_class VARCHAR(255) NOT NULL,
     event_long_name VARCHAR(255) NOT NULL,
     event_date DATE NOT NULL,
+    description VARCHAR(2000),
     url VARCHAR(255),
     public BOOLEAN NOT NULL,
 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
