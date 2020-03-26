c8a5e89adaaf19c8a45bc7a3c5c4f99366317d4b
Firebird <wastelandfirebird@gmail.com>
2020-03-26 16:07:31 -0700

    fix bug

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -65,7 +65,7 @@ class SearchController extends Controller
                 gender,
                 height,
                 birth_year,
-                description,
+                users.description,
                 title_index,
                 number_photos,
                 hoping_to_find_love,
