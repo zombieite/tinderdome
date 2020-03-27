72b7309b43284597b623a4e3923b6d1abcc9c6be
Firebird <wastelandfirebird@gmail.com>
2020-03-26 23:28:45 -0700

    fixing crap on event page

--- resources/views/create_event.blade.php
+++ resources/views/create_event.blade.php
@@ -8,13 +8,13 @@
     {{ csrf_field() }}
 
     <label for="event_long_name">Event name</label>
-    <input id="event_long_name" type="text" name="event_long_name" pattern="^[A-Za-z0-9 ]+$" size="50" maxlength="50" required>
+    <input id="event_long_name" type="text" name="event_long_name" size="50" maxlength="50" required>
     <br>Make this unique. If it's a recurring event, add the year to the event name. If it's an additional mission at an existing event, add something like &quot;optional second mission.&quot;
 
     <br><br>
     <label for="event_class">Event class</label>
-    <input id="event_class" type="text" name="event_class" pattern="^[A-Za-z0-9 ]+$" size="50" maxlength="50" required>
-    <br>This is just a word or a few words describing the type of event it is. Event class can be re-used for multiple similar events. You can create a new class, or use an existing class such as atomic_falls, ball, detonation, neotropolis, wasteland, winter_games.
+    <input id="event_class" type="text" name="event_class" size="50" maxlength="50" value="{{ $logged_in_user_name }}'s events" required>
+    <br>This is just a word or a few words to classify the type of event it is. Event class can be re-used for multiple similar events. Some example classes would be "post-apocalyptic," "burning man," ""concert," "festival," "wedding reception," or "corporate retreat."
 
     <br><br>
     <label for="event_date">Event date</label>
@@ -24,6 +24,11 @@
     <label for="url">URL</label>
     <input id="url" type="url" name="url" size="50" maxlength="255">
     <br>URL of the event. Often this can be a Facebook event or group page.
+
+    <br><br>
+    <label for="description">Time, location, and description</label>
+    <textarea rows="10" name="description" id="description"></textarea>
+    <br>Time, location, and description of the event. 2000 characters maximum.
 
     @if ($logged_in_user_can_create_public_missions)
         <br><br>
