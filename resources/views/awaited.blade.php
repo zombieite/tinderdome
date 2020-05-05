a876d558f1d9ba1480d3815b2bf6c64b8e2f57b4
zombieite <387685+zombieite@users.noreply.github.com>
2020-05-05 00:07:14 -0700

    new video, apache conf

--- resources/views/awaited.blade.php
+++ resources/views/awaited.blade.php
@@ -4,7 +4,7 @@
 
 <img style="width:100%;filter:none;" src="/images/awaited-nonfictional-delusion/awaited-nonfictional-delusion.jpg">
 
-<iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/zQqjvO-useM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/bhFScP_Ag5Y" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
 <p>
 What if, by going to Wasteland Weekend, you really could enter another world? Too implausible to be an autobiography. Too real to be fiction. It's much more than just a story about Wasteland Weekend and Uranium Springs. It's a missing instruction manual for life. This book will help you make friends. This book will help you make money. This book might even save your life. Two years in the making, this tiny, 143-page book has been meticulously engineered to evoke a sense of hope during the apocalypse. It will inspire you to change your life and change the world. Anarchy, attachment, cars, economics, entrepreneurship, entropy, ethics, grief, guilt, guns, kindness, self-reliance, seventies nostalgia, and stoicism. This is the You Are Awaited story. Awaited: Nonfictional Delusion, by John Binns.
