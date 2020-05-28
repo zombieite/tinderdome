b79dd1e759a1af5ab85ef3513d5e2f7d9d0fd2a6
Firebird <wastelandfirebird@gmail.com>
2020-05-27 21:54:11 -0700

    show signup checkbox on event page

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -1,27 +1,36 @@
 @extends('layouts.app')
 @section('content')
 
-<form action="" method="POST">
     {{ csrf_field() }}
 
     <h1>{{ $event->event_long_name }}: {{ $event->event_date }}</h1>
     @if ($event->url)
         <h3><a href="{{ $event->url }}">{{ $event->url }}</a></h3>
     @endif
-    {{ $event->description }}<br><br>
     @if ($event->created_by_name)
         @if ($logged_in_user_created_this_event)
-            <span class="small">Event created by you</span>
+            <span class="small">Event created by you</span><br><br>
         @else
-            <span class="small">Event created by {{ $event->created_by_name }}</span>
+            <span class="small">Event created by {{ $event->created_by_name }}</span><br><br>
         @endif
     @endif
+    {{ $event->description }}<br>
     @guest
         <a href="/register" class="bright">Sign up here</a>.
+    @else
+        <form action="/" method="POST">
+            {{ csrf_field() }}
+            <input type="hidden" name="attending_event_form" value="1">
+            <br><input class="upcoming_event_checkbox" type="checkbox" name="attending_event_id_{{ $event->event_id }}"
+                @if ($event->attending_event_id)
+                    @if ($event->user_id_of_match || $event->already_matched_but_dont_know_it)
+                        disabled
+                    @endif
+                    checked
+                @endif
+            > I will be attending this event.
+            <br><br><button id="submit" type="submit" class="yesyes">Submit changes</button>
+        </form>
     @endguest
-    @if ($logged_in_user_created_this_event)
-        <br><button id="submit" type="submit" class="yesyes">Submit changes</button>
-    @endif
-</form>
 
 @endsection
