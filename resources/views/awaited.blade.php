017a13de4e9b2267aadaaf0cd051f9bed5cf6e30
zombieite <387685+zombieite@users.noreply.github.com>
2020-07-04 17:02:10 -0700

    add more stories to stories page

--- resources/views/awaited.blade.php
+++ resources/views/awaited.blade.php
@@ -4,47 +4,59 @@
 
 <img style="width:100%;filter:none;" src="/images/awaited-nonfictional-delusion/awaited-nonfictional-delusion.jpg">
 
-<iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/bhFScP_Ag5Y" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-
 <p>
 What if, by going to Wasteland Weekend, you really could enter another world? Too implausible to be an autobiography. Too real to be fiction. It's much more than just a story about Wasteland Weekend and Uranium Springs. It's a missing instruction manual for life. This book will help you make friends. This book will help you make money. This book might even save your life. Two years in the making, this tiny, 143-page book has been meticulously engineered to evoke a sense of hope during the apocalypse. It will inspire you to change your life and change the world. Anarchy, attachment, cars, economics, entrepreneurship, entropy, ethics, grief, guilt, guns, kindness, self-reliance, seventies nostalgia, and stoicism. This is the You Are Awaited story. Awaited: Nonfictional Delusion, by John Binns.
 </p>
 
-<h1><a href="http://www.lulu.com/shop/john-binns/awaited-nonfictional-delusion/paperback/product-24218988.html">Paperback</a></h1>
-
-<h1><a href="https://www.amazon.com/Awaited-Nonfictional-Delusion-John-Binns-ebook/dp/B07ZYBBCGF/">Kindle</a>
+<iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/zQqjvO-useM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
 <!--<h1><a href="/images/awaited-nonfictional-delusion/awaited-nonfictional-delusion.pdf">Download the pdf</a></h1>-->
 
-<h1>Download samples:</h1>
+<div class="profile_search_block" style="vertical-align:top;">
+<h1><a href="http://www.lulu.com/shop/john-binns/awaited-nonfictional-delusion/paperback/product-24218988.html">Paperback</a></h1>
+</div>
 
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-000000.txt">Chapter 00 (txt)</a><br>
+<div class="profile_search_block" style="vertical-align:top;">
+<h1><a href="https://www.amazon.com/Awaited-Nonfictional-Delusion-John-Binns-ebook/dp/B07ZYBBCGF/">Kindle</a></h1>
+</div>
+<br>
+
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
+<a href="/txt/awaited_nonfictional_delusion-000000.txt">Chapter 00 (txt)</a><br><br>
 <a href="/images/awaited-nonfictional-delusion/00.mp3">Chapter 00 (mp3)</a><br>
 <div class="small">
 Hymie's Basement - 21st Century Pop Song<br>
 AC/DC - Back In Black<br>
 Carpenters - Close To You
+</div><br>
+<a href="/images/awaited-nonfictional-delusion/00-kelli.m4a">Chapter 00 (m4a Aussie narration)</a><br>
+<div class="small">
+Read by Kelli Robinson<br>
 </div>
-<a href="/images/awaited-nonfictional-delusion/00-kelli.m4a">Chapter 00, Aussie narration by Kelli Robinson (m4a)</a><br>
 </div>
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
 <!--<a href="/txt/awaited_nonfictional_delusion-012501.txt">Chapter 01 (txt)</a><br>-->
 <a href="/images/awaited-nonfictional-delusion/01.mp3">Chapter 01 (mp3)</a><br>
 <div class="small">
 Sex Pistols - Anarchy In The UK Demo<br>
 Sheldon Allman - Crawl Out Through The Fallout<br>
 <a href="https://thedeloreans.bandcamp.com/album/as-long-as-its-you-single">Deloreans - As Long As It's You</a>
-</div>
-<a href="/images/awaited-nonfictional-delusion/01-kelli.m4a">Chapter 01, Aussie narration by Kelli Robinson (m4a)</a><br>
+</div><br>
+<a href="/images/awaited-nonfictional-delusion/01-kelli.m4a">Chapter 01 (m4a Aussie narration)</a><br>
+<div class="small">
+Read by Kelli Robinson<br>
+</div><br>
 <a href="https://www.youtube.com/watch?v=zQqjvO-useM">Chapter 01, Scene II (Film)</a>
 </div>
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
 <!--<a href="/txt/awaited_nonfictional_delusion-022502.txt">Chapter 02 (txt)</a><br>-->
-<a href="/images/awaited-nonfictional-delusion/02.mp3">Chapter 02 (mp3)</a><br>
-<a href="/images/awaited-nonfictional-delusion/02-kelli.m4a">Chapter 02, Aussie narration by Kelli Robinson (m4a)</a><br>
+<a href="/images/awaited-nonfictional-delusion/02.mp3">Chapter 02 (mp3)</a><br><br>
+<a href="/images/awaited-nonfictional-delusion/02-kelli.m4a">Chapter 02 (m4a Aussie narration)</a><br>
+<div class="small">
+Read by Kelli Robinson<br>
 </div>
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
+</div>
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
 <!--<a href="/txt/awaited_nonfictional_delusion-033011.txt">Chapter 03 (txt)</a><br>-->
 <a href="/images/awaited-nonfictional-delusion/03.mp3">Chapter 03 (mp3)</a>
 <div class="small">
@@ -54,31 +66,36 @@ Rush - Spirit Of Radio<br>
 Peter Gabriel and Kate Bush - Don't Give Up
 </div>
 </div>
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
 <!--<a href="/txt/awaited_nonfictional_delusion-043508.txt">Chapter 04 (txt)</a><br>-->
 <a href="/images/awaited-nonfictional-delusion/04.mp3">Chapter 04 (mp3)</a>
 </div>
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
 <!--<a href="/txt/awaited_nonfictional_delusion-053604.txt">Chapter 05 (txt)</a><br>-->
 <a href="/images/awaited-nonfictional-delusion/05.mp3">Chapter 05 (mp3)</a>
 </div>
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
 <!--<a href="/txt/awaited_nonfictional_delusion-063608.txt">Chapter 06 (txt)</a><br>-->
-<a href="/images/awaited-nonfictional-delusion/06.mp3">Chapter 06 (mp3)</a>
+<a href="/images/awaited-nonfictional-delusion/06.mp3">Chapter 06 (mp3)</a><br><br>
+<a href="/images/awaited-nonfictional-delusion/deadline_0.mp3">Deadline's Introduction</a><br>
+<div class="small">Read by Chris Okula</div><br>
 </div>
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
 <!--<a href="/txt/awaited_nonfictional_delusion-073701.txt">Chapter 07 (txt)</a><br>-->
 <a href="/images/awaited-nonfictional-delusion/07.mp3">Chapter 07 (mp3)</a>
 </div>
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
 <!--<a href="/txt/awaited_nonfictional_delusion-083704.txt">Chapter 08 (txt)</a><br>-->
-<a href="/images/awaited-nonfictional-delusion/08.mp3">Chapter 08 (mp3)</a><br>
+<a href="/images/awaited-nonfictional-delusion/08.mp3">Chapter 08 (mp3)</a><br><br>
 <a href="/images/awaited-nonfictional-delusion/deadline_uranium_springs_2019_1.mp3">Deadline's Story 1</a><br>
-<a href="/images/awaited-nonfictional-delusion/deadline_uranium_springs_2019_2.mp3">Deadline's Story 2 (missing)</a><br>
+<div class="small">Read by Chris Okula</div><br>
+Deadline's Story 2 (recording is missing)<br><br>
 <a href="/images/awaited-nonfictional-delusion/deadline_uranium_springs_2019_3.mp3">Deadline's Story 3</a><br>
+<div class="small">Read by Chris Okula</div><br>
 <a href="/images/awaited-nonfictional-delusion/deadline_uranium_springs_2019_4.mp3">Deadline's Story 4</a><br>
+<div class="small">Read by Chris Okula</div><br>
 </div>
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
 <!--<a href="/txt/awaited_nonfictional_delusion-093708.txt">Chapter 09 (txt)</a><br>-->
 <a href="/images/awaited-nonfictional-delusion/09.mp3">Chapter 09 (mp3)</a>
 <div class="small">
@@ -88,7 +105,7 @@ Ministry - Thieves<br>
 Ministry - Jesus Built My Hotrod<br>
 </div>
 </div>
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
 <!--<a href="/txt/awaited_nonfictional_delusion-103711.txt">Chapter 10 (txt)</a><br>-->
 <a href="/images/awaited-nonfictional-delusion/10.mp3">Chapter 10 (mp3)</a>
 <div class="small">
@@ -96,12 +113,14 @@ AC/DC - Back In Black<br>
 Carpenters - Close To You
 </div>
 </div>
-<div class="profile_search_block" style="width:13em;height:10em;vertical-align:top;">
+<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
 <!--<a href="/txt/awaited_nonfictional_delusion-113808.txt">Chapter 11 (txt)</a><br>-->
 <a href="/images/awaited-nonfictional-delusion/11.mp3">Chapter 11 (mp3)</a>
 <div class="small">
 John Gorka - I'm From New Jersey
 </div>
 </div>
+
+<iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/bhFScP_Ag5Y" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
 @endsection
