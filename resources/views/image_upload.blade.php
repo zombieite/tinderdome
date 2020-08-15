4962755635afc0e13543e111473509e2ccf15793
zombieite <387685+zombieite@users.noreply.github.com>
2020-08-14 23:06:44 -0700

    set up some prez election stuff

--- resources/views/image_upload.blade.php
+++ resources/views/image_upload.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 @if ($new_user && $number_photos > 0)
-	<h2>Signup complete! You can upload more photos or <a href="/profile/compatible?">choose who you'd like to meet</a>.</h2>
+	<h2>Signup complete! You can upload more photos or <a href="/">go to the home page</a>.</h2>
 @endif
 @if ($errors)
 	<h2 class="bright">Error updating images: {!! $errors !!} Problems? You can also just email images to Firebird.</h2>
