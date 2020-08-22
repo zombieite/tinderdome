29fc93b49df11fd17e212525f028b9105d8ec06f
zombieite <387685+zombieite@users.noreply.github.com>
2020-08-21 19:56:51 -0700

    splitting up profile page into sensible pieces

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,54 +1,24 @@
 @extends('layouts.app')
 @section('content')
 @if ($is_my_match and $choice !== 0 and $choice !== -1)
-    @if ($match_knows_you_are_their_match)
-	    <h1 class="bright">{{ $logged_in_user->name }}, you are awaited by {{ $wasteland_name }}!</h1>
-        <h2>They have logged in to check their matches. They know you are their match.</h2>
-    @else
-        <h1 class="bright">{{ $logged_in_user->name }}, your mission is to find {{ $wasteland_name }}!</a>
-        <h2>They have not yet checked their matches, but they signed up to be found. They might not know you are their match, but you can still seek them out.</h2>
-    @endif
-	@if ($count_with_same_name)
-		@if ($count_with_same_name == 1)
-			Another wastelander also goes by the name {{ $wasteland_name }}. Be sure to find the right one.
-		@else
-			{{ $count_with_same_name }} other wastelanders go by the name {{ $wasteland_name }}. Be sure to find the right one.
-		@endif
-	@endif
-	Your mission is to seek out {{ $wasteland_name }} at {{ $event_long_name }}.
-    @if ($match_knows_you_are_their_match)
-    	{{ $wasteland_name }} will be looking for you, too.
-    @endif
-    You can find them during the event or after.
-    This mission won't expire until one of you deletes the mission or deletes their profile.
-    If you find them and meet them in person let us know. This will mark your mission as complete.
-	<br>
-    @if ($ok_to_mark_user_found)
-    	@include('rating_form', ['action' => '/profile/compatible?', 'user_id_to_rate' => $unchosen_user_id, 'current_choice' => $choice, 'curse_interface' => $curse_interface])
-    @endif
-    @if ($recently_updated_users)
-        <br><a href="/profile/{{ $recently_updated_users[0]->id }}/{{ $recently_updated_users[0]->wasteland_name_hyphenated }}?review=1" class="bright">Review another recently-updated profile</a><br>
-    @endif
-	<br>
+    @include('profile_is_an_unfound_match', [])
+@elseif (!$is_my_match && !$is_me && $unchosen_user_id != 1)
+    @include('not_match_but_needs_rating_form', [])
 @else
-	@if (!$is_me && $unchosen_user_id != 1 )
-        @if ($is_my_match)
-            <h3>Have you found {{ $wasteland_name }}?</h3>
-        @else
-    		<h3>Would you enjoy meeting this user? @if ($count_left)({{$count_left}} profiles left to view) @endif</h3>
-        @endif
-		@include('rating_form', ['action' => '/profile/compatible?', 'user_id_to_rate' => $unchosen_user_id, 'current_choice' => $choice, 'curse_interface' => $curse_interface])
-	@endif
-    @if ($recently_updated_users)
-        <br><a href="/profile/{{ $recently_updated_users[0]->id }}/{{ $recently_updated_users[0]->wasteland_name_hyphenated }}?review=1" class="bright">Review another recently-updated profile</a>
-    @endif
-	@if ($missions_completed)
-        <h1><span class="dull">{{ $titles[$title_index] }}</span> {{ $wasteland_name }}@if ($campaigning) is running for the office of Prezident of the Restored United States of Murica! @endif</h1>
-        Missions completed: {{ $missions_completed }}.
-	@else
-		<h1>{{ $wasteland_name }}</h1>
-	@endif
+    <h1>{{ $wasteland_name }}</h1>
 @endif
+
+@if ($recently_updated_users)
+    <br><a href="/profile/{{ $recently_updated_users[0]->id }}/{{ $recently_updated_users[0]->wasteland_name_hyphenated }}?review=1" class="bright">Review another recently-updated profile</a>
+@endif
+
+@if ($missions_completed)
+    <h1><span class="dull">{{ $titles[$title_index] }}</span> {{ $wasteland_name }}@if ($campaigning) is running for the office of Prezident of the Restored United States of Murica! @endif    </h1>
+Missions completed: {{ $missions_completed }}.
+@else
+<h1>{{ $wasteland_name }}</h1>
+@endif
+
 @if ($show_how_to_find_me || $share_info || $is_me)
 	@if ($how_to_find_me || $share_info)
 		@if ($profile_id == 1)
