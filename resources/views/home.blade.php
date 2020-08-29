7a91021a6e7caa3e17ae165b1a40707bded1a23e
Firebird <wastelandfirebird@gmail.com>
2020-08-29 13:36:02 -0700

    require photo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,14 @@
     <h2 class="bright">{{ $success_message }}</h2>
 @endif
 
-@include('prezidential_election', [])
-
-@foreach ($matched_to_users as $matched_to_user)
-        @include('you_are_awaited', [])
-@endforeach
-@if ($comments_to_approve)
-        @include('new_comments', [])
-@endif
 @if ($number_photos)
+    @include('prezidential_election', [])
+    @foreach ($matched_to_users as $matched_to_user)
+        @include('you_are_awaited', [])
+    @endforeach
+    @if ($comments_to_approve)
+        @include('new_comments', [])
+    @endif
     @if (count($unrated_users) >= 3)
         @include('enjoy_meeting', [])
     @endif
@@ -23,10 +22,10 @@
             @include('leave_a_comment', [])
         @endif
     @endif
+    @include('mutuals', [])
+    {{-- @include('mission_status', []) --}}
+    @include('mission_matches', [])
+@else
+    <h1>You must <a href="/image/upload">upload a photo</a> to vote or to run for office.</h1>
 @endif
-@include('mutuals', [])
-
-{{-- @include('mission_status', []) --}}
-
-@include('mission_matches', [])
 @endsection
