8460157394fa8f17efafc5a0acf3ed2715174fed
Firebird <wastelandfirebird@gmail.com>
2020-08-28 20:40:10 -0700

    going to show candidates on homepage soon

--- resources/views/profile_name_section.blade.php
+++ resources/views/profile_name_section.blade.php
@@ -1,13 +1,7 @@
-<h1>
-    @if ($missions_completed)
-        <span class="dull">{{ $titles[$title_index] }}</span>
-    @endif
-    {{ $wasteland_name }}
-    @if ($campaigning)
-        is running for the office of
-        </h1><h1>@include('prezident', [])
-    @endif
-</h1>
+<h1>@if ($missions_completed) <span class="dull">{{ $titles[$title_index] }}</span> @endif {{ $wasteland_name }}</h1>
+@if ($campaigning)
+    <h1><span class="dull">CANDIDATE FOR THE OFFICE OF</span> @include('prezident', [])</h1>
+@endif
 @if ($video_id)
     <iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/{{ $video_id }}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 @endif
