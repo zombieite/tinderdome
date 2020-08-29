905a20a6917f0f3728c1b871e31a3271b6f51135
zombieite <387685+zombieite@users.noreply.github.com>
2020-08-28 22:05:32 -0700

    make only one vote button

--- resources/views/voting_form.blade.php
+++ resources/views/voting_form.blade.php
@@ -1,7 +1,2 @@
-<span class="bright">THIS VOTING FORM WILL BECOME ACTIVE ON OCT. 6</span>
-<form action="" method="POST">
-	{{ csrf_field() }}
-	<input type="radio" name="vote" id="vote_{{ $profile->profile_id }}" value="{{ $profile->profile_id }}" disabled="true">
-    <label for="vote_{{ $profile->profile_id }}"><a href="/profile/{{ $profile->profile_id }}/{{ $profile->wasteland_name_hyphenated }}">{{ $profile->name }}</a></label>
-    <input type="submit" name="submit" value="THIS BUTTON WILL BECOME ACTIVE ON OCT. 6" disabled="true">
-</form>
+<input type="radio" name="vote" id="vote_{{ $profile->profile_id }}" value="{{ $profile->profile_id }}" disabled="true">
+<label class="dull" for="vote_{{ $profile->profile_id }}">{{ $profile->name }}</label>
