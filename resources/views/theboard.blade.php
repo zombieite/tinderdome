5bab867f4e7b57eafa87aa08464d80f37a6eb073
zombieite <387685+zombieite@users.noreply.github.com>
2020-09-26 00:58:38 -0700

    txt

--- resources/views/theboard.blade.php
+++ resources/views/theboard.blade.php
@@ -9,7 +9,9 @@ The Next Awaited Book
             <td>
                 <table>
                     <tr><td><span class="small">The first ever Tomorrowland Weekend!</span></td></tr>
-                    <tr><td><span class="small">FB signing books, TD introduces herself to FB, begs him to teach her how to swap timelines. FB is not paying attention until she produces MA, introducing him as Bruce, asking if instead FB can help HIM swap timelines. MA attacks FB, FB's fans grab MA, say no fighting except in the Cage. FB says no it's all right, just get him out of here. FB flees with TD.</span></td></tr>
+                    <tr><td><span class="small">
+FB signing books. Answering questions. Explains he killed the grieving woman. TD introduces herself to FB, begs him to teach her how to swap timelines. FB is not paying attention until she produces MA, introducing him as Bruce, asking if instead FB can help HIM swap timelines. MA attacks FB, FB's fans grab MA, say no fighting except in the Cage. FB says no it's all right, just get him out of here. FB flees with TD.
+                    </span></td></tr>
                     <tr><td><span class="small">+/- FB feeling cocky/FB terrified</span></td></tr>
                     <tr><td><span class="small">&gt;&lt; FB vs MA</span></td></tr>
                 </table>
@@ -17,7 +19,7 @@ The Next Awaited Book
             <td>
                 <table>
                     <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
+                    <tr><td><span class="small">Possibly revealed now that no one swaps timelines, leading to everyone at Tomorrowland Weekend wanting their money back.</span></td></tr>
                     <tr><td><span class="small">+/- Mood change</span></td></tr>
                     <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
                 </table>
