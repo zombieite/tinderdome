af2b328af4921578f5094eb17cba0ed764bc6463
Firebird <wastelandfirebird@gmail.com>
2020-10-03 21:44:44 -0700

    txt

--- resources/views/theboard.blade.php
+++ resources/views/theboard.blade.php
@@ -10,18 +10,18 @@ The Next Awaited Book
                 <table>
                     <tr><td><span class="small">The first ever Tomorrowland Weekend!</span></td></tr>
                     <tr><td><span class="small">
-FB signing books. Answering questions. Explains he killed the grieving woman. TD introduces herself to FB, begs him to teach her how to swap timelines. FB is not paying attention until she produces MA, introducing him as Bruce, asking if instead FB can help HIM swap timelines. MA attacks FB, FB's fans grab MA, say no fighting except in the Cage. FB says no it's all right, just get him out of here. FB flees with TD.
+FB selling tickets. TD introduces herself to FB, asks how to swap timelines. FB tries to get rid of her. She produces MA, introducing him as Bruce. Here because maybe FB can help HIM swap timelines. MA attacks FB, FB's fans grab MA, say no fighting except in the Cage. FB says no it's all right.
                     </span></td></tr>
-                    <tr><td><span class="small">+/- FB feeling cocky/FB terrified</span></td></tr>
+                    <tr><td><span class="small">+/- FB cocky/FB terrified</span></td></tr>
                     <tr><td><span class="small">&gt;&lt; FB vs MA</span></td></tr>
                 </table>
             </td>
             <td>
                 <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Possibly revealed now that no one swaps timelines, leading to everyone at Tomorrowland Weekend wanting their money back.</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
+                    <tr><td><span class="small">Firebird's trailer at Tomorrowland Weekend</span></td></tr>
+                    <tr><td><span class="small">They simultaneously get the idea to rebuild the Trans Am. MA reveals that no one swaps timelines, leading to everyone at Tomorrowland Weekend wanting their money back. They get angry and burn the place down.</span></td></tr>
+                    <tr><td><span class="small">+/- FB in control/FB loses everything</span></td></tr>
+                    <tr><td><span class="small">&gt;&lt; FB vs Tomorrowlanders</span></td></tr>
                 </table>
             </td>
             <td>
