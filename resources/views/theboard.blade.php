e70589e34845fec4f7fb3d342445fee1733551ff
Firebird <wastelandfirebird@gmail.com>
2020-10-03 23:34:15 -0700

    txt

--- resources/views/theboard.blade.php
+++ resources/views/theboard.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 
-The Next Awaited Book
+Awaited: The Phoenix
 <table>
     <tr><td colspan="10">Act 1</td></tr>
         <tr>
             <td>
                 <table>
-                    <tr><td><span class="small">The first ever Tomorrowland Weekend!</span></td></tr>
+                    <tr><td><span class="small">The first ever Tomorrowland Weekend</span></td></tr>
                     <tr><td><span class="small">
 FB selling tickets. TD introduces herself to FB, asks how to swap timelines. FB tries to get rid of her. She produces MA, introducing him as Bruce. Here because maybe FB can help HIM swap timelines. MA attacks FB, FB's fans grab MA, say no fighting except in the Cage. FB says no it's all right.
                     </span></td></tr>
@@ -18,16 +18,16 @@ FB selling tickets. TD introduces herself to FB, asks how to swap timelines. FB 
             </td>
             <td>
                 <table>
-                    <tr><td><span class="small">Firebird's trailer at Tomorrowland Weekend</span></td></tr>
-                    <tr><td><span class="small">They simultaneously get the idea to rebuild the Trans Am. MA reveals that no one swaps timelines, leading to everyone at Tomorrowland Weekend wanting their money back. They get angry and burn the place down.</span></td></tr>
+                    <tr><td><span class="small">FB's trailer at Tomorrowland Weekend</span></td></tr>
+                    <tr><td><span class="small">FB and MA simultaneously get the idea to rebuild the Trans Am. MA reveals that no one swaps timelines, leading to everyone at Tomorrowland Weekend wanting their money back. They get angry and burn the place down.</span></td></tr>
                     <tr><td><span class="small">+/- FB in control/FB loses everything</span></td></tr>
                     <tr><td><span class="small">&gt;&lt; FB vs Tomorrowlanders</span></td></tr>
                 </table>
             </td>
             <td>
                 <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
+                    <tr><td><span class="small">Woolworths Bakersfield</span></td></tr>
+                    <tr><td><span class="small">FB and DT meet up with MS to ask for her help.</span></td></tr>
                     <tr><td><span class="small">+/- Mood change</span></td></tr>
                     <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
                 </table>
