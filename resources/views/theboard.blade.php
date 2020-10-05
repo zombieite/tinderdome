23996d9c28b30107f007a17d7d6ad4fffcbcdb0e
zombieite <387685+zombieite@users.noreply.github.com>
2020-10-05 00:07:18 -0700

    txt

--- resources/views/theboard.blade.php
+++ resources/views/theboard.blade.php
@@ -2,342 +2,41 @@
 
 @section('content')
 
-Awaited: Revenge and Redemption
+Awaited: The Sequel
 <table>
-    <tr><td colspan="10">Act 1</td></tr>
-        <tr>
-            <td>
-                <table>
-                    <tr><td><span class="small">The first ever Tomorrowland Weekend</span></td></tr>
-                    <tr><td><span class="small">
+    <tr><td colspan="6">Act 1</td></tr>
+    <tr><td>Page</td><td>Beat</td><td>Setting</td><td>Summary</td><td>Emotional Trajectory
+ </td><td>Conflict</td></tr>
+    <tr>
+        <td>1-5</td>
+        <td>Opening Image, Theme Stated</td>
+        <td>The first ever Tomorrowland Weekend</td>
+        <td>
 FB selling tickets. TD introduces herself to FB, asks how to swap timelines. FB tries to get rid of her. She produces MA, introducing him as Bruce. Here because maybe FB can help HIM swap timelines. MA attacks FB, FB's fans grab MA, say no fighting except in the Cage. FB says no it's all right.
-                    </span></td></tr>
-                    <tr><td><span class="small">+/- FB cocky/FB terrified</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; FB vs MA</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">FB's trailer at Tomorrowland Weekend</span></td></tr>
-                    <tr><td><span class="small">FB and MA simultaneously get the idea to rebuild the Trans Am. MA reveals that no one swaps timelines, leading to everyone at Tomorrowland Weekend wanting their money back. They get angry and burn the place down.</span></td></tr>
-                    <tr><td><span class="small">+/- FB in control/FB loses everything</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; FB vs Tomorrowlanders</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Woolworths Bakersfield</span></td></tr>
-                    <tr><td><span class="small">FB and DT meet up with MS to ask for help.</span></td></tr>
-                    <tr><td><span class="small">-/+ FB hopeless/FB hopeful</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; FB vs Skelli</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-        </tr>
-    <tr><td colspan="10">Act 2</td></tr>
-        <tr>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-        </tr>
-    <tr><td colspan="10">Midpoint</td></tr>
-        <tr>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-        </tr>
-    <tr><td colspan="10">Act 3</td></tr>
-        <tr>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><td><span class="small">Setting</span></td></tr>
-                    <tr><td><span class="small">Scene details</span></td></tr>
-                    <tr><td><span class="small">+/- Mood change</span></td></tr>
-                    <tr><td><span class="small">&gt;&lt; Conflict</span></td></tr>
-                </table>
-            </td>
-        </tr>
+        </td>
+        <td>FB&nbsp;cocky&nbsp;&#x2198&nbsp;FB&nbsp;terrified</td>
+        <td>MA&gt;FB</td>
+    </tr>
+    <tr>
+        <td>12</td>
+        <td>Catalyst</td>
+        <td>FB's trailer at Tomorrowland Weekend</td>
+        <td>
+FB and MA simultaneously get the idea to rebuild the Trans Am. MA reveals that no one swaps timelines, leading to everyone at Tomorrowland Weekend wanting their money back. They get angry and burn the place down.
+        </td>
+        <td>FB&nbsp;in&nbsp;control&nbsp;&#x2198&nbsp;FB&nbsp;loses&nbsp;everything</td>
+        <td>Tomorrowlanders&gt;FB</td>
+    </tr>
+    <tr>
+        <td>12-25</td>
+        <td>Debate</td>
+        <td>Woolworths Bakersfield</td>
+        <td>
+FB and DT meet up with MS to ask for help.
+        </td>
+        <td>FB&nbsp;hopeless&nbsp;&#x2197&nbsp;FB&nbsp;hopeful</td>
+        <td>FB>Skelli</td>
+    </tr>
 </table>
 
 @endsection
