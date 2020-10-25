f6c57361d6d19dae8ae8d6b43ec54d259023137c
zombieite <387685+zombieite@users.noreply.github.com>
2020-10-24 19:53:04 -0700

    txt

--- resources/views/theboard.blade.php
+++ resources/views/theboard.blade.php
@@ -8,125 +8,87 @@ Awaited: The Sequel
     <tr><td>Page</td><td>Beat</td><td>Setting</td><td>Summary</td><td>Emotional Trajectory
  </td><td>Conflict</td></tr>
     <tr>
-        <td>1-5</td>
-        <td>Opening Image, Mirror Of Final Image, Save The Cat, Theme Stated, This Is Gonna Be Good</td>
-        <td>Entrance gate at the first ever Tomorrowland Weekend</td>
+        <td>1-11</td>
+        <td>Opening Image, Mirror Of Final Image, Theme Stated, This Is Gonna Be Good, Everyday Life, 6 Things That Need Fixing, Introduce Running Gags And Other Things That Will Come Up Later, Introduce A-Story Characters Who Are Mirrors Of B-Story Characters, Save The Cat</td>
+        <td>The first ever Tomorrowland Weekend</td>
         <td>
-FB selling tickets to an event called Tomorrowland Weekend, where postapocalyptic weirdos dress up as nonapocalyptic normies. DT introduces herself to FB, asks how to swap timelines. FB tries to get rid of her. She produces MA, introducing him as Bruce. Here because maybe FB can help HIM swap timelines. MA attacks FB, FB's fans grab MA, say no fighting except in the Cage. FB wants to keep up the charade, says no it's all right.
+FB selling tickets to an event called Tomorrowland Weekend, where postapocalyptic weirdos dress up as nonapocalyptic normies. DT introduces herself to FB, asks how to swap timelines. FB tries to get rid of her.
         </td>
-        <td>FB&nbsp;cocky&nbsp;&#x2198&nbsp;FB&nbsp;terrified</td>
-        <td>MA&gt;FB</td>
+        <td>&#x2197&#x2198</td>
+        <td>&gt;&lt;</td>
     </tr>
     <tr>
-        <td>6-11</td>
-        <td>Setup, Introduce A-Story Characters, Everyday Life, 6 Things That Need Fixing, Begin Running Gags</td>
-        <td>FB's trailer at Tomorrowland Weekend</td>
-        <td>They say hi to Mad Skelli on the way to FB's trailer. It's clear that FB and MS still haven't figured out what their relationship is. In trailer FB asks what DT and MA are doing there. MA finds out his car was destroyed. FB and MA get the idea to rebuild a new version of the car they used to both covet. MA reveals that no one swaps timelines, leading to DT being mad and everyone at Tomorrowland Weekend wanting their money back. They get angry and burn the place down.
-        <td>FB&nbsp;in&nbsp;control&nbsp;&#x2198&nbsp;FB&nbsp;loses&nbsp;everything</td>
-        <td>Tomorrowlanders>FB</td>
-    </tr>
-    <tr>
-        <td>12</td>
-        <td>Catalyst</td>
-        <td>Woolworths Bakersfield</td>
-        <td>
-FB and DT meet up with MS. FB asks MS for money. DT asks them both for help killing MA because he killed her father/their old friend, MK.
-        </td>
-        <td>FB hopeless&nbsp;&#x2197&nbsp;FB&nbsp;hopeful</td>
-        <td>DT&gt;FB&gt;MS</td>
-    </tr>
-    <tr>
-        <td>13-24</td>
-        <td>Debate</td>
-        <td>Narration</td>
-        <td>
-Narrator rants about capitalism and explains the plan. Plan is to start by checking out the old Van Nuys Firebird factory.
-        </td>
+        <td>12-24</td>
+        <td>Catalyst &amp; Debate</td>
         <td></td>
-        <td></td>
+        <td>
+
+        </td>
+        <td>&#x2197&#x2198</td>
+        <td>&gt;&lt;</td>
     </tr>
     <tr><td colspan="6">Act 2: Antithesis</td></tr>
     <tr>
-        <td>25-29</td>
-        <td>Break Into Two, Hero Embarks On Adventure</td>
+        <td>25-30</td>
+        <td>Break Into Two, Hero Embarks On Adventure, B-Story Or Love Story, Introduce B-Story Characters Who Are Mirrors Of A-Story Characters</td>
         <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>30</td>
-        <td>B-Story Or Love Story, Introduce Mirror Characters Of A-Story Characters</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>
+
+        </td>
+        <td>&#x2197&#x2198</td>
+        <td>&gt;&lt;</td>
     </tr>
     <tr>
         <td>31-54</td>
-        <td>Fun And Games, The Promise Of The Premise</td>
+        <td>Fun &amp; Games, The Promise Of The Premise, Set Pieces</td>
         <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>
+
+        </td>
+        <td>&#x2197&#x2198</td>
+        <td>&gt;&lt;</td>
     </tr>
     <tr><td colspan="6">Midpoint</td></tr>
     <tr>
-        <td>55</td>
-        <td>Midpoint, False Victory Or False Defeat, Stakes Are Raised, Mirror Of All Is Lost</td>
+        <td>55-74</td>
+        <td>Midpoint, Mirror Of All Is Lost, False Victory Or False Defeat, Stakes Are Raised, Bad Guys Close In</td>
         <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>
+
+        </td>
+        <td>&#x2197&#x2198</td>
+        <td>&gt;&lt;</td>
     </tr>
     <tr>
-        <td>56-74</td>
-        <td>Bad Guys Close In</td>
+        <td>75-84</td>
+        <td>All Is Lost, Mirror Of Midpoint, Whiff Of Death, Dark Night Of The Soul</td>
         <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>75</td>
-        <td>All Is Lost, Whiff Of Death, Mirror Of Midpoint</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>76-84</td>
-        <td>Dark Night Of The Soul</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>
+
+        </td>
+        <td>&#x2197&#x2198</td>
+        <td>&gt;&lt;</td>
     </tr>
     <tr><td colspan="6">Act 3: Synthesis</td></tr>
     <tr>
-        <td>85</td>
-        <td>Break Into Three, Fusion of A And B</td>
+        <td>85-100</td>
+        <td>Break Into Three, Fusion Of A And B, Finale, Climax, Change The World</td>
         <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>
+
+        </td>
+        <td>&#x2197&#x2198</td>
+        <td>&gt;&lt;</td>
     </tr>
     <tr>
-        <td>86-109</td>
-        <td>Finale, Climax, Change The World</td>
+        <td>101-110</td>
+        <td>Final Image, Mirror Of Opening Image, All Arcs Are Complete Except For The One Open Question</td>
         <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>110</td>
-        <td>Final Image, Mirror Of Opening Image, Arcs Are Complete And Clear</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>
+
+        </td>
+        <td>&#x2197&#x2198</td>
+        <td>&gt;&lt;</td>
     </tr>
 </table>
 @endsection
