6fd14e02bf89fa6894c449a29ae941ee5e5537b1
Firebird <wastelandfirebird@gmail.com>
2020-11-03 01:08:47 -0700

    txt

--- resources/views/theboard.blade.php
+++ resources/views/theboard.blade.php
@@ -4,34 +4,37 @@
 
 Awaited: The Sequel
 <table>
-    <tr><td colspan="7">Act 1: Thesis</td></tr>
-    <tr><td>Chapter</td><td>Page</td><td>Beat</td><td>Setting</td><td>Summary</td><td>Emotional Trajectory
+    <tr><td colspan="8">Act 1: Thesis</td></tr>
+    <tr><td>Chapter</td><td>Title</td><td>Pages</td><td>Beats</td><td>Setting</td><td>Summary</td><td>Emotional Trajectory
  </td><td>Conflict</td></tr>
     <tr>
         <td>0</td>
+        <td>Tomorrowland</td>
         <td>1-11</td>
         <td>Opening Image, Mirror Of Final Image, Theme Hinted At, This Is Gonna Be Good, Everyday Life, 6 Things That Need Fixing, Introduce Running Gags And Callbacks, Introduce A-Story Characters Who Are Mirrors Of B-Story Characters, All Characters Have Primal Motivations, Save The Cat</td>
         <td>The first ever Tomorrowland Weekend</td>
         <td>
-FB selling tickets to an event called Tomorrowland Weekend, where postapocalyptic weirdos dress up as nonapocalyptic normies. DT introduces herself to FB, asks how to swap timelines. Hellies and Gillies introduced. "In a world gone mad you have to let loose your inner animal."
+FB selling tickets to TW, where postapocalyptic weirdos dress up as nonapocalyptic normies. DT introduces herself to FB. Hellies and Gillies introduced. FB: "When you're trying to get by in a world gone crazy sometimes you gotta go a little crazy too."
         </td>
-        <td>&#x2197&#x2198</td>
-        <td>&gt;&lt;</td>
+        <td>FB proud of TW &#x2198 FB ashamed of TW</td>
+        <td>Hellies &amp; Gilles &gt; FB</td>
     </tr>
     <tr>
         <td>1</td>
+        <td>Dorktown</td>
         <td>12-24</td>
         <td>Catalyst &amp; Debate</td>
-        <td></td>
+        <td>FB's trailer, TW, Woolies</td>
         <td>
-
+DT brings MA to FB. MA and FB and DT all get the idea to build a new Firebird in Norwood Ohio where the original plant was.
         </td>
         <td>&#x2197&#x2198</td>
         <td>&gt;&lt;</td>
     </tr>
-    <tr><td colspan="7">Act 2: Antithesis</td></tr>
+    <tr><td colspan="8">Act 2: Antithesis</td></tr>
     <tr>
         <td>2</td>
+        <td></td>
         <td>25-29</td>
         <td>Break Into Two, Hero Embarks On Adventure</td>
         <td></td>
@@ -43,6 +46,7 @@ FB selling tickets to an event called Tomorrowland Weekend, where postapocalypti
     </tr>
     <tr>
         <td>3</td>
+        <td></td>
         <td>30-54</td>
         <td>B-Story Or Love Story, Introduce B-Story Characters Who Are Mirrors Of A-Story Characters, Fun &amp; Games, The Promise Of The Premise, Set Pieces</td>
         <td></td>
@@ -52,9 +56,10 @@ FB selling tickets to an event called Tomorrowland Weekend, where postapocalypti
         <td>&#x2197&#x2198</td>
         <td>&gt;&lt;</td>
     </tr>
-    <tr><td colspan="7">Midpoint</td></tr>
+    <tr><td colspan="8">Midpoint</td></tr>
     <tr>
         <td>4</td>
+        <td></td>
         <td>55-74</td>
         <td>Midpoint, Mirror Of All Is Lost, False Victory Or False Defeat, Stakes Are Raised, Bad Guys Close In</td>
         <td></td>
@@ -66,6 +71,7 @@ FB selling tickets to an event called Tomorrowland Weekend, where postapocalypti
     </tr>
     <tr>
         <td>5</td>
+        <td></td>
         <td>75-84</td>
         <td>All Is Lost, Mirror Of Midpoint, Whiff Of Death, Dark Night Of The Soul</td>
         <td></td>
@@ -75,9 +81,10 @@ FB selling tickets to an event called Tomorrowland Weekend, where postapocalypti
         <td>&#x2197&#x2198</td>
         <td>&gt;&lt;</td>
     </tr>
-    <tr><td colspan="7">Act 3: Synthesis</td></tr>
+    <tr><td colspan="8">Act 3: Synthesis</td></tr>
     <tr>
         <td>6</td>
+        <td></td>
         <td>85-104</td>
         <td>Break Into Three, Fusion Of A And B, Finale, Change The World</td>
         <td></td>
@@ -89,8 +96,9 @@ FB selling tickets to an event called Tomorrowland Weekend, where postapocalypti
     </tr>
     <tr>
         <td>7</td>
+        <td></td>
         <td>105-110</td>
-        <td>Final Image, Mirror Of Opening Image, All Arcs Are Complete Except For The One Open Question</td>
+        <td>Final Image, Mirror Of Opening Image, All Arcs Are Complete Except For Maybe The One Open Question</td>
         <td></td>
         <td>
 
