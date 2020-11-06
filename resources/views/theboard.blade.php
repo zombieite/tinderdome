ffe79ad627d7ce25443ba3ea772b5f6bc696ea7b
Firebird <wastelandfirebird@gmail.com>
2020-11-06 13:31:15 -0700

    txt

--- resources/views/theboard.blade.php
+++ resources/views/theboard.blade.php
@@ -10,14 +10,14 @@ Awaited: Satirical Parable
         <td>0</td>
         <td>Tomorrowland</td>
         <td>1-11</td>
-        <td>Opening Image, Mirror Of Final Image, Theme Hinted At, This Is Gonna Be Good, Everyday Life, 6 Things That Need Fixing, Introduce Running Gags And Callbacks, Introduce A-Story Characters Who Are Mirrors Of B-Story Characters, All Characters Have Primal Motivations, Save The Cat</td>
+        <td>Opening Image, Mirror Of Final Image, Theme Hinted At, This Is Gonna Be Good, Everyday Life, 6 Things That Need Fixing, Introduce Running Gags, Introduce A-Story Characters Who Are Mirrors Of B-Story Characters, All Characters Have Primal Motivations, Save The Cat</td>
         <td>TW</td>
         <td>
-FB selling tickets to TW, where postapocalyptic weirdos dress up as nonapocalyptic normies. DT introduces herself to FB. Hellies and Gillies introduced. FB: "When you're trying to get by in a world gone crazy sometimes you gotta go a little crazy too."
+FB working the gate at TW. DT introduces herself. Hellies and Gillies introduced. FB: "When you're trying to get by in a world gone crazy sometimes you gotta go a little crazy too." Hellies and Gillies go to war in The Cage.
         </td>
         <td>FB proud of TW &#x2198 FB concerned about TW</td>
         <td>FB &gt; Hellies &amp; Gillies</td>
-        <td>Is a civil war brewing between the Hellies and the Gillies?</td>
+        <td>Is a civil war brewing?</td>
     </tr>
     <tr>
         <td>1</td>
@@ -26,11 +26,11 @@ FB selling tickets to TW, where postapocalyptic weirdos dress up as nonapocalypt
         <td>Catalyst &amp; Debate</td>
         <td>TW, Woolies</td>
         <td>
-DT brings MA to FB. MA and FB and DT all get the idea to build a new Firebird in Norwood Ohio where the original plant was. Tomorrowlanders burn down TW and FB saves DT from the flames.
+DT brings MA to FB. MA and FB and DT all get the idea to build a new Firebird in Norwood Ohio where the original plant was. Cage battle spills out of The Cage and tomorrowlanders burn down TW. FB saves DT from the flames.
         </td>
-        <td>TW ok &#x2198 TW destroyed</td>
+        <td>TW successful &#x2198 TW destroyed</td>
         <td>Hellies &amp; Gillies &gt; FB</td>
-        <td></td>
+        <td>Are FB and DT going to join forces?</td>
     </tr>
     <tr><td colspan="9">Act 2: Antithesis</td></tr>
     <tr>
@@ -40,11 +40,11 @@ DT brings MA to FB. MA and FB and DT all get the idea to build a new Firebird in
         <td>Break Into Two, Hero Embarks On Adventure</td>
         <td></td>
         <td>
-FB and DT meet up with MS to get funding for their quest. A second narrator joins us.
+FB and DT meet up with MS to get funding and weapons. DT begins assisting with narration.
         </td>
         <td>No money &#x2197 Money</td>
-        <td>FB &amp; DT &gt; Skelli</td>
-        <td></td>
+        <td>DT &gt; FB &amp; Skelli</td>
+        <td>Why are they going to Van Nuys instead of Norwood?</td>
     </tr>
     <tr>
         <td>3</td>
