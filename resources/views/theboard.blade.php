75b1ad2c6814164f7b583da9acdfe1f7104762ac
zombieite <387685+zombieite@users.noreply.github.com>
2020-11-25 08:29:32 +1100

    txt

--- resources/views/theboard.blade.php
+++ resources/views/theboard.blade.php
@@ -5,12 +5,12 @@
 Awaited: Satirical Parable
 <table>
     <tr><td colspan="9">Act 1: Thesis</td></tr>
-    <tr><td>Chapter</td><td>Title</td><td>Pages</td><td>Beats</td><td>Setting</td><td>Summary</td><td>Emotional Trajectory</td><td>Conflict</td><td>Urgent Question</td></tr>
+    <tr><td>Chapter</td><td>Pages</td><td>Beats</td><td>Title</td><td>Setting</td><td>Summary</td><td>Emotional Trajectory</td><td>Conflict</td><td>Question</td></tr>
     <tr>
         <td>0</td>
-        <td>Tomorrowland</td>
-        <td>1-11</td>
+        <td>1-11 (00%-10%)</td>
         <td>Opening Image, Mirror Of Final Image, Theme Hinted At, This Is Gonna Be Good, Everyday Life, 6 Things That Need Fixing, Introduce Running Gags, Introduce A-Story Characters Who Are Mirrors Of B-Story Characters, All Characters Have Primal Motivations, Save The Cat</td>
+        <td>Tomorrowland</td>
         <td>TW</td>
         <td>
 FB working the gate at TW. DT introduces herself. Hellies and Gillies introduced. FB: "When you're trying to get by in a world gone crazy sometimes you gotta go a little crazy too." Hellies and Gillies go to war in The Cage.
@@ -21,10 +21,10 @@ FB working the gate at TW. DT introduces herself. Hellies and Gillies introduced
     </tr>
     <tr>
         <td>1</td>
-        <td>Dorktown</td>
-        <td>12-24</td>
+        <td>12-24 (11%-22%)</td>
         <td>Catalyst &amp; Debate</td>
-        <td>TW, Woolies</td>
+        <td>Dorktown</td>
+        <td>TW</td>
         <td>
 DT brings MA to FB. MA and FB and DT all get the idea to build a new Firebird in Norwood Ohio where the original plant was. Cage battle spills out of The Cage and tomorrowlanders burn down TW. FB saves DT from the flames.
         </td>
@@ -35,10 +35,10 @@ DT brings MA to FB. MA and FB and DT all get the idea to build a new Firebird in
     <tr><td colspan="9">Act 2: Antithesis</td></tr>
     <tr>
         <td>2</td>
-        <td>Bakersfield</td>
-        <td>25-29</td>
+        <td>25-29 (23%-26%)</td>
         <td>Break Into Two, Hero Embarks On Adventure</td>
-        <td></td>
+        <td>Bakersfield</td>
+        <td>Woolworth's Diner</td>
         <td>
 FB and DT meet up with MS to get funding and weapons. DT begins assisting with narration.
         </td>
@@ -48,9 +48,9 @@ FB and DT meet up with MS to get funding and weapons. DT begins assisting with n
     </tr>
     <tr>
         <td>3</td>
-        <td>Van Nuys</td>
-        <td>30-54</td>
+        <td>30-54 (27%-49%)</td>
         <td>B-Story Or Love Story, Introduce B-Story Characters Who Are Mirrors Of A-Story Characters, Fun &amp; Games, The Promise Of The Premise, Set Pieces</td>
+        <td>Van Nuys</td>
         <td></td>
         <td>
 FB gets philosophical about rebuilding the world. FB and DT lay out their quest goals. They battle the deep sea diver at a servo but fail to get the lunchbox back.
@@ -62,9 +62,9 @@ FB gets philosophical about rebuilding the world. FB and DT lay out their quest 
     <tr><td colspan="9">Midpoint</td></tr>
     <tr>
         <td>4</td>
-        <td></td>
-        <td>55-74</td>
+        <td>55-74 (50%-67%)</td>
         <td>Midpoint, Mirror Of All Is Lost, False Victory Or False Defeat, Stakes Are Raised, Bad Guys Close In</td>
+        <td></td>
         <td></td>
         <td>
 
@@ -75,9 +75,9 @@ FB gets philosophical about rebuilding the world. FB and DT lay out their quest 
     </tr>
     <tr>
         <td>5</td>
-        <td></td>
-        <td>75-84</td>
+        <td>75-84 (68%-76%)</td>
         <td>All Is Lost, Mirror Of Midpoint, Whiff Of Death, Dark Night Of The Soul</td>
+        <td></td>
         <td></td>
         <td>
 
@@ -89,9 +89,9 @@ FB gets philosophical about rebuilding the world. FB and DT lay out their quest 
     <tr><td colspan="9">Act 3: Synthesis</td></tr>
     <tr>
         <td>6</td>
-        <td></td>
-        <td>85-104</td>
+        <td>85-104 (77%-94%)</td>
         <td>Break Into Three, Fusion Of A And B, Finale, Change The World</td>
+        <td></td>
         <td></td>
         <td>
 
@@ -102,9 +102,9 @@ FB gets philosophical about rebuilding the world. FB and DT lay out their quest 
     </tr>
     <tr>
         <td>7</td>
-        <td></td>
-        <td>105-110</td>
+        <td>(95%-100%)</td>
         <td>Final Image, Mirror Of Opening Image, All Arcs Are Complete Except For Maybe The One Open Question</td>
+        <td></td>
         <td></td>
         <td>
 
