3b800b8ae5b811c585ddc6703c47cc02d0a35297
zombieite <387685+zombieite@users.noreply.github.com>
2021-04-19 08:17:33 +1000

    txt

--- resources/views/mission_status.blade.php
+++ resources/views/mission_status.blade.php
@@ -123,19 +123,17 @@
     <li>
         @foreach ($upcoming_events_and_signup_status as $upcoming_event)
             @if ($upcoming_event->attending_event_id)
-                You have viewed all profiles.
-                @if ($recently_updated_users)
-                    <a href="/profile/{{ $recently_updated_users[0]->id }}/{{ $recently_updated_users[0]->wasteland_name_hyphenated }}?review=1" class="bright">Some profiles were recently updated</a>.
-                @else
-                    Come back later to see new arrivals.
-                @endif
-                @php ($viewed_all = 1)
-                @break
+                You have viewed all profiles. Come back later to see new arrivals.
             @endif
+            @php ($viewed_all = 1)
+            @break
         @endforeach
         @if (isset($viewed_all))
         @else
             Let us know who you'd like to meet.
+        @endif
+        @if ($recently_updated_users)
+            <a href="/profile/{{ $recently_updated_users[0]->id }}/{{ $recently_updated_users[0]->wasteland_name_hyphenated }}?review=1" class="bright">Some profiles were recently updated</a>.
         @endif
     </li>
 @endif
