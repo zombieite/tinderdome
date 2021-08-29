0e09bd8fddf4b0cc06593f273a70948678e54ac8
zombieite <387685+zombieite@users.noreply.github.com>
2021-08-29 20:50:28 +1000

    renaming

---
+++ resources/views/layouts/heads-will-rock.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Heads Will Rock: A chronicle of postapocalyptic mayhem">
+<meta property="og:url" content="http://youareawaited.com/heads-will-rock">
+<meta property="og:image" content="http://youareawaited.com/h-w-r/heads-will-rock.jpg">
+<meta property="og:description" content="Is it possible to retain your humanity in a world gone mad?">
+<meta name="description" content="Is it possible to retain your humanity in a world gone mad?">
+<title>Heads Will Rock: A chronicle of postapocalyptic mayhem</title>
+<link href="/css/app.css?rev=3" rel="stylesheet">
+</head>
+<body>
+@yield('content')
+<hr>
+For updates, <a href="https://www.facebook.com/YouAreAwaited">follow us on another social network</a>. For films, <a href="https://www.youtube.com/wastelandfirebird">follow us on this video site</a>. Contact <a href="mailto:wastelandfirebird@gmail.com">wastelandfirebird@gmail.com</a> (<a href="/profile/Firebird">Firebird</a>) with questions or comments.
+</body>
+</html>
