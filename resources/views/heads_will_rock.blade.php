dd6ac216e6bcc69c3119bde318a15ec1d1d6bfbc
zombieite <387685+zombieite@users.noreply.github.com>
2021-08-29 21:15:40 +1000

    moving book stuff

--- resources/views/heads_will_rock.blade.php
+++ resources/views/heads_will_rock.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<img style="width:100%;filter:none;" src="/stories/awaited-nonfictional-delusion.jpg">
+<img style="width:100%;filter:none;" src="/h-w-r/heads-will-rock.jpg">
 
 <p>
 "Not many people have written books in the past 38 years. So nowadays every book that gets written sort of has to be a history book and a philosophy book and a survival book too. I'd be doing you a disservice if I didn't tell you what I know about how things got to be the way they are and offer you some ideas about how to get things back to the way they were."
