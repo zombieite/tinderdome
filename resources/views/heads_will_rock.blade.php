f0b917f8c1703fedf9a7cac95d05f9a4f96ff374
John Binns <john_binns@yahoo.com>
2021-08-29 20:25:01 +1000

    relocate books

--- resources/views/heads_will_rock.blade.php
+++ resources/views/heads_will_rock.blade.php
@@ -5,53 +5,18 @@
 <img style="width:100%;filter:none;" src="/stories/awaited-nonfictional-delusion.jpg">
 
 <p>
-Too implausible to be an autobiography. Too real to be fiction. It's a missing instruction manual for life. This book will help you make friends. This book will help you make money. This book might even save your life. Two years in the making, this tiny, 143-page book has been meticulously engineered to evoke a sense of hope during the apocalypse. It will inspire you to change your life and change the world. Anarchy, attachment, cars, economics, entrepreneurship, entropy, ethics, grief, guilt, guns, kindness, self-reliance, seventies nostalgia, and stoicism. This is the You Are Awaited story. Awaited: Nonfictional Delusion, by John Binns.
+"Not many people have written books in the past 38 years. So nowadays every book that gets written sort of has to be a history book and a philosophy book and a survival book too. I'd be doing you a disservice if I didn't tell you what I know about how things got to be the way they are and offer you some ideas about how to get things back to the way they were."
+</p>
+<p>
+This is the story of how we killed some people while trekking across the USA on Route 66 trying to rediscover the American Dream. 22000 words. 110 pages. 5 illustrations. Short sentences. No commas. No descriptions. Words and actions only. Give me two hours of your life. I'm not gonna waste your time.
 </p>
 
 <div class="profile_search_block" style="vertical-align:top;">
-<h1><a href="https://www.lulu.com/shop/john-binns/awaited-nonfictional-delusion/paperback/product-24218988.html">Paperback</a></h1>
+<h1><a href="https://www.lulu.com/en/us/shop/john-binns-and-steve-scholz-and-marky-make-up/heads-will-rock/paperback/product-r2vrpv.html?page=1&pageSize=4">Paperback</a></h1>
 </div>
 
 <div class="profile_search_block" style="vertical-align:top;">
-<h1><a href="/stories/awaited-nonfictional-delusion.pdf">PDF</a></h1>
-</div>
-<br>
-
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-000000.txt">Chapter 00 (txt)</a><br><br>
-</div>
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-012501.txt">Chapter 01 (txt)</a><br>
-</div>
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-022502.txt">Chapter 02 (txt)</a><br>
-</div>
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-033011.txt">Chapter 03 (txt)</a><br>
-</div>
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-043508.txt">Chapter 04 (txt)</a><br>
-</div>
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-053604.txt">Chapter 05 (txt)</a><br>
-</div>
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-063608.txt">Chapter 06 (txt)</a><br>
-</div>
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-073701.txt">Chapter 07 (txt)</a><br>
-</div>
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-083704.txt">Chapter 08 (txt)</a><br>
-</div>
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-093708.txt">Chapter 09 (txt)</a><br>
-</div>
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-103711.txt">Chapter 10 (txt)</a><br>
-</div>
-<div class="profile_search_block" style="width:13em;height:20em;vertical-align:top;">
-<a href="/txt/awaited_nonfictional_delusion-113808.txt">Chapter 11 (txt)</a><br>
+<h1><a href="/heads-will-rock/heads-will-rock.pdf">PDF</a></h1>
 </div>
 
 @endsection
