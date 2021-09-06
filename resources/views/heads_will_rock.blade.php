cdb5fb3e8f663f02ce665881ac7c9ddffd2e7cf6
zombieite <387685+zombieite@users.noreply.github.com>
2021-09-06 23:44:45 +1000

    hwr

--- resources/views/heads_will_rock.blade.php
+++ resources/views/heads_will_rock.blade.php
@@ -1,27 +1,22 @@
 @extends('layouts.heads_will_rock')
-
+@php
+$hwr = "https://www.lulu.com/en/us/shop/john-binns-and-steve-scholz-and-marky-make-up/heads-will-rock/paperback/product-r2vrpv.html"
+@endphp
 @section('content')
 
-<img style="width:100%;filter:none;" src="/h-w-r/heads-will-rock.jpg">
+<a href="{{ $hwr }}"><img style="width:100%;filter:none;" src="/h-w-r/heads-will-rock.jpg"></a>
 
-<p>
+<p class="small">
 "Not many people have written books in the past 38 years. So nowadays every book that gets written sort of has to be a history book and a philosophy book and a survival book too. I'd be doing you a disservice if I didn't tell you what I know about how things got to be the way they are and offer you some ideas about how to get things back to the way they were."
 </p>
-<p>
+<p class="small">
 This is the story of how we killed some people while trekking across the USA on Route 66 trying to rediscover the American Dream. 22000 words. 110 pages. 5 illustrations. Short sentences. No commas. No descriptions. Words and actions only. Give me two hours of your life. I'm not gonna waste your time.
 </p>
 
-<div class="profile_search_block" style="vertical-align:top;">
-<h1><a href="https://www.lulu.com/en/us/shop/john-binns-and-steve-scholz-and-marky-make-up/heads-will-rock/paperback/product-r2vrpv.html?page=1&pageSize=4">Paperback</a></h1>
-</div>
-
-<div class="profile_search_block" style="vertical-align:top;">
-<h1><a href="/h-w-r/heads-will-rock.pdf">PDF</a></h1>
-</div>
-
-<hr>
+<a href="{{ $hwr }}"><img style="width:100%;" src="/h-w-r/heads-will-rock.png"></a>
 
 <div class="profile_search_block" style="width:20em;vertical-align:top;">
+<span class="small">Act I</span><br>
 <a class="small bright" href="/heads-will-rock-a-chronicle-of-postapocalyptic-mayhem/chapter-0-tomorrowland">Chapter 0: Tomorrowland</a>
 <br><br>
 Firebird is the gatekeeper at an event he calls Tomorrowland Weekend. He saves the life of a man who turns out to be the leader of a tribe called the Hellies. The Hellies are at war with a rival tribe known as the Gillies. The Gillies turn against Firebird for saving the Helly leader.
@@ -36,6 +31,7 @@ A girl named Dorktown inspires both Firebird and his nemesis, Mohawk Asshole, to
 </div>
 
 <div class="profile_search_block" style="width:20em;vertical-align:top;">
+<span class="small">Act II</span><br>
 <a class="small bright" href="/heads-will-rock-a-chronicle-of-postapocalyptic-mayhem/chapter-2-bakersfield">Chapter 2: Bakersfield</a>
 <br><br>
 Firebird and Dorktown try to secure funding and weapons for their endeavour. 
@@ -50,6 +46,7 @@ As Firebird and Dorktown chase Mohawk Asshole and Chumdumpster to the old GM fac
 </div>
 
 <div class="profile_search_block" style="width:20em;vertical-align:top;">
+<span class="small">Midpoint</span><br>
 <a class="small bright" href="/heads-will-rock-a-chronicle-of-postapocalyptic-mayhem/chapter-4-seligman">Chapter 4: Seligman</a>
 <br><br>
 Tensions rise between Firebird and Dorktown. Firebird refuses to show his allegiance to the Hellies and in doing so he makes an enemy of them, too. The Hellies and the Gillies converge on Firebird's hideout in Seligman, Arizona and do battle.
@@ -64,6 +61,7 @@ Firebird comes to the conclusion that it might not be possible to retain one's h
 </div>
 
 <div class="profile_search_block" style="width:20em;vertical-align:top;">
+<span class="small">Act III</span><br>
 <a class="small bright" href="/heads-will-rock-a-chronicle-of-postapocalyptic-mayhem/chapter-6-norwood">Chapter 6: Norwood</a>
 <br><br>
 Will a new Pontiac Firebird be built in Norwood? Or will the Hellies and the Gillies burn it all down?
@@ -75,6 +73,16 @@ Will a new Pontiac Firebird be built in Norwood? Or will the Hellies and the Gil
 <br><br>
 The victors celebrate, while the losers mourn... and plan their revenge.
 <br><br>
+</div>
+
+<hr>
+
+<div class="profile_search_block" style="vertical-align:top;">
+<h1><a href="{{ $hwr }}">Paperback</a></h1>
+</div>
+
+<div class="profile_search_block" style="vertical-align:top;">
+<h1><a href="/h-w-r/heads-will-rock.pdf">PDF</a></h1>
 </div>
 
 @endsection
