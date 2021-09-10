58f4f561fef7180d824a3b563ee7b9510d55556c
zombieite <387685+zombieite@users.noreply.github.com>
2021-09-10 22:09:00 +1000

    txt

--- resources/views/comment_form.blade.php
+++ resources/views/comment_form.blade.php
@@ -1,7 +1,7 @@
 <form action="{{ $action }}" method="POST">
     {{ csrf_field() }}
     <input type="hidden" name="commented_upon_user_id" value="{{ $user_id_to_rate }}">
-    <label for="comment">You can comment on this profile. Comments must be approved.</label>
+    <label for="comment">You can comment on this profile. Comments must be approved. Emojis are not supported.</label>
     <br>
     <input type="text" name="comment" style="width:100%;" maxlength="280">
     <input type="submit" value="Submit comment" class="yesyes">
