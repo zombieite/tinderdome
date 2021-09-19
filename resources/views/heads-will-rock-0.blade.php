36469a20899eeb6f2c3c7183c918998c65876ea4
zombieite <387685+zombieite@users.noreply.github.com>
2021-09-19 19:09:03 +1000

    add chap 0

--- resources/views/heads-will-rock-0.blade.php
+++ resources/views/heads-will-rock-0.blade.php
@@ -1,7 +1,100 @@
 @extends('layouts.heads_will_rock')
-
 @section('content')
-
-<img style="width:100%;filter:none;" src="/h-w-r/heads-will-rock.jpg">
-
+<h1>CHAPTER 0: TOMORROWLAND</h1>
+<p>"Tomorrowland Weekend! Where the Big Bang never happened! You are awaited at Tomorrowland Weekend!"</p><p>
+Carnival barking isn't my style so I paid a bloke to shout this stuff for me.</p><p>
+"Tomorrowland Weekend! No suffering! No hunger! No politics! No war!" </p><p>
+Some of my other employees were putting up the new sign that read "Tomorrowland" and taking down the old sign that read "The Gathering." The Gathering was this annual event in the California desert where people came from all over the world to meet up with family and friends. This year I'd paid for the whole thing myself and jazzed it up to look like a parallel universe where the apocalypse never happened.</p><p>
+"Tomorrowland Weekend! The shoes have shoelaces. The cars have guzzoline. The streets are paved with asphalt! You must have a Firebuddy and a costume to enter."</p><p>
+The Firebuddy was a "lappy" computer I'd invented. I called it a "lappy" cuz it was small enough to fit in your lap. It was Firebuddy sales that had paid for this whole event.</p><p>
+As for the costumes well my plan was that we would all dress up in our best prewar outfits. And instead of camping in boring old tents we'd build little office buildings and flower shops and libraries and stuff. We were gonna momentarily recreate the feeling of living in a world that was beautiful and boring. </p><p>
+People loved the idea. Hundreds of people were lined up at the gate and I was the gatekeeper.</p><p>
+The next festival attendee waiting in line was an impeccably dressed man. Suit and tie and hat and briefcase. It had been 38 years since the Big Bang so I was sure it had been that long since I'd seen someone dressed up that nice. I remembered being a kid and seeing my dad off to work and he'd be dressed like that.</p><p>
+"What are you dressed up as?" I asked.</p><p>
+"I'm an insurance salesman" he said. "Hi. How are you. How are the wife and kids? Can I interest you in a policy of insurance?" He handed me his business card.</p><p>
+"Nailing it. Where'd you get the suit?"</p><p>
+"Found it in a bunker."</p><p>
+"Far out." I said.</p><p>
+Next guy had on a red hat and a yellow coat. He carried an axe. He offered me a metal bucket full of coins for a Firebuddy but I was more interested in that axe.</p><p>
+"Nice fireman gettup. That axe is in great shape."</p><p>
+"The head's a little loose" he said. He handed it to me so I could take a closer look.</p><p>
+"Looks good to me. Wanna keep your shrapnel and barter me this instead?"</p><p>
+He took me up on the deal and handed me his business card.</p><p>
+A woman in a homemade police officer uniform was next. She looked at me accusingly. "Have you been doing crimes?"</p><p>
+"Crimes against humanity" I replied.</p><p>
+"Remain silent!" she ordered. She handed me a frame holding a torn and stained twenty dollar bill pressed between two sheets of clear plastic.</p><p>
+"You don't have coins?" I asked.</p><p>
+"This is all I've got."</p><p>
+"You're gonna have to take it out of there. And you're not getting any change."</p><p>
+She borrowed my axe to break open the frame. "Head's a little loose" she said.</p><p>
+I took another look at the axe and shrugged. She handed me her business card.</p><p>
+The business cards went into my Pac Man lunchbox. The money went into my Six Million Dollar Man lunchbox. Both were overflowing.</p><p>
+One person was dressed as a race car driver. Another was a movie star. There was a baseball player and a chef and a nun. There was even a thief but to me she looked more like the Hamburglar.</p><p>
+The prewars were living out their stolen childhood dreams. The postwars were dreaming of a world they'd never known.</p><p>
+The next customer in line was a teenage girl. She asked "Are you Firebird?"</p><p>
+She had a lovely Australian accent. It was nice to hear an Aussie again. When I was a little tacker I lived in the USA. But when the Big Bang happened my family was visiting Australia so I ended up stuck there for 25 years. I kinda missed the place.</p><p>
+"Used to be" I said. "But my Firebird was smashed to bits. I might have to change my name."</p><p>
+"What do you drive now?"</p><p>
+"77 Ford Pinto."</p><p>
+"Ok Pinto" she said. "Will you sign my book?" She handed me a copy of a book I'd written last year called Awaited: Nonfictional Delusion. "Make it out to Torkdown."</p><p>
+"Dorktown?"</p><p>
+"Torkdown."</p><p>
+"To Dorktown" I read aloud as I wrote. "Love Pinto."</p><p>
+"How can I get to Tomorrowland?" she asked.</p><p>
+"Buy a Firebuddy" I told her. "And get a costume. What are you supposed to be? A homeless orphan? You can't wear that here. It breaks immersion."</p><p>
+"Not Tomorrowland Weekend" the girl said. "The real Tomorrowland. Like in your book."</p><p>
+My book was about a world called Tomorrowland. Tomorrowland was just like our world except that in Tomorrowland the Big Bang never happened. A lot of people read that book and got it into their heads that Tomorrowland was a real place you could go and visit. That's what gave me the idea for Tomorrowland Weekend. It would be a chance for us all to live in the world I'd written about. A place to make history and a place to make the future.</p><p>
+"You gotta buy a Firebuddy." I repeated. "You gotta dress up. Then I let you in. While you're in there you might teleport into the real Tomorrowland just like in my book. Or you might just end up camping for a few unremarkable days. No guarantees. Are you buying a Firebuddy or what?"</p><p>
+"I don't have any money" she said.</p><p>
+"Hunkidory!" I shouted.</p><p>
+My hired macho man appeared.</p><p>
+"Take her to the highway. Let her hitchhike back to Australia."</p><p>
+At that moment a skirmish broke out in line and me and Hunkidory got distracted. The girl ran off.</p><p>
+The race car driver and the movie star I mentioned earlier had knives in their hands and they had the baseball player on the ground. He was unarmed. They'd already cut him up good and they were about to finish the job.</p><p>
+"Show us what he looks like on the inside!" somebody shouted.</p><p>
+But then someone else shouted "No fighting at the Gathering except for in the Cage!"</p><p>
+The fighting paused and all eyes turned to me. I wasn't a fan of the Cage but it was pretty much an institution at this point. The Cage was where instead of fighting in the streets you'd lock two people in a ring together and let them have it out.</p><p>
+"It's not the Gathering anymore" I said. "It's Tomorrowland Weekend. But the rule still applies."</p><p>
+"You don't understand" the race car driver explained. "He's the president of the Hellies!"</p><p>
+Someone shouted "Kill all Hellies!"</p><p>
+I turned to Hunkidory. "What's a Helly?" </p><p>
+Hunkidory grunted.</p><p>
+The movie star said "We're not inside the Gathering yet. The rules don't apply." She pointed to some orange tape I'd nailed to the ground to mark the boundaries of the event.</p><p>
+I had to think fast if I didn't want a dead body stinking up the line. I went over to the tape on the ground and pulled it up and away from the entrance gate. I unspooled some more tape and walked it around them and back to the gate. I put some more nails in the ground to hold the tape in place. The three wankers were now within the boundaries of the event.</p><p>
+"Welcome to Tomorrowland Weekend" I said.</p><p>
+"See you in the Cage Helly" the race car driver said to the baseball player. He threw his business card at me.</p><p>
+His card read "Kill all Hellies! Dr. Freeky Freedums. President of the Gillies."</p><p>
+"What's a Gilly?" I asked Hunkidory.</p><p>
+Hunkidory grunted.</p><p>
+The baseball player got up. "You did the right thing. Welcome to the winning side of history." He gave me his business card.</p><p>
+"I'm not on any side of history" I replied.</p><p>
+His business card was covered in blood. It read "Death to Gillies! Count Bartholomew Bubury. President of the Hellies."</p><p>
+Later that night hundreds of people stood outside the Cage screaming and cheering and banging on the fence. It was my job to step into the center of it all and give a speech to get everyone riled up.</p><p>
+"Welcome to the first ever Tomorrowland Weekend!" I shouted into the microphone.</p><p>
+The crowd went wild.</p><p>
+"You've come from all over the world to be here tonight. Some of us are here to find thems we're looking for and thems we've lost. Some of us are here to have a good time. Some of us are here to make money. But every one of us is here because we wanna build a better tomorrow!" I paused for a big cheer but didn't get one.</p><p>
+"Death to Gillies!" someone shouted.</p><p>
+"Kill all Hellies!" someone retorted.</p><p>
+They all started shouting back and forth and I had to shout into the microphone again to get them to shut up.</p><p>
+"Death comes soon enough!" I said. "It doesn't matter if you're a Helly or a Gilly. What matters is that we are all Tomorrowlanders!"</p><p>
+A bottle flew at me from a random direction. It was time to give these primates what they wanted.</p><p>
+"Dollars to wingnuts every one of us here tonight has killed someone at some point in our lives. But here in the Cage we kill each other in a civilized manner!"</p><p>
+Now the crowd was getting worked up again.</p><p>
+"Tonight's Cage match is not just any Cage match. It's a battle between two presidents. In this corner we have Count Bartholomew Bubury. President of the Hellies!"</p><p>
+Half the crowd cheered and half the crowd booed.</p><p>
+"And in this corner we have Dr. Freeky Freedums. President of the Gillies!"</p><p>
+Half the crowd booed and half the crowd cheered.</p><p>
+I handed swords to both of them and got out of there. I handed the microphone to my carnival barker.</p><p>
+"You're not gonna stay and watch?" he asked.</p><p>
+"I'm trying to retain my humanity" I said.</p><p>
+"Good luck with that."</p><p>
+I went back to my trailer and fired up my Firebuddy. Now if you don't have a Firebuddy let me explain what you're missing.</p><p>
+Firebuddies connect with other computers to form a computer network. On the network you can communicate with friends and strangers. You can teach or learn. You can create or consume. You can build a "netsite" to manipulate data in any way you can imagine.</p><p>
+I'd built a "social network" called You Are Awaited. People could share pictures and news articles and comment on them. It started out friendly and positive. But lately things had changed. People had started posting things like "If you're a Helly we can't be friends" and "Gillies should be lined up and shot."</p><p>
+This wasn't like the old days of Democrats vs. Republicans. I once saw footage of Gore Vidal and William F. Buckley debating. They hurled some insults at each other but for the most part they used big words and behaved like grownups. The Hellies and the Gillies explicitly eschewed intellectualism and had instead decided to be angry at each other in the stupidest ways possible.</p><p>
+Now I know a lot of you readers out there are Hellies and Gillies and I know you care a lot about your cause. And I ain't saying your cause ain't just. I know ideas have consequences and sometimes those consequences are life and death. And I'm sure somewhere people are debating these ideas rationally and if you're doing that then good for you.</p><p>
+But my You Are Awaited topic promotion algorithm hadn't sparked reasoned debate. It highlighted the differences between people. Rather than working out those differences people just decided they hated anyone who wasn't on their side.</p><p>
+Ideas that seem obvious to half of humanity can terrify and infuriate the other half. The algorithm promoted discussions of topics like these simply because it noticed people were more engaged in these discussions. My algorithm had radicalized both sides and no one was listening to each other.</p><p>
+The Hellies and Gillies weren't just doomed to repeat history. They were downright excited about it. My dream of bringing humanity together had ended up tearing it in half.</p>
 @endsection
+
