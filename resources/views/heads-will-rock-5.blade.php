458695929c540fdacf16e680fd1e1062130ce57d
John Binns <john_binns@yahoo.com>
2021-09-19 19:46:05 +1000

    added chapter contents

--- resources/views/heads-will-rock-5.blade.php
+++ resources/views/heads-will-rock-5.blade.php
@@ -1,7 +1,98 @@
 @extends('layouts.heads_will_rock')
-
 @section('content')
-
-<img style="width:100%;filter:none;" src="/h-w-r/heads-will-rock.jpg">
-
+PREVIOUS: <a href="/heads-will-rock-a-chronicle-of-postapocalyptic-mayhem/chapter-4-seligman">/heads-will-rock-a-chronicle-of-postapocalyptic-mayhem/chapter-4-seligman</a>
+<h1>CHAPTER 5: AMARILLO</h1>
+<p>That night I snuck the Frankenbird out of Seligman with the headlights off. Once I was far enough away I turned the headlights on and picked up the pace.</p><p>
+I kept my eyes to the sky looking for helicopters but because of that I didn't notice that Mohawk Hineyhole was discreetly following me in the Pinto with Chumdumpster behind him on the Elsinore. I figured this out when they stopped being discreet about it and the Pinto started ramming the Frankenbird. Mohawk Hineyhole was clearly not afraid of blowing himself up in the Pinto as long as he could take me with him.</p><p>
+To make things worse they hadn't been as careful about sneaking out of town as I'd been. They had one of those little round helicopters on their tail. I think it was a Loach but my helicopter expert has gone AWOL. Fortunately it had no guns mounted on it so it wasn't shooting at us. I kicked it in the guts and the Frankenbird left them all behind.</p><p>
+I was outdriving my headlights probably going 200 kays when I saw that upside down bug. The upside down bug with the dead family inside. The upside down bug that was right before the wreckage of the tanker truck. The tanker truck that was splayed out across the entire road.</p><p>
+I stood on the brake pedal and all four wheels locked up. The tanker truck came into view but the Frankenbird wasn't stopping. The Frankenbird bumped into the tanker trailer but not hard enough to do any damage.</p><p>
+I'd almost died but I didn't have time to dilly dally. Mohawk Hineyhole and Chumdumpster would be here any minute.</p><p>
+Then it hit me. They'd be here any minute. At top speed. With no headlights.</p><p>
+Now I know you probably bought this book cuz you like action and adventure. I hate to tell you this but that's not what this book is about. It's about my life and Dorktown's life. Our lives haven't always been good. Adventures don't always turn out how you want them to and action is something we'd all rather avoid. So you're not supposed to enjoy this part.</p><p>
+I turned off the Frankenbird's lights. I slowly drove around the tanker. I turned around and parked fifty meters beyond it. I shut off the motor. I got out of the Frankenbird and waited.</p><p>
+A minute later I heard the Pinto's engine approaching. The engine was screaming. Tex's carb was at the top of its game. The Elsinore and the chopper joined in the chorus.</p><p>
+Then there was a massive thud that shook the ground under my feet. No explosion. Just a thud. </p><p>
+Then there was a smaller thud and silhouetted against the stars I could see a humanlike figure spinning end over end. It was Chumdumpster. When he hit the ground his metal suit made a splash of sparks. He skidded right by me and off into the distance.</p><p>
+I grabbed my bow and walked back to the wreckage.</p><p>
+I'd wanted to make Mohawk Hineyhole explode just like he'd made my family explode all those years ago. But that's not what happened. On the other side of that trailer there was a human being named Bruce Barbagallo tangled up and dying inside a crumpled heap of steel.</p><p>
+Now I'm no doctor but my diagnosis was that Bruce didn't really have legs anymore and he had about five minutes left before he found out which religion was the right one.</p><p>
+"Where's Steven?" he asked.</p><p>
+"Steven? Is that your son?"</p><p>
+"Yes. Steven."</p><p>
+I went over to the remains of the Elsinore and got my Six Million Dollar Man lunchbox full of money out of the saddlebag. "He went off his bike and slid halfway to Gallup."</p><p>
+"Tell him I love him more than anything."</p><p>
+"I'm not gonna tell him that."</p><p>
+"Please. Tell him I love him more than anything."</p><p>
+I sighed. "Ok."</p><p>
+The Loach came down for a landing behind us. The landing was slow and hesitant at first. Then it was sudden and awkward. I nocked an arrow and readied my bow but to my surprise it was Dorktown who stepped out.</p><p>
+I lowered my bow. "You stole a helicopter?"</p><p>
+"I borrowed it" she said. "Is he dead?"</p><p>
+"No."</p><p>
+"Good. Then there's still time to kill him." She reached into the mangled Pinto and started tugging on Bruce's arms.</p><p>
+I forced myself between them. "Dorktown stop it" I said. "You're gonna tear him in half."</p><p>
+"Do it you cunt" Bruce said.</p><p>
+"Listen to me" I implored. "Chumdumpster is Bruce's son."</p><p>
+"And I'm Messenger Kid's daughter."</p><p>
+"I know but if you do this Chumdumpster is gonna come for you."</p><p>
+"I don't give a rat's ass. Are we gonna assassinate this asshole or what?</p><p>
+"We're not assassinating anyone" I said.</p><p>
+"What do you mean?"</p><p>
+"This isn't political."</p><p>
+"If you kill a president it's gonna be political."</p><p>
+Ok she had a point there. "Yeah but if you drop him out of that helicopter this war is never gonna end."</p><p>
+She turned and went back to the helicopter. She returned with the crossbow and pointed it at me. "This war ends when we win it" she said.</p><p>
+I readied my bow and pointed it at her. "Is this what your pop would have wanted?"</p><p>
+Bruce spoke up. "Your pop? I thought you looked familiar. Your pop was the dumb guy."</p><p>
+"He wasn't dumb!" she shouted. "He was the smartest man I ever knew."</p><p>
+"He couldn't talk" Bruce said.</p><p>
+"He talked with his hands!" she replied.</p><p>
+"Your pop knew where John was. But the only way I could get it out of him was if I had you there to translate."</p><p>
+"You tortured him and killed him while I watched." </p><p>
+"Yes I did. And to his credit it was only when I realized I was torturing the wrong person that he started waving his hands around and telling me what I wanted to know."</p><p>
+"He was the only person on earth who loved me and now he's dead."</p><p>
+"I did what I had to do" Bruce said.</p><p>
+I didn't expect him to say that. "What do you mean?" I asked.</p><p>
+"I was saving the world" he answered.</p><p>
+"From what?"</p><p>
+"From you" he whispered.</p><p>
+I didn't have to include this part so consider yourself lucky. I'm writing this book so I get to be the hero. But everyone's the hero of their own story. Even the villains. Especially the villains. All of history's greatest atrocities were committed in the name of truth and justice. The most dangerous person on earth is someone who's sure that they're right. But how can anyone be sure?</p><p>
+Fuck it. Who gives a shit.</p><p>
+I turned to face Bruce and shot an arrow into his heart. I could tell you about the look in his eyes or the sounds that he made but I really don't wanna do that.</p><p>
+I looked up and saw Steven standing there looking at me. Don't worry. Chumdumpster is not gonna stay Steven for long but for the moment he's Steven.</p><p>
+Steven's impressive tumble had been a death defying one but his metal suit was leaking blood from all of its seams. He looked like a robot oozing hydraulic fluid.</p><p>
+I'd witnessed Bruce killing my family and now Steven had witnessed me killing his. We all become an orphan someday. If we're lucky.</p><p>
+"Your father wanted me to tell you that he loved you more than anything" I said.</p><p>
+I backed away but Steven didn't come after me. Instead he reached into the car and tried to revive his now very totally 100% dead dad.</p><p>
+Then Dorktown turned her crossbow around and pointed it at her chest.</p><p>
+I threw my bow aside and ran to her but she pulled the trigger before I could get there. Fortunately she had the thing so close to her chest that the bolt didn't have anywhere to go. The crossbow flew out of her hands and she fell backward onto the road. She hit her head and and it was lights out for Dorktown.</p><p>
+I crouched down beside her. The bolt was lying on her chest. It had ripped through her clothing and she was bleeding but it hadn't broken her sternum.</p><p>
+"If you die there won't be anyone left in my fan club" I said. I picked her up and started carrying her to the Frankenbird as quickly as I could.</p><p>
+Chumdumpster shambled after us but he was too slow. So he turned around and climbed into the Loach. The rotors spun up and the thing took off but he clearly had no idea what he was doing. As soon as the helicopter left the ground it flipped over and landed on its side sending pieces of the rotors flying in every direction. Then the whole thing exploded but Chumdumpster emerged from the flames like he didn't even care. I guess I shouldn't say he emerged from the flames cuz in fact he took some of the flames with him. He was on fire.</p><p>
+I got Dorktown into the Frankenbird and started it up. Chumdumpster hobbled over to that upside down Volkswagen Beetle convertible. He lifted it up and flipped it over. He threw out the dead dad in the driver's seat but left the rotting corpses of the wife and kids sitting right where they were. He got the car started and came after us. He was still on fire.</p><p>
+We had no problem staying ahead of him but it was clear he was not gonna give up. When I stopped for petrol in Albuquerque I heard two truck drivers talking about a flaming metal monster they saw on the side of the road having a picnic with three dead bodies.</p><p>
+The rest of the night was a blur. At some point in the morning I fell asleep at the wheel. I woke up in terror when road signs and fenceposts started smacking into the Frankenbird's bonnet. I stopped the car and got out. We were somewhere in Texas.</p><p>
+I looked around a bit and figured out that we weren't just somewhere in Texas. We were somewhere special in Texas.</p><p>
+Dorktown woke up and joined me. "What is this place?" she asked.</p><p>
+"Cadillac Ranch" I answered.</p><p>
+"What's Cadillac Ranch?"</p><p>
+"This place."</p><p>
+Cadillac Ranch is nothing more than a place where some cars are lined up and buried halfway in the dirt sticking up almost vertically. The place is strange enough as it is but to make it even stranger someone had left an old piano sitting out there as well.</p><p>
+"You were out for ten hours" I said.</p><p>
+"I told you I'd sleep when Mohawk Hineyhole was dead."</p><p>
+Yeah that's right. She didn't curse.</p><p>
+We climbed onto one of the cars and had a seat in the boot. Dorktown's arm was pressed against mine. It was nice. I usually tried not to touch her but I liked it when every now and then she'd touch me. But don't go getting any ideas. If you haven't figured out what we are to each other by now then you ain't never gonna figure it out.</p><p>
+"I'm a murderer Dorktown."</p><p>
+She grabbed my hand. "You're not a murderer. You're just a killer."</p><p>
+Not much happened for a while.</p><p>
+There's a thing called narrative filigree where a writer will just write a bunch of fancy stuff to take up some space. I would try that here but I'm not a writer. I'm just someone who's trying to tell you a bunch of stuff that happened.</p><p>
+Words. Words will get you into trouble. Words will keep you up at night. The weather wasn't hot or cold or stormy. No red-tailed hawk cried out above us. No roadrunner dashed in front of us. Nothing to see. Nothing to hear. Nothing to do. Texas was flat and lovely and I sat there with Dorktown and her arm was pressed against mine.</p><p>
+What did Skelli whisper to you?</p><p>
+She said "If you kill someone you don't have to live with them anymore. You have to live with yourself."</p><p>
+Dorktown climbed down from the car and took a seat at the piano. She started playing and singing. I didn't know she could play. I didn't know she could sing. And if I had known that she could do that stuff well I sure as heck wouldn't have thought that she would.</p><p>
+The song she played for me was Close To You by The Carpenters. The lullaby my mom used to sing to me as a child. The piano was out of tune and some of the keys were missing but it didn't matter.</p><p>
+When she was done she got up and said "I'm not letting you give up on your dream."</p><p>
+"Why not?" I asked.</p><p>
+"Because it's not just your dream anymore."</p>
+NEXT: <a href="/heads-will-rock-a-chronicle-of-postapocalyptic-mayhem/chapter-6-norwood">/heads-will-rock-a-chronicle-of-postapocalyptic-mayhem/chapter-6-norwood</a>
 @endsection
