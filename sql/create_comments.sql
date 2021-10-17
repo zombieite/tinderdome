9570dc2e89dcd116f9325dc655a8c85e6c7f1f73
zombieite <387685+zombieite@users.noreply.github.com>
2021-10-17 23:13:56 +1100

    Revert "emoji support in comments"
    This reverts commit 28ad658b48e05625c1a2ec2576a2ffd74cb0aff2.

--- sql/create_comments.sql
+++ sql/create_comments.sql
@@ -2,7 +2,7 @@ USE tinderdome;
 
 CREATE TABLE comment (
     comment_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    comment_content VARCHAR(280) CHARACTER SET utf8mb4 NOT NULL,
+    comment_content VARCHAR(280) NOT NULL,
     commenting_user_id INT NOT NULL,
     commented_on_user_id INT NOT NULL,
     approved BOOLEAN DEFAULT 0,
