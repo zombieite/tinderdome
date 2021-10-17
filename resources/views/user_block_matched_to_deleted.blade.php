52f80eee80683790564da484dd92d8377ebaa993
zombieite <387685+zombieite@users.noreply.github.com>
2021-10-17 19:03:15 +1100

    allow blocked user to mark mission as completed

--- resources/views/user_block_matched_to_deleted.blade.php
+++ resources/views/user_block_matched_to_deleted.blade.php
@@ -1,5 +1,10 @@
 <div class="mission_match">
-Matched to deleted user;<br>mission incomplete
+Matched to deleted user
 <br>{{ $event_long_name }}
-<form action="/" method="POST">{{ csrf_field() }}<input type="submit" name="delete_mission_{{ $event_id }}" value="Delete this mission"></form>
+<form action="/" method="POST">
+    {{ csrf_field() }}
+    <input type="hidden" name="chosen" value="{{ $user_id }}">
+    <input type="submit" name="Met" value="I found them" class="met">
+    <input type="submit" name="delete_mission_{{ $event_id }}" value="Delete this mission">
+</form>
 </div>
