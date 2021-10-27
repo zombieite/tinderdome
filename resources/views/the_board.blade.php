6cbea9d3e1cdc132f3c3ec9053a4f5ad798cce08
zombieite <387685+zombieite@users.noreply.github.com>
2021-10-27 23:13:45 +1100

    theboard

--- resources/views/the_board.blade.php
+++ resources/views/the_board.blade.php
@@ -1,114 +1,25 @@
 @extends('layouts.app')
 @section('content')
 <table>
-    <tr><td colspan="11">Act 1: Thesis</td></tr>
-    <tr><td>Chapter</td><td>Pages</td><td>Beats</td><td>Title</td><td>Setting</td><td>Songs</td><td>Summary</td><td>Answered Question</td><td>New Question</td></tr>
+<tr><td>Chapter</td><td>Pages</td><td>Beats</td><td>Title</td><td>Setting</td><td>Summary</td><td>Answered Question</td><td>New Question</td></tr>
+@foreach ($acts as $act)
+    <tr><td colspan="11">{{ $act['name'] }}</td></tr>
+    @foreach ($act['chapters'] as $number => $chapter)
     <tr>
-        <td>0</td>
-        <td>1-11 (0%-10%)</td>
-        <td>Opening Image, Mirror Of Final Image, Theme Hinted At, This Is Gonna Be Good, Everyday Life, 6 Things That Need Fixing, Introduce Running Gags, Introduce A-Story Characters Who Are Mirrors Of B-Story Characters, All Characters Have Primal Motivations, Save The Cat</td>
-        <td>Tomorrowland</td>
-        <td>Tomorrowland Weekend</td>
-        <td></td>
+        <td>{{ $number }}</td>
         <td>
-FB working the gate at TW. DT introduces herself. Hellies and Gillies go to war in the Cage. FB saves a guy who turns out to be a jerk and the Helly president.
+            {{ round($number_minutes * ($chapter['percent_start'] / 100)) }}-{{ round($number_minutes * ($chapter['percent_end'] / 100)) }}
+            <br>
+            ({{ $chapter['percent_start'] }}%-{{ $chapter['percent_end'] }}%)
         </td>
-        <td>Is FB still alive? (Yes)</td>
-        <td>Is a civil war brewing?</td>
+        <td style="font-size:xx-small;">{{ $chapter['beats'] }}</td>
+        <td>{{ $chapter_specifics[$number]['title'] }}</td>
+        <td>{{ $chapter_specifics[$number]['location']}}</td>
+        <td style="font-size:xx-small;">{{ $chapter_specifics[$number]['summary']}}</td>
+        <td>{{ $chapter_specifics[$number]['answered_question']}}</td>
+        <td>{{ $chapter_specifics[$number]['new_question']}}</td>
     </tr>
-    <tr>
-        <td>1</td>
-        <td>12-24 (11%-22%)</td>
-        <td>Catalyst &amp; Debate</td>
-        <td>Dorktown</td>
-        <td>Tomorrowland Weekend</td>
-        <td></td>
-        <td>
-DT brings MA to FB. MA and FB and DT all get the idea to build a new Firebird in Norwood Ohio where the original plant was. DT asks for help killing MA. Cage battle spills out of the Cage and tomorrowlanders burn down TW. FB saves DT from the flames. CD escapes with lunchbox full of FB's money.
-        </td>
-        <td>Q0 (Yes)</td>
-        <td>How will they get to Norwood?</td>
-    </tr>
-    <tr><td colspan="11">Act 2: Antithesis</td></tr>
-    <tr>
-        <td>2</td>
-        <td>25-29 (23%-26%)</td>
-        <td>Break Into Two, Hero Embarks On Adventure</td>
-        <td>Bakersfield</td>
-        <td>Woolworth's Diner</td>
-        <td></td>
-        <td>
-FB and DT meet up with MS to get funding and weapons. MS gives DT some advice we can't hear. DT begins assisting with narration.
-        </td>
-        <td>Q1 (Skelli's money)</td>
-        <td>Why are they going to Van Nuys?</td>
-    </tr>
-    <tr>
-        <td>3</td>
-        <td>30-54 (27%-49%)</td>
-        <td>Narrative Digression, B-Story Or Love Story, Introduce B-Story Characters Who Are Mirrors Of A-Story Characters, Fun &amp; Games, The Promise Of The Premise, Set Pieces, Then... Midpoint, Mirror Of All Is Lost, False Victory Or False Defeat</td>
-        <td>Van Nuys</td>
-        <td>Castaic, Van Nuys</td>
-        <td>Rolling Stones &middot; Shattered<br>Bruce Springsteen &middot; Rosalita Come Out Tonight</td>
-        <td>
-FB lectures us about economics. FB and DT lay out their quest goals. FB battles CD at Gallion's but fails to get the lunchbox back. Oldkoot and friends are executed by guillotine. FB and DT battle MA and CD at Van Nuys Assembly. They lose all their weapons and fail to kill them or stop them from building the Frankenbird, but they steal the Frankenbird instead.
-        </td>
-        <td>Q2 (Another Firebird factory is there)</td>
-        <td>Who's Laura?</td>
-    </tr>
-    <tr><td colspan="11">Midpoint</td>
-    <tr>
-        <td>4</td>
-        <td>55-74 (50%-67%)</td>
-        <td>Stakes Are Raised, Bad Guys Close In, Then... All Is Lost, Mirror Of Midpoint, False Defeat Or False Victory, Whiff Of Death</td>
-        <td>Seligman</td>
-        <td>Amboy, Seligman, Ash Fork</td>
-        <td></td>
-        <td>
-The story goes from FB and DT doing the chasing to FB and DT being chased. The real stakes of the mission become apparent as DT reveals that MA is now the Gilly president and he commands the entire Gilly army. DT becomes increasingly dissatisfied with her partnership with FB and fears he does not have the guts to kill MA. DT goes off on her own and calls in the Helly army and they machine gun the FD and drop the FD out of their helicopters, while destroying the Firebuddy factory.
-        </td>
-        <td>Q3 (The Grieving Woman from A-N-D)</td>
-        <td>Will FB and DT reconcile and team up again?</td>
-    </tr>
-    <tr>
-        <td>5</td>
-        <td>75-84 (68%-76%)</td>
-        <td>Dark Night Of The Soul, Wallowing In Hopelessness And Sadness And Anger</td>
-        <td>Amarillo</td>
-        <td>A dark night on Route 66</td>
-        <td>Carpenters &middot; Close To You (performed by Dorktown)</td>
-        <td>
-FB kills MA as CD watches. The depth of DT's pain is revealed when she tries to kill herself.
-        </td>
-        <td>Q4 (Yes)</td>
-        <td>Will they build cars in Norwood?</td>
-    </tr>
-    <tr><td colspan="11">Act 3: Synthesis</td></tr>
-    <tr>
-        <td>6</td>
-        <td>85-109 (77%-98%)</td>
-        <td>Narrative Digression, Break Into Three, Fusion Of A And B Stories, Climax, Change The World</td>
-        <td>Norwood</td>
-        <td>Norwood Assembly</td>
-        <td>Manfred Mann's Earth Band &middot; Blinded By The Light<br>Earth Wind &amp; Fire &middot; Let's Groove</td>
-        <td>
-FB and DT work together to build the first of the new V8 Trans Ams in Norwood. The Helly and Gilly armies converge on Norwood, but FB gets help from the friends he made along the way and they win the battle.
-        </td>
-        <td>Q5 (Yes)</td>
-        <td>Did FB kill CD? (UNANSWERED)</td>
-    </tr>
-    <tr>
-        <td>7</td>
-        <td>109-110 (99%-100%)</td>
-        <td>Denouement, Final Image, Mirror Of Opening Image, All Arcs Are Complete, All Loose Ends Tied Up Except For The One New Question</td>
-        <td>Norwood</td>
-        <td>Norwood Assembly</td>
-        <td>John Stewart &middot; Mother Country</td>
-        <td>
-They burn down the guillotine as a mirror image of the opening image conflict.
-        </td>
-        <td></td>
-        <td>What was it that the USA got right?</td>
-    </tr>
+    @endforeach
+@endforeach
 </table>
 @endsection
