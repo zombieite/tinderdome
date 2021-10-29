59a01bcbc9a442e637ee196ca501dadd71443b0a
John Binns <john_binns@yahoo.com>
2021-10-29 13:48:26 +1100

    txt

--- resources/views/awaited.blade.php
+++ resources/views/awaited.blade.php
@@ -18,13 +18,9 @@ Too implausible to be an autobiography. Too real to be fiction. It's a missing i
 <h1><a href="/a-n-d/awaited-nonfictional-delusion.pdf">PDF</a></h1>
 </div>
 
-<div class="profile_search_block" style="vertical-align:top;">
-<h1>NEXT: <a href="/heads-will-rock-a-chronicle-of-postapocalyptic-mayhem">Heads Will Rock</a></h1>
-</div>
-
 <br>
 
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-000000.txt">Chapter 00 (txt)</a><br><br>
 <a href="/a-n-d/00.mp3">Chapter 00 (mp3)</a><br>
 <div class="small">
@@ -37,7 +33,7 @@ Carpenters - Close To You
 Read by Kelli Robinson<br>
 </div>
 </div>
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-012501.txt">Chapter 01 (txt)</a><br>
 <a href="/a-n-d/01.mp3">Chapter 01 (mp3)</a><br>
 <div class="small">
@@ -55,7 +51,7 @@ Read by Kelli Robinson<br>
 </div><br>
 <a href="https://www.youtube.com/watch?v=tPGFdrbHxKA">Chapter 01, Scene II (Film)</a>
 </div>
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-022502.txt">Chapter 02 (txt)</a><br>
 <a href="/a-n-d/02.mp3">Chapter 02 (mp3)</a><br><br>
 <a href="/a-n-d/02-kelli.m4a">Chapter 02 (m4a Aussie narration)</a><br>
@@ -63,7 +59,7 @@ Read by Kelli Robinson<br>
 Read by Kelli Robinson<br>
 </div>
 </div>
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-033011.txt">Chapter 03 (txt)</a><br>
 <a href="/a-n-d/03.mp3">Chapter 03 (mp3)</a>
 <div class="small">
@@ -73,25 +69,25 @@ Rush - Spirit Of Radio<br>
 Peter Gabriel and Kate Bush - Don't Give Up
 </div>
 </div>
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-043508.txt">Chapter 04 (txt)</a><br>
 <a href="/a-n-d/04.mp3">Chapter 04 (mp3)</a>
 </div>
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-053604.txt">Chapter 05 (txt)</a><br>
 <a href="/a-n-d/05.mp3">Chapter 05 (mp3)</a>
 </div>
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-063608.txt">Chapter 06 (txt)</a><br>
 <a href="/a-n-d/06.mp3">Chapter 06 (mp3)</a><br><br>
 <a href="/a-n-d/deadline_0.mp3">Deadline's Introduction</a><br>
 <div class="small">By Chris Okula</div><br>
 </div>
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-073701.txt">Chapter 07 (txt)</a><br>
 <a href="/a-n-d/07.mp3">Chapter 07 (mp3)</a>
 </div>
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-083704.txt">Chapter 08 (txt)</a><br>
 <a href="/a-n-d/08.mp3">Chapter 08 (mp3)</a><br><br>
 <a href="/a-n-d/deadline_uranium_springs_2019_1.mp3">Deadline's Story 1</a><br>
@@ -102,7 +98,7 @@ Deadline's Story 2 (recording is missing)<br><br>
 <a href="/a-n-d/deadline_uranium_springs_2019_4.mp3">Deadline's Story 4</a><br>
 <div class="small">By Chris Okula</div><br>
 </div>
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-093708.txt">Chapter 09 (txt)</a><br>
 <a href="/a-n-d/09.mp3">Chapter 09 (mp3)</a>
 <div class="small">
@@ -112,7 +108,7 @@ Ministry - Thieves<br>
 Ministry - Jesus Built My Hotrod<br>
 </div>
 </div>
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-103711.txt">Chapter 10 (txt)</a><br>
 <a href="/a-n-d/10.mp3">Chapter 10 (mp3)</a>
 <div class="small">
@@ -120,13 +116,17 @@ AC/DC - Back In Black<br>
 Carpenters - Close To You
 </div>
 </div>
-<div class="profile_search_block" style="width:13em;height:30em;vertical-align:top;">
+<div class="profile_search_block" style="width:20em;height:30em;vertical-align:top;">
 <a href="/a-n-d/awaited_nonfictional_delusion-113808.txt">Chapter 11 (txt)</a><br>
 <a href="/a-n-d/11.mp3">Chapter 11 (mp3)</a>
 <div class="small">
 John Gorka - I'm From New Jersey
 </div><br>
 <a href="https://www.youtube.com/watch?v=bhFScP_Ag5Y">Year 38 Update, Putting The Rumors To Rest (Film)</a>
+</div>
+
+<div class="profile_search_block" style="vertical-align:top;">
+<h1>NEXT: <a href="/heads-will-rock-a-chronicle-of-postapocalyptic-mayhem" class="bright">Heads Will Rock</a></h1>
 </div>
 
 <iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/bhFScP_Ag5Y" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
