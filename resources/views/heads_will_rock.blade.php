0d165f6a7205b757719683ace031b815920d3bef
John Binns <john_binns@yahoo.com>
2021-11-27 04:27:41 +1100

    txt

--- resources/views/heads_will_rock.blade.php
+++ resources/views/heads_will_rock.blade.php
@@ -8,21 +8,18 @@ $hwr_ebook = "https://www.lulu.com/en/us/shop/john-binns-and-steve-scholz-and-ma
 <a href="{{ $hwr }}"><img style="width:100%;" src="/h-w-r/heads-will-rock.jpg"></a>
 
 <div class="profile_search_block" style="width:20em;vertical-align:top;text-align:center;">
+<h1><a href="{{ $hwr }}" class="bright">BUY PAPERBACK</a></h1>
 <a href="{{ $hwr }}"><img style="width:200px;" src="/h-w-r/heads_will_rock_front_cover.jpg"></a>
-<h1><a href="{{ $hwr }}" class="bright">PAPERBACK</a></h1>
+<h1><a href="{{ $hwr_ebook }}" class="bright">BUY EBOOK</a></h1>
 </div>
 
-<div class="profile_search_block" style="width:20em;vertical-align:top;text-align:center;">
-<h1><a href="{{ $hwr_ebook }}" class="bright">
-EBOOK</a></h1>
-</div>
-
-<p>
+<div class="profile_search_block" style="width:20em;vertical-align:top;">
+INTRODUCTION
+<br><br>
 Not many people have written books in the past 38 years. So nowadays every book that gets written sort of has to be a history book and a philosophy book and a survival book too. I'd be doing you a disservice if I didn't tell you what I know about how things got to be the way they are and offer you some ideas about how to get things back to the way they were.
-</p>
-<p>
+<br><br>
 This is the story of how we killed some people while trekking across the USA on Route 66 trying to rediscover the American Dream. 22000 words. 110 pages. 5 illustrations. Short sentences. No commas. No descriptions. Gimme two hours of your life. I'm not gonna waste your time.
-</p>
+</div>
 
 <div class="profile_search_block" style="width:20em;vertical-align:top;">
 <span class="small">Act I</span><br>
