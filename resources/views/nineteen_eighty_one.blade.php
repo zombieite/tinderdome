05191506bb8cd14706e74dee9fd1112255e8def3
John Binns <john_binns@yahoo.com>
2021-12-13 20:24:36 +1100

    adding 1981 short film homepage

---
+++ resources/views/nineteen_eighty_one.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.nineteen_eighty_one_layout')
+@php
+#$hwr = "https://www.lulu.com/en/us/shop/john-binns-and-steve-scholz-and-marky-make-up/heads-will-rock/paperback/product-r2vrpv.html";
+#$hwr_ebook = "https://www.lulu.com/en/us/shop/john-binns-and-steve-scholz-and-marky-make-up/heads-will-rock/ebook/product-d77kzp.html";
+@endphp
+@section('content')
+
+TODO
+
+@endsection
