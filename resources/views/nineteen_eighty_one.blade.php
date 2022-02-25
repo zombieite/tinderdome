a31a63f265f33695fe68b4ea9b0270d978ef3d4b
zombieite <387685+zombieite@users.noreply.github.com>
2022-02-25 21:52:57 +1100

    txt

--- resources/views/nineteen_eighty_one.blade.php
+++ resources/views/nineteen_eighty_one.blade.php
@@ -93,7 +93,7 @@ John "Firebird" Binns
 <p class="character">DEBBY</p>
 <p class="p3"><div class="dialogue">Working late again?</div></p>
 <p class="character">BRUCE</p>
-<p class="p3"><div class="dialogue">The CEO is coming in to town.</div></p>
+<p class="p3"><div class="dialogue">The CEO is coming in to town today.</div></p>
 <p class="character">DEBBY</p>
 <p class="p3"><div class="dialogue">Pappagallo?</div></p>
 <p class="character">BRUCE</p>
