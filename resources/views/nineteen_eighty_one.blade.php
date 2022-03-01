6143d4af3fd0248f78ecf46d06bbc48667304e1d
zombieite <387685+zombieite@users.noreply.github.com>
2022-03-01 22:19:12 +1100

    txt

--- resources/views/nineteen_eighty_one.blade.php
+++ resources/views/nineteen_eighty_one.blade.php
@@ -207,7 +207,8 @@ As you've probably heard we're being nationalised. We all knew it was coming. So
 <p class="character">BRUCE</p>
 <p class="p3"><div class="dialogue">That oil doesn't belong to you. It belongs to everyone.</div></p>
 <p class="character">PAPPAGALLO</p>
-<p class="p3"><div class="dialogue">That may be, but it takes the brains of people like me to get it out of the ground. It takes our engineers to turn it into something everyone can use. It takes people like you to keep the trucks running. That's why I want you out there with us. You're the best mechanic we've got. </div></p>
+<p class="p3"><div class="dialogue">
+That may be, but it takes the brains of people like me to get it out of the ground. It takes our engineers to turn it into something everyone can use. And it takes guys like you to keep the trucks running. That's why I want you out there with us. You're the best mechanic we've got.</div></p>
 <p class="character">BRUCE</p>
 <p class="p3"><div class="dialogue">No I'm not. Neil is.</div></p>
 <p class="p1">Pappagallo looks at Neil in the distance.</p>
