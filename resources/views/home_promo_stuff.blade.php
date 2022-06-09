2f8895d68053b669048abbf8c5ce573fb5bfaeac
zombieite <387685+zombieite@users.noreply.github.com>
2022-06-09 11:55:44 +1000

    txt

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,2 +1,2 @@
-<a class="small bright" href="https://www.youtube.com/watch?v=YaMjCREtDP0">1981: The Fall of 7 Sisters Petroleum</a>
+<iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/YaMjCREtDP0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 <hr>
