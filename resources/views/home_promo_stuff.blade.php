349786d22f80d7ba21d23934de1ca6165e8e5dbb
John Binns <john_binns@yahoo.com>
2022-10-25 08:16:51 +1100

    txt

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,2 +1,2 @@
-<iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/YaMjCREtDP0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<a href="https://www.youtube.com/watch?v=YaMjCREtDP0">1981: The Fall of 7 Sisters Petroleum</a>
 <hr>
