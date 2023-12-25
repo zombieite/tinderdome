f9459aabd5fb50b4c8fa828ff5e52367d70396d5
Firebird <john_binns@yahoo.com>
2023-12-25 22:27:25 +1100

    add error message when someone picks the same person you picked

--- resources/views/hunt.blade.php
+++ resources/views/hunt.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 @section('content')
+@if ($quarry_already_chosen)
+    <h1>Your chosen quarry was just chosen by someone else. You must choose a new quarry.</h1>
+@endif
 @if ($matchme)
     @if ($my_match_user_id)
         <a href="/profile/match?event_id={{ $event_id }}">You now have a match for {{ $event_name }}!</a>
