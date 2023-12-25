5b4c9d217d58909a15e668cf24803dcd3d4c671f
zombieite <387685+zombieite@users.noreply.github.com>
2023-12-25 22:38:43 +1100

    txt for bounty hunt

--- resources/views/you_are_awaited.blade.php
+++ resources/views/you_are_awaited.blade.php
@@ -1,3 +1,7 @@
     @if ($matched_to_user->event_is_in_future and $matched_to_user->name and $matched_to_user->this_users_rating_of_logged_in_user !== 0)
-        <h1>YOU ARE AWAITED AT {{ strtoupper($matched_to_user->event_long_name) }}! <a class="bright" href="/profile/match?event_id={{ $matched_to_user->event_id }}">Here's your match</a>.</h1>
+        @if ($matched_to_user->bounty_hunt)
+            <h1>YOU ARE SIGNED UP FOR A BOUNTY HUNT AT {{ strtoupper($matched_to_user->event_long_name) }}! <a class="bright" href="/profile/match?event_id={{ $matched_to_user->event_id }}">Here's your quarry</a>. Someone else might be hunting you down, too.</h1>
+        @else
+            <h1>YOU ARE AWAITED AT {{ strtoupper($matched_to_user->event_long_name) }}! <a class="bright" href="/profile/match?event_id={{ $matched_to_user->event_id }}">Here's your match</a>.</h1>
+        @endif
     @endif
