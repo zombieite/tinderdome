49dd4a124e657b8eecc63351391ef54b293c779d
Firebird <john_binns@yahoo.com>
2024-01-09 07:39:04 +1100

    txt

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,2 +1,2 @@
-<a href="https://www.youtube.com/wastelandfirebird/videos">Subscribe to Firebird's YouTube channel</a>&nbsp;&nbsp;&middot;&nbsp;&nbsp;<a href="/heads-will-rock">Heads Will Rock: A chronicle of postapocalyptic mayhem</a>
+<a href="https://www.youtube.com/wastelandfirebird/videos">Firebird's YouTube channel</a>&nbsp;&nbsp;&middot;&nbsp;&nbsp;<a href="/heads-will-rock">Heads Will Rock: A chronicle of postapocalyptic mayhem</a>&nbsp;&nbsp;&middot;&nbsp;&nbsp;<a href="/awaited-nonfictional-delusion">Awaited: Nonfictional Delusion</a>
 <hr>
