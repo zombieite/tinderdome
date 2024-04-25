222407493b938cdbab4ad293cc27fac2ec3f1ef9
zombieite <387685+zombieite@users.noreply.github.com>
2024-04-24 21:47:32 -0500

    txt

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,14 +1,14 @@
-<a href="https://www.youtube.com/wastelandfirebird/videos" class="bright">Firebird's YouTube channel</a>
+<a href="https://www.youtube.com/wastelandfirebird/videos">Firebird's YouTube channel</a>
 &nbsp;&middot;&nbsp;
 <a href="https://www.youtube.com/watch?v=YaMjCREtDP0">1981: The Fall of 7 Sisters Petroleum</a>
 &nbsp;&middot;&nbsp;
-<a href="https://www.youtube.com/watch?v=rjM1SrQjmCE" class="bright">1982: A Mad Max story</a>
+<a href="https://www.youtube.com/watch?v=rjM1SrQjmCE">1982: A Mad Max story</a>
 &nbsp;&middot;&nbsp;
 <a href="https://www.youtube.com/watch?v=sssM63WuQmM">Feral: A Mad Max story</a>
 &nbsp;&middot;&nbsp;
-<a href="https://www.youtube.com/watch?v=tPGFdrbHxKA" class="bright">Roadblock</a>
+<a href="https://www.youtube.com/watch?v=tPGFdrbHxKA">Roadblock</a>
 <hr>
 <a href="/awaited-nonfictional-delusion">Awaited: Nonfictional Delusion</a>
 &nbsp;&middot;&nbsp;
-<a href="/heads-will-rock" class="bright">Heads Will Rock: A chronicle of postapocalyptic mayhem</a>
+<a href="/heads-will-rock">Heads Will Rock: A chronicle of postapocalyptic mayhem</a>
 <hr>
