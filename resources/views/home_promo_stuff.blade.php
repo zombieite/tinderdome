3d7525e029b726816aaf325c7b2b10623be8e959
EC2 Default User <ec2-user@ip-172-31-43-178.us-east-2.compute.internal>
2025-03-21 06:57:34 +0000

    txt

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,4 +1,4 @@
-<iframe style="width:100%;height:240px" src="https://www.youtube.com/embed/cHlSDE7MjbI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<iframe style="width:100%;height:240px" src="https://www.youtube.com/embed/cxKiqG13Y3U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 <hr>
 <a href="https://www.youtube.com/wastelandfirebird/videos">Firebird's YouTube channel</a>
 &nbsp;&middot;&nbsp;
