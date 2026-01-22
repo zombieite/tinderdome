fe0e2a67fff430ced94fb2ab37b922bc68d8248c
zombieite <387685+zombieite@users.noreply.github.com>
2026-01-23 00:11:52 +1100

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <title>You Are Awaited</title>
 <link rel="manifest" href="/manifest.json">
 <link rel="apple-touch-icon" href="/icon-192.png">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 <meta name="apple-mobile-web-app-title" content="You Are Awaited">
