8f99bacb0b709219e95474d91858e6a91a9b612b
zombieite <387685+zombieite@users.noreply.github.com>
2026-04-04 07:49:32 -0700

    header promo convergence

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,12 +1,4 @@
-<iframe style="width:100%;height:240px" src="https://www.youtube.com/embed/YaMjCREtDP0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<iframe style="width:100%;height:240px" src="https://www.youtube.com/embed/cxKiqG13Y3U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 <hr>
-<a href="https://www.youtube.com/wastelandfirebird/videos">Firebird's YouTube channel</a>
-&nbsp;&middot;&nbsp;
-<a href="https://www.youtube.com/watch?v=YaMjCREtDP0">1981: The Fall of 7 Sisters Petroleum</a>
-&nbsp;&middot;&nbsp;
-<a href="https://www.youtube.com/watch?v=rjM1SrQjmCE">1982: A Mad Max story</a>
-&nbsp;&middot;&nbsp;
-<a href="/awaited-nonfictional-delusion">Awaited: Nonfictional Delusion</a>
-&nbsp;&middot;&nbsp;
-<a href="/heads-will-rock">Heads Will Rock: A chronicle of postapocalyptic mayhem</a>
+<h1 class="bright">Traveling I40 west to Neotropolis? Join our caravan: <a href="https://wastelandfirebird.com/">The Great Route 66 Centennial Convergence</a></h1>
 <hr>
