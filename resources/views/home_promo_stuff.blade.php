548497e5af382024a0b5ffd48ccf19b03168bf9e
zombieite <387685+zombieite@users.noreply.github.com>
2026-05-04 11:53:30 +0000

    update promo stuff

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,4 +1,4 @@
 <iframe style="width:100%;height:240px" src="https://www.youtube.com/embed/cxKiqG13Y3U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 <hr>
-<h1 class="bright">Traveling I40 west to Neotropolis? Join our caravan: <a href="https://wastelandfirebird.com/">The Great Route 66 Centennial Convergence</a></h1>
+<a class="bright" href="https://youtube.com/wastelandfirebird">Check out Wasteland Firebird on YouTube</a>
 <hr>
