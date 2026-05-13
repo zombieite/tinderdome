9812d645288c7c1188182d9c7f6eb2dc086508e6
zombieite <387685+zombieite@users.noreply.github.com>
2026-05-13 21:55:15 +1000

    home promo

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,4 +1,8 @@
 <iframe style="width:100%;height:240px" src="https://www.youtube.com/embed/cxKiqG13Y3U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 <hr>
 <a class="bright" href="https://youtube.com/wastelandfirebird">Check out Wasteland Firebird on YouTube</a>
+&nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="/awaited-nonfictional-delusion">Awaited: Nonfictional Delusion</a>
+&nbsp;&nbsp;|&nbsp;&nbsp; 
+<a href="/heads-will-rock">Heads Will Rock: A chronicle of postapocalyptic mayhem</a>
 <hr>
