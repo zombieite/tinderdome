2d03412269ccbd50877ecb2b34407a71c3e0beec
zombieite <387685+zombieite@users.noreply.github.com>
2026-05-15 06:44:13 +1000

    txt

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,6 +1,6 @@
 <iframe style="width:100%;height:240px" src="https://www.youtube.com/embed/cxKiqG13Y3U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 <hr>
-<a class="bright" href="https://youtube.com/wastelandfirebird">Check out Wasteland Firebird on YouTube</a>
+<a href="https://youtube.com/wastelandfirebird">Wasteland Firebird on YouTube</a>
 &nbsp;&nbsp;|&nbsp;&nbsp;
 <a href="/awaited-nonfictional-delusion">Awaited: Nonfictional Delusion</a>
 &nbsp;&nbsp;|&nbsp;&nbsp; 
