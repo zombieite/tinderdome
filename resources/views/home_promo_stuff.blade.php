84cd396f22202718a86deb546f70e8619b410ef7
zombieite <387685+zombieite@users.noreply.github.com>
2026-07-23 19:33:00 +1000

    txt

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,6 +1,6 @@
 <iframe style="width:100%;height:240px" src="https://www.youtube.com/embed/cxKiqG13Y3U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 <hr>
-<a href="https://youtube.com/wastelandfirebird">Wasteland Firebird on YouTube</a>
+<a href="https://wastelandfirebird.com">Wasteland Firebird's Big List of the Best Things On Route 66</a>
 &nbsp;&nbsp;|&nbsp;&nbsp;
 <a href="/awaited-nonfictional-delusion">Awaited: Nonfictional Delusion</a>
 &nbsp;&nbsp;|&nbsp;&nbsp; 
