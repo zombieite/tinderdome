6ef7de0ef58db2d5d5e319f559a9ce6b3dfc75fc
zombieite <387685+zombieite@users.noreply.github.com>
2026-08-05 20:00:50 +1000

    txt

--- resources/views/awaited.blade.php
+++ resources/views/awaited.blade.php
@@ -4,11 +4,10 @@
 
 <a href="https://www.lulu.com/shop/john-binns/awaited-nonfictional-delusion/paperback/product-24218988.html"><img style="width:100%;filter:none;" src="/a-n-d/awaited-nonfictional-delusion.jpg"></a>
 
-<p>
-Too implausible to be an autobiography. Too real to be fiction. It's a missing instruction manual for life. This book will help you make friends. This book will help you make money. This book might even save your life. Two years in the making, this tiny, 143-page book has been meticulously engineered to evoke a sense of hope during the apocalypse. It will inspire you to change your life and change the world. Anarchy, attachment, cars, economics, entrepreneurship, entropy, ethics, grief, guilt, guns, kindness, self-reliance, seventies nostalgia, and stoicism. This is the You Are Awaited story. Awaited: Nonfictional Delusion, by John Binns.
-</p>
 
 <iframe style="width:100%;height:480px" src="https://www.youtube.com/embed/tPGFdrbHxKA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+<p>Too implausible to be an autobiography. Too real to be fiction. It's a missing instruction manual for life. This book will help you make friends. This book will help you make money. This book might even save your life. Two years in the making, this tiny, 143-page book has been meticulously engineered to evoke a sense of hope during the apocalypse. It will inspire you to change your life and change the world. Anarchy, attachment, cars, economics, entrepreneurship, entropy, ethics, grief, guilt, guns, kindness, self-reliance, seventies nostalgia, and stoicism. This is the You Are Awaited story. Awaited: Nonfictional Delusion, by John Binns.</p>
 
 <div class="profile_search_block" style="vertical-align:top;">
 <h1><a class="bright" href="https://www.lulu.com/shop/john-binns/awaited-nonfictional-delusion/paperback/product-24218988.html">Paperback</a></h1>
