aa9467d5009fb24351c8de83fce227bbae97d59f
zombieite <387685+zombieite@users.noreply.github.com>
2026-09-22 21:16:26 +1000

    clean up home promo css

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,10 +1,10 @@
 <iframe style="width:100%;height:240px" src="https://www.youtube.com/embed/cxKiqG13Y3U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 <hr>
-<a href="https://wastelandfirebird.com">Wasteland&nbsp;Firebird's&nbsp;Big&nbsp;List&nbsp;of&nbsp;the&nbsp;Best&nbsp;Things&nbsp;On&nbsp;Route&nbsp;66</a>
+<a class="homepromo" href="https://wastelandfirebird.com">Wasteland&nbsp;Firebird's&nbsp;Big&nbsp;List&nbsp;of&nbsp;the&nbsp;Best&nbsp;Things&nbsp;On&nbsp;Route&nbsp;66</a>
 &nbsp;&nbsp;|&nbsp;&nbsp;
-<a href="https://www.youtube.com/watch?v=YaMjCREtDP0">1981:&nbsp;The&nbsp;Fall&nbsp;of&nbsp;7&nbsp;Sisters&nbsp;Petroleum</a>
+<a class="homepromo" href="https://www.youtube.com/watch?v=YaMjCREtDP0">1981:&nbsp;The&nbsp;Fall&nbsp;of&nbsp;7&nbsp;Sisters&nbsp;Petroleum</a>
 &nbsp;&nbsp;|&nbsp;&nbsp; 
-<a href="/awaited-nonfictional-delusion">Awaited:&nbsp;Nonfictional&nbsp;Delusion</a>
+<a class="homepromo" href="/awaited-nonfictional-delusion">Awaited:&nbsp;Nonfictional&nbsp;Delusion</a>
 &nbsp;&nbsp;|&nbsp;&nbsp; 
-<a href="/heads-will-rock">Heads&nbsp;Will&nbsp;Rock:&nbsp;A&nbsp;chronicle&nbsp;of&nbsp;postapocalyptic&nbsp;mayhem</a>
+<a class="homepromo" href="/heads-will-rock">Heads&nbsp;Will&nbsp;Rock:&nbsp;A&nbsp;chronicle&nbsp;of&nbsp;postapocalyptic&nbsp;mayhem</a>
 <hr>
