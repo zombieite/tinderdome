7e706c0c28542308f55d83255d4f67fff82afa42
zombieite <387685+zombieite@users.noreply.github.com>
2026-09-22 18:44:06 +1000

    clean up promo links

--- resources/views/home_promo_stuff.blade.php
+++ resources/views/home_promo_stuff.blade.php
@@ -1,10 +1,10 @@
 <iframe style="width:100%;height:240px" src="https://www.youtube.com/embed/cxKiqG13Y3U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 <hr>
-<a href="https://wastelandfirebird.com">Wasteland Firebird's Big List of the Best Things On Route 66</a>
+<a href="https://wastelandfirebird.com">Wasteland&nbsp;Firebird's&nbsp;Big&nbsp;List&nbsp;of&nbsp;the&nbsp;Best&nbsp;Things&nbsp;On&nbsp;Route&nbsp;66</a>
 &nbsp;&nbsp;|&nbsp;&nbsp;
-<a href="https://www.youtube.com/watch?v=YaMjCREtDP0">1981: The Fall of 7 Sisters Petroleum</a>
+<a href="https://www.youtube.com/watch?v=YaMjCREtDP0">1981:&nbsp;The&nbsp;Fall&nbsp;of&nbsp;7&nbsp;Sisters&nbsp;Petroleum</a>
 &nbsp;&nbsp;|&nbsp;&nbsp; 
-<a href="/awaited-nonfictional-delusion">Awaited: Nonfictional Delusion</a>
+<a href="/awaited-nonfictional-delusion">Awaited:&nbsp;Nonfictional&nbsp;Delusion</a>
 &nbsp;&nbsp;|&nbsp;&nbsp; 
-<a href="/heads-will-rock">Heads Will Rock: A chronicle of postapocalyptic mayhem</a>
+<a href="/heads-will-rock">Heads&nbsp;Will&nbsp;Rock:&nbsp;A&nbsp;chronicle&nbsp;of&nbsp;postapocalyptic&nbsp;mayhem</a>
 <hr>
